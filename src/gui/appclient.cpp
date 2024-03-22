c421b7305445440407e6c0af2b1da399737c7679
Luc Guyot <luc.guyot@infomaniak.com>
2024-03-22 15:02:50 +0100

    Adds comment about std::chrono::day (C++20 only)

--- src/gui/appclient.cpp
+++ src/gui/appclient.cpp
@@ -547,7 +547,7 @@ void AppClient::setupLogging() {
 
         logger->setupTemporaryFolderLogDir();
         if (ParametersCache::instance()->parametersInfo().purgeOldLogs()) {
-            logger->setLogExpire(std::chrono::hours(ClientGui::logsPurgeRate() * 24));
+            logger->setLogExpire(std::chrono::hours(ClientGui::logsPurgeRate() * 24));  // TODO(Luc): C++20 can handle days.
         } else {
             logger->setLogExpire(std::chrono::hours(0));
         }
