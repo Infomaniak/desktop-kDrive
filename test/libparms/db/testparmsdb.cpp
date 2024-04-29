2f1b9a15b3a7bbee60edeb88ff70698bad068822
Herve Eruam <82284536+herve-er@users.noreply.github.com>
2024-04-29 12:09:35 +0200

    Apply suggestions from code review
    Co-authored-by: Luc Guyot <162997198+luc-guyot-infomaniak@users.noreply.github.com>

--- test/libparms/db/testparmsdb.cpp
+++ test/libparms/db/testparmsdb.cpp
@@ -302,11 +302,11 @@ void TestParmsDb::testAppState(void) {
     bool found = true;
     std::string value;
     CPPUNIT_ASSERT(ParmsDb::instance()->selectAppState(AppStateKeyTest, value, found) && found);
-    CPPUNIT_ASSERT(value == "Test");
+    CPPUNIT_ASSERT_EQUAL("Test", value);
 
     CPPUNIT_ASSERT(ParmsDb::instance()->updateAppState(AppStateKeyTest, "value 1", found));
     CPPUNIT_ASSERT(ParmsDb::instance()->selectAppState(AppStateKeyTest, value, found) && found);
-    CPPUNIT_ASSERT(value == "value 1");
+    CPPUNIT_ASSERT_EQUAL("value 1", value);
 
     CPPUNIT_ASSERT(ParmsDb::instance()->updateAppState(static_cast<AppStateKey>(9548215525211611), "value 2", found));
     CPPUNIT_ASSERT(!found);
