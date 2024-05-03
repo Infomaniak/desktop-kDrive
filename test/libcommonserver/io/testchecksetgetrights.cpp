30f50dd79b3ec476a490834eb45cff1f661d8975
Herve_eruam <herve.eruam@infomaniak.com>
2024-05-03 09:34:32 +0200

    Do not restore perm to keep temp dir.

--- test/libcommonserver/io/testchecksetgetrights.cpp
+++ test/libcommonserver/io/testchecksetgetrights.cpp
@@ -77,20 +77,20 @@ void TestIo::testCheckSetAndGetRights() {
                 bool result = IoHelper::setRights(path, rightsSet.read, rightsSet.write, rightsSet.execute, ioError);
                 result &= ioError == IoErrorSuccess;
                 if (!result) {
-                    IoHelper::setRights(path, true, true, true, ioError);
+                    //IoHelper::setRights(path, true, true, true, ioError);
                     CPPUNIT_ASSERT(false /* Failed to set base rights */);
                 }
 
                 result = IoHelper::getRights(path, isReadable, isWritable, isExecutable, ioError);
                 result &= ioError == IoErrorSuccess;
                 if (!result) {
-                    IoHelper::setRights(path, true, true, true, ioError);
+                    //IoHelper::setRights(path, true, true, true, ioError);
                     CPPUNIT_ASSERT(false /* Failed to get base rights */);
                 }
 
                 if (!(isReadable == rightsSet.read && isWritable == rightsSet.write &&
                       isExecutable == rightsSet.execute)) {
-                    IoHelper::setRights(path, true, true, true, ioError);
+                    //IoHelper::setRights(path, true, true, true, ioError);
                     CPPUNIT_ASSERT(false /* Set base rights mismatch  with get base rights */);
                 }
 
@@ -98,20 +98,20 @@ void TestIo::testCheckSetAndGetRights() {
                 result = IoHelper::setRights(path, rightsSet.read, rightsSet.write, rightsSet.execute, ioError);
                 result &= ioError == IoErrorSuccess;
                 if (!result) {
-                    IoHelper::setRights(path, true, true, true, ioError);
+                    //IoHelper::setRights(path, true, true, true, ioError);
                     CPPUNIT_ASSERT(false /* Failed to set target rights */);
                 }
 
                 result = IoHelper::getRights(path, isReadable, isWritable, isExecutable, ioError);
                 result &= ioError == IoErrorSuccess;
                 if (!result) {
-                    IoHelper::setRights(path, true, true, true, ioError);
+                    //IoHelper::setRights(path, true, true, true, ioError);
                     CPPUNIT_ASSERT(false /* Failed to get target rights */);
                 }
 
                 if (!(isReadable == rightsSet.read && isWritable == rightsSet.write &&
                       isExecutable == rightsSet.execute)) {
-                    IoHelper::setRights(path, true, true, true, ioError);
+                    //IoHelper::setRights(path, true, true, true, ioError);
                     CPPUNIT_ASSERT(false /* Set target rights mismatch with get target rights */);
                 }
             }
