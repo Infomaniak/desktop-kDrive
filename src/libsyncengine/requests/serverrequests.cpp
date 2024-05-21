1683837dbeb2bd15ae1f307059234c7bb26ec720
Luc Guyot <luc.guyot@infomaniak.com>
2024-05-21 11:33:58 +0200

    Revert unnecessary change in isDisplayableError()

--- src/libsyncengine/requests/serverrequests.cpp
+++ src/libsyncengine/requests/serverrequests.cpp
@@ -912,9 +912,6 @@ bool ServerRequests::isDisplayableError(const Error &error) {
                     return false;
             }
         }
-        case ExitCodeSystemError: {
-            return error.exitCause() == ExitCauseSyncDirDoesntExist;
-        }
         case ExitCodeUnknown: {
             return error.inconsistencyType() != InconsistencyTypePathLength && error.cancelType() != CancelTypeAlreadyExistRemote;
         }
