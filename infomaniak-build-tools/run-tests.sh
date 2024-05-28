0334a9b7ca23d35c8fc5f7a083471afae6351fe2
Maxime Pouce <maxime.pouce@infomaniak.com>
2024-05-28 10:53:39 +0200

    Automatically running all built tests (Linux/Mac)

---
+++ infomaniak-build-tools/run-tests.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+#
+# Infomaniak kDrive - Desktop
+# Copyright (C) 2023-2024 Infomaniak Network SA
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#!/bin/bash
+
+testers=$(find . -type f -name "kDrive_test_*")
+errors=0
+
+for tester in ${testers[@]}; do
+    chmod +x $tester
+    pushd $(dirname "$tester")
+    ./$(basename "$tester")
+    errors=$(($errors + $?))
+    popd
+done
+
+exit $errors
