eefb8d848194b18321b9af1639f293d32fe57b57
Luc Guyot <luc.guyot@infomaniak.com>
2024-06-13 18:38:26 +0200

    Set the git branch name as projectKey for Sonar's scanner

--- infomaniak-build-tools/run-tests.sh
+++ infomaniak-build-tools/run-tests.sh
@@ -30,7 +30,7 @@ testers=$(find . -type f -name "kDrive_test_*")
 errors=0
 failures=()
 
-/Applications/BullseyeCoverage/bin/cov01 -1 # coverage on
+cov01 -1 # coverage on
 
 for tester in ${testers[@]}; do
 
@@ -59,6 +59,6 @@ else
     echo -e "${NC}"
 fi
 
-/Applications/BullseyeCoverage/bin/cov01 -0 # coverage off
+cov01 -0 # coverage off
 
 exit $errors
