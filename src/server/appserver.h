d77fb49302170e6fef1c5571bed3609a964c0ada
Herve Eruam <82284536+herve-er@users.noreply.github.com>
2024-07-04 15:22:03 +0200

    Update src/server/appserver.h
    Co-authored-by: Luc Guyot <162997198+luc-guyot-infomaniak@users.noreply.github.com>

--- src/server/appserver.h
+++ src/server/appserver.h
@@ -132,7 +132,7 @@ class AppServer : public SharedTools::QtSingleApplication {
         void parseOptions(const QStringList &);
         void initLogging() noexcept(false);
         void setupProxy();
-        void handleCrashRecovery(bool &shouldQuit);  // Returns true if the crash recovery is successful, false if the application should exit.
+        void handleCrashRecovery(bool &shouldQuit);  // Sets `shouldQuit` with true if the crash recovery is successful, false if the application should exit.
         bool serverCrashedRecently(int seconds = 60 /*Allow one server self restart per minute (default)*/);
         bool clientCrashedRecently(int second = 60 /*Allow one client self restart per minute (default)*/);
 
