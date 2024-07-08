069b41806869f73509bf3780a6a3f7f9a039067f
Clément Kunz <clement.kunz@infomaniak.com>
2024-07-08 15:23:04 +0200

    KDESKTOP-1019 - Do not set status while starting upload

--- src/libsyncengine/propagation/executor/executorworker.cpp
+++ src/libsyncengine/propagation/executor/executorworker.cpp
@@ -868,11 +868,11 @@ bool ExecutorWorker::convertToPlaceholder(const SyncPath &relativeLocalPath, boo
         return false;
     }
 
-    bool isSyncing = hydrated;
-    if (!_syncPal->vfsForceStatus(absoluteLocalFilePath, isSyncing, 100, hydrated)) {
-        LOGW_WARN(_logger, L"Error in vfsForceStatus: " << Utility::formatSyncPath(absoluteLocalFilePath).c_str());
-        return false;
-    }
+    // bool isSyncing = hydrated;
+    // if (!_syncPal->vfsForceStatus(absoluteLocalFilePath, isSyncing, 100, hydrated)) {
+    //     LOGW_WARN(_logger, L"Error in vfsForceStatus: " << Utility::formatSyncPath(absoluteLocalFilePath).c_str());
+    //     return false;
+    // }
 
     if (!_syncPal->vfsSetPinState(absoluteLocalFilePath, hydrated ? PinStateAlwaysLocal : PinStateOnlineOnly)) {
         LOGW_WARN(_logger, L"Error in vfsSetPinState: " << Utility::formatSyncPath(absoluteLocalFilePath).c_str());
