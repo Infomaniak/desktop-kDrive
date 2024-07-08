3473bdc76274b2ff9cf3e2262627282888cbf373
Herve_eruam <herve.eruam@infomaniak.com>
2024-07-08 14:21:02 +0200

    Remove duplicate funciton in SyncPal

--- src/libsyncengine/syncpal/syncpal.h
+++ src/libsyncengine/syncpal/syncpal.h
@@ -218,10 +218,6 @@ class SYNCENGINE_EXPORT SyncPal : public std::enable_shared_from_this<SyncPal> {
         virtual void refreshTmpBlacklist();
         virtual void removeItemFromTmpBlacklist(const NodeId &nodeId, ReplicaSide side);
 
-        std::shared_ptr<UpdateTree> getUpdateTree(ReplicaSide side) {
-            return side == ReplicaSideLocal ? _localUpdateTree : _remoteUpdateTree;
-        }
-
         //! Makes copies of real-time snapshots to be used by synchronization workers.
         void copySnapshots();
 
