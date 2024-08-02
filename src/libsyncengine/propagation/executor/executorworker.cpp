d8738f1c1848899502f79b0646c37e45118cc6f2
Christophe Larchier <christophe.larchier@infomaniak.com>
2024-08-02 09:00:32 +0200

    Fix Sentry message

--- src/libsyncengine/propagation/executor/executorworker.cpp
+++ src/libsyncengine/propagation/executor/executorworker.cpp
@@ -1022,10 +1022,8 @@ bool ExecutorWorker::generateEditJob(SyncOpPtr syncOp, std::shared_ptr<AbstractJ
                 _executorExitCause = ExitCauseUnknown;
 
 #ifdef NDEBUG
-                std::stringstream ss;
-                ss << "Edit operation with empty corresponding node id (" << _localItemId << "/" << otherNodeId << ")";
-                sentry_capture_event(
-                    sentry_value_new_message_event(SENTRY_LEVEL_WARNING, "ExecutorWorker::generateEditJob", ss.str().c_str()));
+                sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_WARNING, "ExecutorWorker::generateEditJob",
+                                                                    "Edit operation with empty corresponding node id"));
 #endif
 
                 return false;
