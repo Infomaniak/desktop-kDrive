81f107c5982d8f7b3527ae0270d86bd13ac4c963
Herve Eruam <82284536+herve-er@users.noreply.github.com>
2024-08-05 17:43:37 +0200

    Update src/server/socketlistener.cpp

--- src/server/socketlistener.cpp
+++ src/server/socketlistener.cpp
@@ -40,7 +40,6 @@ SocketListener::SocketListener(QIODevice *socket) : socket(socket) {
 
 void SocketListener::sendMessage(const QString &message, bool doWait) const {
         assert(_threadId != std::this_thread::get_id() && "SocketListener::sendMessage should only be called from the main thread");
-    }
 
     if (!socket) {
         LOGW_INFO(KDC::Log::instance()->getLogger(), L"Not sending message to dead socket: " << message.toStdWString().c_str());
