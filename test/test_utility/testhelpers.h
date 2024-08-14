6066726155810c2559f2baddcf4d347f8ccdece1
Clément Kunz <clement.kunz@infomaniak.com>
2024-08-14 08:28:51 +0200

    Fix merge issue

--- test/test_utility/testhelpers.h
+++ test/test_utility/testhelpers.h
@@ -1,4 +1,3 @@
-
 /*
  * Infomaniak kDrive - Desktop
  * Copyright (C) 2023-2024 Infomaniak Network SA
@@ -16,38 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #pragma once
 
-< < < < < < < < HEAD : test / test_utility /
-                       testhelpers.h
 #include "utility/types.h"
 
-                       namespace KDC::testhelpers {
-    SyncName makeNfdSyncName();
-    SyncName makeNfcSyncName();
+namespace KDC::testhelpers {
+SyncName makeNfdSyncName();
+SyncName makeNfcSyncName();
 
 }  // namespace KDC::testhelpers
-== == == ==
-#include "testincludes.h"
-#include "server/updater_v2/abstractupdater.h"
-#include "utility/types.h"
-    using namespace CppUnit;
-
-namespace KDC {
-class TestAbstractUpdater : public CppUnit::TestFixture {
-    public:
-        CPPUNIT_TEST_SUITE(TestAbstractUpdater);
-        CPPUNIT_TEST(testCheckUpdateAvailable);
-        CPPUNIT_TEST(testCurrentVersion);
-        CPPUNIT_TEST_SUITE_END();
-
-    public:
-        void setUp() override;
-        void tearDown() override;
-
-    protected:
-        void testCheckUpdateAvailable();
-        void testCurrentVersion();
-};
-}  // namespace KDC
->>>>>>>> 37bcbb1e65f30900cafbb83e6cff12156a7a3dfe : test / server / updater / testabstractupdater.h
