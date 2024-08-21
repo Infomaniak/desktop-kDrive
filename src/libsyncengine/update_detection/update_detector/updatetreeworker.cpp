0f3ba5d3c5afe41a89cde7d94ed5d64c99095ff2
Herve Eruam <herve.eruam@infomaniak.com>
2024-08-21 17:00:41 +0200

    use Str2SyncName instead of s2ws.

--- src/libsyncengine/update_detection/update_detector/updatetreeworker.cpp
+++ src/libsyncengine/update_detection/update_detector/updatetreeworker.cpp
@@ -1174,7 +1174,8 @@ void UpdateTreeWorker::drawUpdateTreeRow(const std::shared_ptr<Node> node, SyncN
     treeStr += Str("[");
     treeStr += Str2SyncName(*node->id());
     treeStr += Str(" / ");
-    treeStr += node->changeEvents() != OperationType::None ? Utility::s2ws(enumClassToString(node->changeEvents())).c_str() : Str("-");
+    treeStr +=
+        node->changeEvents() != OperationType::None ? Str2SyncName(enumClassToString(node->changeEvents())).c_str() : Str("-");
     treeStr += Str("]");
     treeStr += Str("\n");
 
