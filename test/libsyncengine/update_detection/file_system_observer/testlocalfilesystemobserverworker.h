ce7b00904f4fa33298221fd64c3c119be6758a72
Herve Eruam <herve.eruam@infomaniak.com>
2024-12-03 08:54:31 +0100

    Remove empty line.

--- test/libsyncengine/update_detection/file_system_observer/testlocalfilesystemobserverworker.h
+++ test/libsyncengine/update_detection/file_system_observer/testlocalfilesystemobserverworker.h
@@ -98,7 +98,6 @@ class TestLocalFileSystemObserverWorker : public CppUnit::TestFixture {
         void testLFSOWithSpecialCases1();
         void testLFSOWithSpecialCases2();
 
-
         static bool vfsStatus(int, const SyncPath &, bool &, bool &, bool &, int &) { return true; };
         static bool vfsPinState(int, const SyncPath &, PinState &) { return true; };
         static bool vfsFileStatusChanged(int, const SyncPath &, SyncFileStatus) { return true; };
