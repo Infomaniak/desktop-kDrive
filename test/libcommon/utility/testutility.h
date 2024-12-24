3bb0cb2ed228f106a9091eb04200345912bcaff2
Christophe Larchier <christophe.larchier@infomaniak.com>
2024-12-24 16:05:56 +0100

    Re activate some tests

--- test/libcommon/utility/testutility.h
+++ test/libcommon/utility/testutility.h
@@ -25,15 +25,15 @@ namespace KDC {
 
 class TestUtility : public CppUnit::TestFixture {
         CPPUNIT_TEST_SUITE(TestUtility);
-        // CPPUNIT_TEST(testGetAppSupportDir);
-        // CPPUNIT_TEST(testIsVersionLower);
-        // CPPUNIT_TEST(testStringToAppStateValue);
-        // CPPUNIT_TEST(testArgsWriter);
-        // CPPUNIT_TEST(testCompressFile);
-        // CPPUNIT_TEST(testCurrentVersion);
+        CPPUNIT_TEST(testGetAppSupportDir);
+        CPPUNIT_TEST(testIsVersionLower);
+        CPPUNIT_TEST(testStringToAppStateValue);
+        CPPUNIT_TEST(testArgsWriter);
+        CPPUNIT_TEST(testCompressFile);
+        CPPUNIT_TEST(testCurrentVersion);
         CPPUNIT_TEST(testGenerateRandomStringAlphaNum);
 #ifdef _WIN32
-        // CPPUNIT_TEST(testGetLastErrorMessage);
+        CPPUNIT_TEST(testGetLastErrorMessage);
 #endif
         CPPUNIT_TEST_SUITE_END();
 
