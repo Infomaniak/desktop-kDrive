b2157ee5df8f454fe87fca765b7af07af38e9ee0
Clément Kunz <clement.kunz@infomaniak.com>
2025-01-14 11:45:19 +0100

    Address PR comment

--- src/gui/preferenceswidget.cpp
+++ src/gui/preferenceswidget.cpp
@@ -442,13 +442,10 @@ void PreferencesWidget::clearUndecidedLists() {
 }
 
 bool PreferencesWidget::isStaff() const {
+    constexpr auto isStaffCallback = [](std::pair<int, UserInfoClient> const &item) { return item.second.isStaff(); };
     // To be used with an later gcc version
-    // constexpr auto isStaffCallback = [](std::pair<int, UserInfoClient> const &item) { return item.second.isStaff(); };
     // return std::ranges::find_if(_gui->userInfoMap(), isStaffCallback) != _gui->userInfoMap().end();
-    for (const auto &[_, userInfo]: _gui->userInfoMap()) {
-        if (userInfo.isStaff()) return true;
-    }
-    return false;
+    return std::any_of(_gui->userInfoMap().cbegin(), _gui->userInfoMap().cend(), isStaffCallback);
 }
 
 void PreferencesWidget::onFolderConfirmationSwitchClicked(bool checked) {
