85cfd1699fab9fc5b2809a82a62ac5b2a2f1bc5d
Herve Eruam <herve.eruam@infomaniak.com>
2025-02-10 14:28:58 +0100

    Allow parallel build on windows.

--- infomaniak-build-tools/windows/build-drive.ps1
+++ infomaniak-build-tools/windows/build-drive.ps1
@@ -305,8 +305,7 @@ $cmake = ('cmake {0}'-f($args -Join ' '))
 
 Write-Host $cmake
 Invoke-Expression $cmake
-
-$buildArgs += @('--build', $buildPath, '--target all install')
+$buildArgs += @('--build', $buildPath, '--parallel', 12, -- /p:CL_MPcount=4,   '--target all install')
 $buildCall = ('cmake {0}' -f ($buildArgs -Join ' '))
 
 Write-Host $buildCall
