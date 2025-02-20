e44fb83d370f87e81c2403b8b274505bce2be33f
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-02-20 17:35:45 +0100

    [KDESKTOP-1576] Use a uniform build structure for both Linux architectures

--- infomaniak-build-tools/linux/build-release-amd64.sh
+++ infomaniak-build-tools/linux/build-release-amd64.sh
@@ -18,38 +18,118 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-set -ex
-BUILDDIR="$PWD/build-linux"
-CLIENTDIR="$BUILDDIR/client"
-INSTALLDIR="$BUILDDIR/install"
+set -e
 
-rm -Rf "${BUILDDIR}"
-mkdir -p "${BUILDDIR}"
-mkdir -p "${CLIENTDIR}"
-mkdir -p "${INSTALLDIR}"
+src_dir=$PWD
+build_dir="$src_dir/build-linux-amd64"
+app_dir="$build_dir/install"
+build_type="RelWithDebInfo"
 
-podman machine stop build_kdrive
-ulimit -n unlimited
-podman machine start build_kdrive
-podman run --rm -it \
-	--privileged \
-	--ulimit nofile=4000000:4000000 \
-	--volume $HOME/Projects/desktop-kDrive:/src \
-	--volume $HOME/Projects/desktop-kDrive/build-linux:/build \
-	--volume $HOME/Projects/desktop-kDrive/build-linux/install:/install \
-	--workdir "/src" \
-	--env APPLICATION_SERVER_URL="$APPLICATION_SERVER_URL" \
-	--env KDRIVE_VERSION_BUILD="$(date +%Y%m%d)" \
-	--arch amd64 \
-	ghcr.io/infomaniak/kdrive-desktop-linux:latest /bin/bash -c "/src/infomaniak-build-tools/linux/release-appimage-build-amd64.sh"
-podman machine stop build_kdrive
+echo
+echo "Build type: $build_type"
+echo "Source directory '$src_dir'"
+echo "Build directory '$build_dir'"
+echo "Install directory '$app_dir'"
+echo
 
-VERSION=$(grep "KDRIVE_VERSION_FULL" "$BUILDDIR/client/version.h" | awk '{print $3}')
+extract_debug () {
+    objcopy --only-keep-debug "$1/$2" $build_dir/$2.dbg
+    objcopy --strip-debug "$1/$2"
+    objcopy --add-gnu-debuglink=$build_dir/kDrive.dbg "$1/$2"
+}
 
-SUFFIX="master"
+build_release() {
+	export QT_BASE_DIR="~/Qt/6.2.3"
+	export QTDIR="$QT_BASE_DIR/gcc_64"
+	export QMAKE=$QTDIR/bin/qmake
+	export PATH=$QTDIR/bin:$QTDIR/libexec:$PATH
+	export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
+	export PKG_CONFIG_PATH=$QTDIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
-echo $SUFFIX
-mv "${INSTALLDIR}/kDrive-${SUFFIX}-x86_64.AppImage" $INSTALLDIR/kDrive-$VERSION-amd64.AppImage
+	mkdir -p $app_dir
+	mkdir -p $build_dir
+	
+	# Set defaults
+	export SUFFIX=""
+	
+	# Build client
+	mkdir -p $build_dir/client
+	cd $build_dir/client
+	
+	CMAKE_PARAMS=()
+	
+	export KDRIVE_DEBUG=0
+	
+	cmake -B$build_dir/client -H$src_dir \
+	    -DOPENSSL_ROOT_DIR=/usr/local \
+	    -DOPENSSL_INCLUDE_DIR=/usr/local/include \
+	    -DOPENSSL_CRYPTO_LIBRARY=/usr/local/lib64/libcrypto.so \
+	    -DOPENSSL_SSL_LIBRARY=/usr/local/lib64/libssl.so \
+	    -DQT_FEATURE_neon=OFF \
+	    -DCMAKE_BUILD_TYPE=$build_type \
+	    -DCMAKE_INSTALL_PREFIX=/usr \
+	    -DBIN_INSTALL_DIR=$build_dir/client/bin \
+	    -DKDRIVE_VERSION_SUFFIX=$SUFFIX \
+	    -DKDRIVE_THEME_DIR="$src_dir/infomaniak" \
+	    -DKDRIVE_VERSION_BUILD="$(date +%Y%m%d)" \
+	    "${CMAKE_PARAMS[@]}" \
+	
+	make -j$(nproc)
+	
+	extract_debug ./bin kDrive
+	extract_debug ./bin kDrive_client
+	
+	make DESTDIR=$app_dir install
+	
+	cp $src_dir/sync-exclude-linux.lst $build_dir/client/bin/sync-exclude.lst
+} 
 
-rm -Rf "${BUILDDIR}-amd64"
-mv "${BUILDDIR}" "${BUILDDIR}-amd64"
+package_release() {
+	export QT_BASE_DIR=$HOME/Qt/6.2.3/gcc_64
+	export QTDIR=$QT_BASE_DIR
+	export QMAKE=$QT_BASE_DIR/bin/qmake
+	export PATH=$QT_BASE_DIR/bin:$QT_BASE_DIR/libexec:$PATH
+	export LD_LIBRARY_PATH=$QT_BASE_DIR/lib:$app_dir/usr/lib:/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH
+	export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
+
+	mkdir -p $app_dir/usr/plugins
+	cd $app_dir
+	
+	cp -P -r $QTDIR/plugins/* $app_dir/usr/plugins/
+	cp -P -r $QTDIR/libexec $app_dir/usr
+	cp -P -r $QTDIR/resources $app_dir/usr
+	cp -P -r $QTDIR/translations/ $app_dir/usr
+	
+	mv $app_dir/usr/lib/x86_64-linux-gnu/* $app_dir/usr/lib/
+	
+	mkdir -p $app_dir/usr/qml
+	
+	rm -rf $app_dir/usr/lib/x86_64-linux-gnu/
+	rm -rf $app_dir/usr/include/
+	
+	cp $src_dir/sync-exclude-linux.lst $app_dir/usr/bin/sync-exclude.lst
+	cp $app_dir/usr/share/icons/hicolor/512x512/apps/kdrive-win.png $app_dir
+	
+	cp -P /usr/local/lib64/libssl.so* $app_dir/usr/lib/
+	cp -P /usr/local/lib64/libcrypto.so* $app_dir/usr/lib/
+	cp -P -r /usr/lib/x86_64-linux-gnu/nss/ $app_dir/usr/lib/
+	cp ~/Qt/Tools/QtCreator/lib/Qt/lib/libQt6SerialPort.so.6 $app_dir/usr/lib/
+	
+	$HOME/desktop-setup/linuxdeploy-x86_64.AppImage --appdir $app_dir -e $app_dir/usr/bin/kDrive -i $app_dir/kdrive-win.png -d $app_dir/usr/share/applications/kDrive_client.desktop --plugin qt --output appimage -v0
+	
+	full_version=$(grep "KDRIVE_VERSION_FULL" "$build_dir/version.h" | awk '{print $3}')
+    app_name=kDrive-${full_version}-amd64.AppImage
+	mv kDrive*.AppImage $app_dir/$app_name
+}
+
+echo "Building ..."
+build_release
+
+echo
+
+echo "Packaging ..."
+package_release
+
+echo
+
+echo "Done."
