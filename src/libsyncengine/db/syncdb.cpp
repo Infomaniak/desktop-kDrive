e4e5e0ffedc09fda6b8bd5ed8489dde3a39b175f
Clément Kunz <clement.kunz@infomaniak.com>
2025-02-25 15:52:28 +0100

    Remove commented out code

--- src/libsyncengine/db/syncdb.cpp
+++ src/libsyncengine/db/syncdb.cpp
@@ -558,17 +558,6 @@ bool SyncDb::insertNode(const DbNode &node, DbNodeId &dbNodeId, bool &constraint
     return true;
 }
 
-// bool SyncDb::insertNode(const DbNode &node) {
-//     DbNodeId dummyNodeId = 0;
-//     bool dummyConstraintError = false;
-//     return insertNode(node, dummyNodeId, dummyConstraintError);
-// }
-//
-// bool SyncDb::insertNode(const DbNode &node, DbNodeId &dbNodeId) {
-//     bool dummyConstraintError = false;
-//     return insertNode(node, dbNodeId, dummyConstraintError);
-// }
-
 bool SyncDb::updateNode(const DbNode &node, bool &found) {
     const std::lock_guard<std::mutex> lock(_mutex);
 
