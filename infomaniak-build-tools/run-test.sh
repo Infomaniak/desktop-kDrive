c8b2d3a99d20812776a2ad505484d38df0cd5eee
Herve Eruam <herve.eruam@infomaniak.com>
2025-03-03 15:14:23 +0100

    firun-test script.

--- infomaniak-build-tools/run-test.sh
+++ infomaniak-build-tools/run-test.sh
@@ -45,16 +45,16 @@ if [ ! -f "$tester" ]; then
     exit 1
 fi
 
-echo -e "${YELLOW}---------- Running $(basename $tester) ----------${NC}"
-pushd $(dirname "$dir") 1>/dev/null
+echo -e "${YELLOW}---------- Running $($tester) ----------${NC}"
+pushd $dir 1>/dev/null
 chmod +x $tester
-./$(basename "$tester")
+./$tester
 
 if [ $? -ne 0 ]; then
-    echo -e "${RED}---------- Failure: $(basename $tester) ----------${NC}"
+    echo -e "${RED}---------- Failure: $($tester) ----------${NC}"
     exit 1
 else
-    echo -e "${GREEN}---------- Success: $(basename $tester) ----------${NC}"
+    echo -e "${GREEN}---------- Success: $($tester) ----------${NC}"
 fi
 
 popd 1>/dev/null
