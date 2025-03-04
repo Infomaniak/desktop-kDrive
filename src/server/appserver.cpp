00c3a5b79a8fe82a44f385ffa99e5f6fd8d9f9a2
Luc Guyot <luc.guyot@infomaniak.com>
2025-03-04 17:41:33 +0100

    Fixes the Linux and Windows builds, plus some code smells (SonarCloud)

--- src/server/appserver.cpp
+++ src/server/appserver.cpp
@@ -1983,7 +1983,7 @@ void AppServer::onRequestReceived(int id, RequestNum num, const QByteArray &para
             QString tmp;
             QDataStream paramsStream(params);
             paramsStream >> tmp;
-            _updateManager->updater()->skipVersion(tmp.toStdString());
+            AbstractUpdater::skipVersion(tmp.toStdString());
             break;
         }
         default: {
