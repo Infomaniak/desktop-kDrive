148fc3ff475ee4b937ac78f406d996ef959817ff
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-03-10 16:29:33 +0100

    Address Sonar comment

--- src/libsyncengine/jobs/network/API_v2/csvfullfilelistwithcursorjob.cpp
+++ src/libsyncengine/jobs/network/API_v2/csvfullfilelistwithcursorjob.cpp
@@ -31,7 +31,7 @@ static const std::string endOfFileDelimiter("#EOF");
 
 namespace KDC {
 
-SnapshotItemHandler::SnapshotItemHandler(log4cplus::Logger logger) : _logger(logger) {}
+SnapshotItemHandler::SnapshotItemHandler(const log4cplus::Logger &logger) : _logger(logger) {}
 
 void SnapshotItemHandler::logError(const std::wstring &methodName, const std::wstring &stdErrorType, const std::string &str,
                                    const std::exception &exc) {
