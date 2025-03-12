84180f1e64426f3c06ab14e5d11bb7b5d14e73dd
Clément Kunz <clement.kunz@infomaniak.com>
2025-03-12 11:09:22 +0100

    KDESKTOP-1497 - Satisfying Sonar

--- src/server/requests/serverrequests.cpp
+++ src/server/requests/serverrequests.cpp
@@ -495,22 +495,22 @@ ExitCode ServerRequests::getUserAvailableDrives(int userDbId, QHash<int, DriveAv
 
     list.clear();
     for (size_t i = 0; i < dataArray->size(); i++) {
-        Poco::JSON::Object::Ptr obj = dataArray->getObject(static_cast<unsigned int>(i));
+        Poco::JSON::Object::Ptr obj = dataArray->getObject(i);
         if (!obj) {
             continue;
         }
 
-        int driveId;
+        int driveId = -1;
         if (!JsonParserUtility::extractValue(obj, driveIdKey, driveId)) {
             return ExitCode::BackError;
         }
 
-        int userId;
+        int userId = -1;
         if (!JsonParserUtility::extractValue(obj, idKey, userId)) {
             return ExitCode::BackError;
         }
 
-        int accountId;
+        int accountId = -1;
         if (!JsonParserUtility::extractValue(obj, accountIdKey, accountId)) {
             return ExitCode::BackError;
         }
@@ -521,8 +521,7 @@ ExitCode ServerRequests::getUserAvailableDrives(int userDbId, QHash<int, DriveAv
         }
 
         std::string colorHex;
-        Poco::JSON::Object::Ptr prefObj = obj->getObject(preferenceKey);
-        if (prefObj) {
+        if (Poco::JSON::Object::Ptr prefObj = obj->getObject(preferenceKey)) {
             if (!JsonParserUtility::extractValue(prefObj, colorKey, colorHex, false)) {
                 return ExitCode::BackError;
             }
@@ -532,7 +531,7 @@ ExitCode ServerRequests::getUserAvailableDrives(int userDbId, QHash<int, DriveAv
 
         // Search user in DB
         User user;
-        bool found;
+        bool found = false;
         if (!ParmsDb::instance()->selectUserByUserId(userId, user, found)) {
             LOG_WARN(Log::instance()->getLogger(), "Error in ParmsDb::selectUserByUserId");
             return ExitCode::DbError;
@@ -547,8 +546,8 @@ ExitCode ServerRequests::getUserAvailableDrives(int userDbId, QHash<int, DriveAv
     return ExitCode::Ok;
 }
 
-ExitCode ServerRequests::getSubFolders(int userDbId, int driveId, const QString &nodeId, QList<NodeInfo> &list,
-                                       bool withPath /*= false*/) {
+ExitCode ServerRequests::getSubFolders(const int userDbId, const int driveId, const QString &nodeId, QList<NodeInfo> &list,
+                                       const bool withPath /*= false*/) {
     std::shared_ptr<GetRootFileListJob> job = nullptr;
     if (nodeId.isEmpty()) {
         try {
@@ -560,8 +559,7 @@ ExitCode ServerRequests::getSubFolders(int userDbId, int driveId, const QString 
         }
     } else {
         try {
-            job = (std::shared_ptr<GetFileListJob>) std::make_shared<GetFileListJob>(userDbId, driveId, nodeId.toStdString(), 1,
-                                                                                     true);
+            job = std::make_shared<GetFileListJob>(userDbId, driveId, nodeId.toStdString(), 1, true);
         } catch (const std::exception &e) {
             LOG_WARN(Log::instance()->getLogger(), "Error in GetFileListJob::GetFileListJob for userDbId="
                                                            << userDbId << " driveId=" << driveId << " nodeId="
