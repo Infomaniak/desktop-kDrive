232cc5160faab3fd333a23960c33375539705a28
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-03-18 17:03:09 +0100

    Add logs

--- src/server/appserver.cpp
+++ src/server/appserver.cpp
@@ -193,6 +193,7 @@ AppServer::AppServer(int &argc, char **argv) :
     }
 
     // Clear old server errors
+    LOG_INFO(_logger, "Clear old server errors");
     if (clearErrors(0) != ExitCode::Ok) {
         LOG_WARN(_logger, "Error in AppServer::clearErrors");
         throw std::runtime_error("Unable to clear old errors.");
@@ -216,6 +217,7 @@ AppServer::AppServer(int &argc, char **argv) :
     }
 
     // Init KeyChainManager instance
+    LOG_INFO(_logger, "Init KeyChainManager instance");
     if (!KeyChainManager::instance()) {
         LOG_WARN(_logger, "Error in KeyChainManager::instance");
         throw std::runtime_error("Unable to initialize key chain manager.");
@@ -223,20 +225,24 @@ AppServer::AppServer(int &argc, char **argv) :
 
 #if defined(__unix__) && !defined(__APPLE__)
     // For access to keyring in order to promt authentication popup
+    LOG_INFO(_logger, "Test KeyChainManager");
     KeyChainManager::instance()->writeDummyTest();
     KeyChainManager::instance()->clearDummyTest();
 #endif
 
     // Init ParametersCache instance
+    LOG_INFO(_logger, "Init ParametersCache instance");
     if (!ParametersCache::instance()) {
         LOG_WARN(_logger, "Error in ParametersCache::instance");
         throw std::runtime_error("Unable to initialize parameters cache.");
     }
 
     // Setup translations
+    LOG_INFO(_logger, "Setup translations");
     CommonUtility::setupTranslations(this, ParametersCache::instance()->parameters().language());
 
     // Configure logger
+    LOG_INFO(_logger, "Configure logger");
     if (!Log::instance()->configure(ParametersCache::instance()->parameters().useLog(),
                                     ParametersCache::instance()->parameters().logLevel(),
                                     ParametersCache::instance()->parameters().purgeOldLogs())) {
@@ -244,7 +250,8 @@ AppServer::AppServer(int &argc, char **argv) :
         addError(Error(errId(), ExitCode::SystemError, ExitCause::Unknown));
     }
 
-    // Log usefull infomation
+    // Log usefull information
+    LOG_INFO(_logger, "Log usefull information");
     logUsefulInformation();
 
     // Init ExclusionTemplateCache instance
