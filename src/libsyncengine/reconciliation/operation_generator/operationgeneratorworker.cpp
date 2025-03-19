0203d0b4a5b622238cded337ae18a2bd6b3cd331
Herve Eruam <herve.eruam@infomaniak.com>
2025-03-19 10:23:23 +0100

    Fix log \"

--- src/libsyncengine/reconciliation/operation_generator/operationgeneratorworker.cpp
+++ src/libsyncengine/reconciliation/operation_generator/operationgeneratorworker.cpp
@@ -252,9 +252,9 @@ void OperationGeneratorWorker::generateMoveOperation(std::shared_ptr<Node> curre
     } else {
         if (ParametersCache::isExtendedLogEnabled()) {
             LOGW_SYNCPAL_DEBUG(_logger, L"Move operation "
-                                                << op->id() << L" to be propagated on " << op->targetSide() << L" replica from \""
+                                                << op->id() << L" to be propagated on " << op->targetSide() << L" replica from "
                                                 << Utility::formatSyncPath(currentNode->moveOriginInfos().path())
-                                                << L"\" to \"" << Utility::formatSyncPath(currentNode->getPath()) << L"\" (ID: "
+                                                << L" to " << Utility::formatSyncPath(currentNode->getPath()) << L" (ID: "
                                                 << Utility::s2ws(currentNode->id() ? currentNode->id().value() : "-1") << L")");
         }
     }
