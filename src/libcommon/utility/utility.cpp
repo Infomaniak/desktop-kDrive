cde2d8e7f6f3925167bb789152b15c218ff5517c
Herve Eruam <herve.eruam@infomaniak.com>
2025-03-28 16:48:57 +0100

    Add const specifier.

--- src/libcommon/utility/utility.cpp
+++ src/libcommon/utility/utility.cpp
@@ -464,8 +464,8 @@ bool CommonUtility::colorThresholdCheck(const int red, const int green, const in
 }
 
 SyncPath CommonUtility::relativePath(const SyncPath &rootPath, const SyncPath &path) {
-    auto pathStr = path.string();
-    auto rootStr = rootPath.string();
+    const auto pathStr = path.string();
+    const auto rootStr = rootPath.string();
     auto rootIt = rootStr.begin();
     auto pathIt = pathStr.begin();
     const auto rootItEnd = rootStr.end();
