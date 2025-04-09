8f21022f4ed043bbb8f863aff4979b44ff7ac7a4
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-04-09 11:37:09 +0200

    Fix issue

--- src/gui/customdialog.cpp
+++ src/gui/customdialog.cpp
@@ -51,6 +51,7 @@ CustomDialog::CustomDialog(const bool popup, QWidget *parent) :
     QDialog(parent), _backgroundColor(QColor()), _buttonIconColor(QColor()), _backgroundForcedColor(QColor()), _layout(nullptr) {
     setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::NoDropShadowWindowHint);
     setAttribute(Qt::WA_TranslucentBackground);
+    setAttribute(Qt::WA_Hover, true);
 
     setMinimumSize(windowMinimumSize);
 
