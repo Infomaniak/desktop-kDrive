58675a1d5b7afb1999b60d3893f89d756a167f9c
Clément Kunz <clement.kunz@infomaniak.com>
2025-05-06 08:35:58 +0200

    Fix copyrights

--- src/libsyncengine/jobs/network/API_v2/listing/abstractlistingjob.cpp
+++ src/libsyncengine/jobs/network/API_v2/listing/abstractlistingjob.cpp
@@ -1,20 +1,24 @@
-// Infomaniak kDrive - Desktop
-// Copyright (C) 2023-2025 Infomaniak Network SA
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "abstractlistingjob.h"
+
+#include <Poco/Net/HTTPRequest.h>
 
 namespace KDC {
 
@@ -22,7 +26,8 @@ AbstractListingJob::AbstractListingJob(const int driveDbId, const NodeSet& black
     AbstractListingJob(ApiType::Drive, driveDbId, blacklist) {}
 
 AbstractListingJob::AbstractListingJob(const ApiType apiType, const int driveDbId, const NodeSet& blacklist /*= {}*/) :
-    AbstractTokenNetworkJob(apiType, 0, 0, driveDbId, 0), _blacklist(blacklist) {
+    AbstractTokenNetworkJob(apiType, 0, 0, driveDbId, 0),
+    _blacklist(blacklist) {
     _httpMethod = Poco::Net::HTTPRequest::HTTP_GET;
 }
 
