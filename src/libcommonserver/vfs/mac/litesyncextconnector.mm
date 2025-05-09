c32334ba35035edf32ed5f07cb0a52db77fdb4e1
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-05-09 08:09:00 +0200

    Address comments

--- src/libcommonserver/vfs/mac/litesyncextconnector.mm
+++ src/libcommonserver/vfs/mac/litesyncextconnector.mm
@@ -1420,19 +1420,17 @@ bool LiteSyncExtConnector::vfsSetStatus(const QString &path, const QString &loca
 
             if (vfsStatus.isSyncing) {
                 {
-                    const std::lock_guard<std::mutex> lock(_mutex);
+                    const std::scoped_lock lock(_mutex);
                     _syncingFolders[parentPath].insert(path);
                 }
 
                 vfsSetStatus(parentPath, localSyncPath, VfsStatus({.isSyncing = vfsStatus.isSyncing, .isHydrated = vfsStatus.isHydrated, .progress = 100}));
             } else {
                 {
-                    const std::lock_guard<std::mutex> lock(_mutex);
+                    const std::scoped_lock lock(_mutex);
                     _syncingFolders[parentPath].remove(path);
                     if (_syncingFolders[parentPath].empty()) {
                         _syncingFolders.remove(parentPath);
-                        _mutex.unlock();
-
                         if (!vfsProcessDirStatus(parentPath, localSyncPath)) {
                             return false;
                         }
@@ -1445,7 +1443,7 @@ bool LiteSyncExtConnector::vfsSetStatus(const QString &path, const QString &loca
 }
 
 bool LiteSyncExtConnector::vfsCleanUpStatuses(const QString &localSyncPath) {
-    const std::lock_guard<std::mutex> lock(_mutex);
+    const std::scoped_lock lock(_mutex);
     QHashIterator<QString, QSet<QString>> it(_syncingFolders);
     while (it.hasNext()) {
         it.next();
