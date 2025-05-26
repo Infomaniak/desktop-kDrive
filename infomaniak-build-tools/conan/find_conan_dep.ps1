a03acd3c9eb8312f6d336f474d9daecb80823f88
Romain Galland <romain.galland@infomaniak.com>
2025-05-26 18:19:12 +0200

    fix(build): find conan exe

--- infomaniak-build-tools/conan/find_conan_dep.ps1
+++ infomaniak-build-tools/conan/find_conan_dep.ps1
@@ -5,6 +5,48 @@ param(
     [Parameter(Mandatory=$true)][string]$Version
 )
 
+function Get-ConanExePath {
+    try {
+        $cmd = Get-Command conan.exe -ErrorAction Stop
+        Log "Conan executable found at: $($cmd.Path)"
+        return $cmd.Path
+    } catch { }
+
+    try {
+        $pythonCmd = Get-Command python -ErrorAction Stop
+
+        $venvCheck = & $pythonCmd -c 'import sys; print(sys.prefix != sys.base_prefix)'
+        if($venvCheck.Trim().ToLower() -ne "true") {
+            Write-Warning "Python virtual environment not activated. It is recommended to install conan with a virtual env."
+        }
+    } catch {
+        Err "Interpreter 'python' not found. Please install Python 3 and/or add it to the PATH."
+        return $null
+    }
+
+    $pythonCode = @"
+import os, sys
+try:
+    import conans
+except ImportError:
+    sys.exit(1)
+modpath = conans.__file__
+prefix = modpath.split(os.sep + 'Lib' + os.sep)[0]
+exe = os.path.join(prefix, 'Scripts', 'conan.exe')
+print(exe)
+"@
+
+    $rawPath = & $pythonCmd.Path -c $pythonCode 2>$null
+    $exePath = $rawPath.Trim()
+    if ($LASTEXITCODE -ne 0 -or -not (Test-Path $exePath)) {
+        Err "Unable to locate 'conan.exe' via Python."
+        return $null
+    }
+
+    Log "Conan executable found at: $exePath"
+    return $exePath
+}
+
 $reference = "$Package/$Version"
 
 # Vérification de sqlite3
@@ -13,7 +55,8 @@ if (-not (Get-Command sqlite3 -ErrorAction SilentlyContinue)) {
     exit 1
 }
 
-$conan_home = & conan config home
+$conan_exe = Get-ConanExePath
+$conan_home = & $conan_exe config home
 $conan_db = Join-Path $conan_home "p\cache.sqlite3"
 
 if (-not (Test-Path $conan_db)) {
