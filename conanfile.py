6ce95e2340aa6ab7cf029eb605d892e9db3390d0
Romain Galland <romain.galland@infomaniak.com>
2025-05-27 17:41:26 +0200

    feat(openssl|zlib): add zlib dependency for macOS universal OpenSSL build

--- conanfile.py
+++ conanfile.py
@@ -59,6 +59,9 @@ class KDriveDesktop(ConanFile):
         self.requires("log4cplus/2.1.2", options=log4cplus_options) # From https://conan.io/center/recipes/log4cplus
 
         if self.settings.os == "Macos":
+            # openssl depends on zlib, its already inside the conanfile.py of openssl-universal
+            # but since we build openssl-universal two times (for x86_64 and arm64) in single arch and then merge them, we need to add zlib in 'armv8|x86_64' arch mode.
+            self.requires("zlib/[>=1.2.11 <2]", options={"shared": True}) # From https://conan.io/center/recipes/zlib
             # On macOS, we need to use the universal version of OpenSSL
             self.requires("openssl-universal/3.2.4")
         else:
