8b0a536b62bbb4db320ac89608b6d75b7e4d952a
Romain Galland <romain.galland@infomaniak.com>
2025-05-27 08:53:22 +0200

    fix(build): improve error handling and logging in conan dependency script

--- infomaniak-build-tools/conan/find_conan_dep.ps1
+++ infomaniak-build-tools/conan/find_conan_dep.ps1
@@ -11,17 +11,11 @@ function Err { Write-Error "[ERROR] $($args -join ' ')" ; exit 1 }
 function Get-ConanExePath {
     try {
         $cmd = Get-Command conan.exe -ErrorAction Stop
-        Log "Conan executable found at: $($cmd.Path)"
         return $cmd.Path
     } catch { }
 
     try {
         $pythonCmd = Get-Command python -ErrorAction Stop
-
-        $venvCheck = & $pythonCmd -c 'import sys; print(sys.prefix != sys.base_prefix)'
-        if($venvCheck.Trim().ToLower() -ne "true") {
-            Write-Warning "Python virtual environment not activated. It is recommended to install conan with a virtual env."
-        }
     } catch {
         Err "Interpreter 'python' not found. Please install Python 3 and/or add it to the PATH."
         return $null
@@ -45,26 +39,19 @@ print(exe)
         Err "Unable to locate 'conan.exe' via Python."
         return $null
     }
-
-    Log "Conan executable found at: $exePath"
     return $exePath
 }
 
 if ($CI) {
-    # Activate the python virtual environment.
-    & "C:\Program Files\Python313\.venv\Scripts\activate.ps1"
-
-    # Call vcvarsall.bat to set up the environment for MSVC
-    & "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
-    Log "CI mode enabled."
+    & "C:\Program Files\Python313\.venv\Scripts\activate.ps1"`
+    Write-Err "Running in CI mode, activating Python venv."
 }
 
 $reference = "$Package/$Version"
 
 # Vérification de sqlite3
 if (-not (Get-Command sqlite3 -ErrorAction SilentlyContinue)) {
-    Write-Error "Veuillez installer sqlite3 pour utiliser ce script."
-    exit 1
+    Err "Please install sqlite3 and add it to the PATH."
 }
 
 $conan_exe = Get-ConanExePath
@@ -72,8 +59,7 @@ $conan_home = & $conan_exe config home
 $conan_db = Join-Path $conan_home "p\cache.sqlite3"
 
 if (-not (Test-Path $conan_db)) {
-    Write-Error "Conan database not found at $conan_db. Please execute this script from the root of the project: ./infomaniak-build-tools/conan/build_dependencies.ps1"
-    exit 1
+    Err "Conan database not found at $conan_db. Please execute this script from the root of the project: ./infomaniak-build-tools/conan/build_dependencies.ps1"
 }
 
 $sql_req = "SELECT path FROM packages WHERE reference='$reference' ORDER BY timestamp DESC LIMIT 1; .exit"
@@ -81,8 +67,11 @@ $sql_req = "SELECT path FROM packages WHERE reference='$reference' ORDER BY time
 $subpath = & sqlite3 $conan_db $sql_req 2>$null
 
 if (-not $subpath) {
-    Write-Error "Paquet $reference non trouvé dans le cache Conan."
-    exit 1
+    Err "The reference '$reference' does not exists in the conan cache.."
 }
 
-Write-Output (Join-Path $conan_home "p\$subpath\p")
+if (-not (Test-Path (Join-Path $conan_home "p\$subpath\p"))) {
+    Err "The path to the folder found, does not exists. : $(Join-Path $conan_home "p\$subpath\p")"
+} else {
+    Write-Output (Join-Path $conan_home "p\$subpath\p")
+}
