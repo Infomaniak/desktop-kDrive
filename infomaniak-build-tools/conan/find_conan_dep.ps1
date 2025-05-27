4efcc571431f73af772eb10d71f082e9077371d7
Romain Galland <romain.galland@infomaniak.com>
2025-05-27 10:03:22 +0200

    Add license information to find_conan_dep.ps1

--- infomaniak-build-tools/conan/find_conan_dep.ps1
+++ infomaniak-build-tools/conan/find_conan_dep.ps1
@@ -1,5 +1,20 @@
-# infomaniak-build-tools/conan/find_conan_dep.ps1
+<#
+ Infomaniak kDrive - Desktop App
+ Copyright (C) 2023-2025 Infomaniak Network SA
 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
 param(
     [Parameter(Mandatory=$true)][string]$Package,
     [Parameter(Mandatory=$true)][string]$Version,
