cae15b46166cfa43824117b3d1c49d9e82102a7c
Herve Eruam <82284536+herve-er@users.noreply.github.com>
2025-05-27 16:58:43 +0200

    Update src/libsyncengine/syncpal/operationprocessor.h

--- src/libsyncengine/syncpal/operationprocessor.h
+++ src/libsyncengine/syncpal/operationprocessor.h
@@ -30,7 +30,6 @@ class OperationProcessor : public ISyncWorker {
 
     protected:
         // Return false if only elements that are not synced with the corresponding side change (e.g., creation date). Else return
-        // true.
         bool editChangeShouldBePropagated(std::shared_ptr<Node> affectedNode);
         
         bool isPseudoConflict(std::shared_ptr<Node> node, std::shared_ptr<Node> correspondingNode);
