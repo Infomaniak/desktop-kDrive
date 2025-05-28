58cfb2ff5d5a0dae932f35cb5479ce9fc404c1fd
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-05-28 16:37:36 +0200

    Debugging

--- infomaniak-build-tools/run-tests.sh
+++ infomaniak-build-tools/run-tests.sh
@@ -25,7 +25,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib
+export DYLD_LIBRARY_PATH=/usr/local/lib:$DYLD_LIBRARY_PATH
 
 testers=$(find . -type f -name "kDrive_test_*")
 errors=0
