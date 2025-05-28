60515cc8d80e3cca28f80825a5c94c5bb947160a
Herve Eruam <herve.eruam@infomaniak.com>
2025-05-28 10:04:56 +0200

    Add log verbose.

--- test/server/appserver/testappserver.cpp
+++ test/server/appserver/testappserver.cpp
@@ -31,60 +31,88 @@ namespace KDC {
 
 void TestAppServer::setUp() {
     TestBase::start();
-
+    std::cout << "1" << std::endl;
     if (QCoreApplication::instance()) {
         _appPtr = dynamic_cast<MockAppServer *>(QCoreApplication::instance());
         return;
     }
+    std::cout << "2" << std::endl;
 
     const testhelpers::TestVariables testVariables;
 
     const std::string localPathStr = _localTempDir.path().string();
+    std::cout << "3" << std::endl;
 
     // Insert api token into keystore
     ApiToken apiToken;
     apiToken.setAccessToken(testVariables.apiToken);
+    std::cout << "4" << std::endl;
 
     const std::string keychainKey("123");
     (void) KeyChainManager::instance(true);
     (void) KeyChainManager::instance()->writeToken(keychainKey, apiToken.reconstructJsonString());
+    std::cout << "5" << std::endl;
 
     // Create parmsDb
     bool alreadyExists = false;
     const std::filesystem::path parmsDbPath = MockDb::makeDbName(alreadyExists);
     ParmsDb::instance(parmsDbPath, KDRIVE_VERSION_STRING, false, true);
     ParametersCache::instance()->parameters().setExtendedLog(true);
+    std::cout << "6" << std::endl;
 
     // Insert user, account, drive & sync
     const int userId(atoi(testVariables.userId.c_str()));
     const User user(1, userId, keychainKey);
     (void) ParmsDb::instance()->insertUser(user);
+    std::cout << "7" << std::endl;
 
     const int accountId(atoi(testVariables.accountId.c_str()));
     const Account account(1, accountId, user.dbId());
     (void) ParmsDb::instance()->insertAccount(account);
+    std::cout << "8" << std::endl;
 
     const int driveId = atoi(testVariables.driveId.c_str());
     const Drive drive(1, driveId, account.dbId(), std::string(), 0, std::string());
     (void) ParmsDb::instance()->insertDrive(drive);
+    std::cout << "9" << std::endl;
 
     _localPath = localPathStr;
     _remotePath = testVariables.remotePath;
     Sync sync(1, drive.dbId(), _localPath, _remotePath);
+    std::cout << "10" << std::endl;
+
     (void) ParmsDb::instance()->insertSync(sync);
+    std::cout << "11" << std::endl;
 
     ParmsDb::instance()->close();
+    std::cout << "12" << std::endl;
+
     ParmsDb::reset();
+    std::cout << "13" << std::endl;
 
     // Create AppServer
     SyncPath exePath = KDC::CommonUtility::applicationFilePath();
     try {
+        std::cout << "14" << std::endl;
+
         const std::vector<std::string> args = {Path2Str(exePath)};
+        std::cout << "15" << std::endl;
+
         std::vector<char *> argv;
+        std::cout << "16" << std::endl;
+
         for (size_t i = 0; i < args.size(); ++i) argv.push_back(const_cast<char *>(args[i].c_str()));
+        std::cout << "17" << std::endl;
+
         auto argc = static_cast<int>(args.size());
+        std::cout << "18" << std::endl;
+
         _appPtr = new MockAppServer(argc, &argv[0]);
+        std::cout << "19" << std::endl;
+
         _appPtr->setParmsDbPath(parmsDbPath);
+        std::cout << "20" << std::endl;
+
         _appPtr->init();
     } catch (const std::exception &e) {
         std::cerr << "kDrive server initialization error: " << e.what() << std::endl;
@@ -102,39 +130,39 @@ void TestAppServer::testInitAndStopSyncPal() {
     const int syncDbId = 1;
 
     Sync sync;
-  //  bool found = false;
-  //  CPPUNIT_ASSERT(ParmsDb::instance()->selectSync(syncDbId, sync, found) && found);
+    bool found = false;
+    CPPUNIT_ASSERT(ParmsDb::instance()->selectSync(syncDbId, sync, found) && found);
 
     // Check sync nesting
-  //  ExitInfo exitInfo = _appPtr->checkIfSyncIsValid(sync);
-  //  CPPUNIT_ASSERT(exitInfo);
+    ExitInfo exitInfo = _appPtr->checkIfSyncIsValid(sync);
+    CPPUNIT_ASSERT(exitInfo);
     // Start Vfs
-  //  exitInfo = _appPtr->createAndStartVfs(sync);
-  //  CPPUNIT_ASSERT(exitInfo);
+    exitInfo = _appPtr->createAndStartVfs(sync);
+    CPPUNIT_ASSERT(exitInfo);
     // Start SyncPal
-  //  const std::chrono::seconds startDelay{0};
-  //  exitInfo = _appPtr->initSyncPal(sync, QSet<QString>(), QSet<QString>(), QSet<QString>(), /*start*/ true, startDelay,
-                                    /*resumedByUser*/// false, /*firstInit*/ true);
-   // CPPUNIT_ASSERT(exitInfo);
-   // CPPUNIT_ASSERT(syncIsActive(syncDbId));
+    const std::chrono::seconds startDelay{0};
+    exitInfo = _appPtr->initSyncPal(sync, QSet<QString>(), QSet<QString>(), QSet<QString>(), /*start*/ true, startDelay,
+                                    /*resumedByUser*/ false, /*firstInit*/ true);
+    CPPUNIT_ASSERT(exitInfo);
+    CPPUNIT_ASSERT(syncIsActive(syncDbId));
     // Stop SyncPal (pause by user)
-  //  exitInfo = _appPtr->stopSyncPal(syncDbId, /*pausedByUser*/ true);
-  //  CPPUNIT_ASSERT(exitInfo);
-  //  CPPUNIT_ASSERT(waitForSyncStatus(syncDbId, SyncStatus::Stopped));
+    exitInfo = _appPtr->stopSyncPal(syncDbId, /*pausedByUser*/ true);
+    CPPUNIT_ASSERT(exitInfo);
+    CPPUNIT_ASSERT(waitForSyncStatus(syncDbId, SyncStatus::Stopped));
     // Resume SyncPal
-  //  exitInfo = _appPtr->initSyncPal(sync, QSet<QString>(), QSet<QString>(), QSet<QString>(), /*start*/ true, startDelay,
-                                    /*resumedByUser*/ //true, /*firstInit*/ false);
-  //  CPPUNIT_ASSERT(exitInfo);
-  //  CPPUNIT_ASSERT(syncIsActive(syncDbId));
-//
-   // // Stop SyncPal (cleanup)
-   // exitInfo = _appPtr->stopSyncPal(syncDbId, /*pausedByUser*/ false, /*quit*/ true, /*clear*/ true);
-   // CPPUNIT_ASSERT(exitInfo);
-   // CPPUNIT_ASSERT(waitForSyncStatus(syncDbId, SyncStatus::Stopped));
-//
-    //// Stop Vfs
-   // exitInfo = _appPtr->stopVfs(syncDbId, /*unregister*/ false);
-   // CPPUNIT_ASSERT(exitInfo);
+    exitInfo = _appPtr->initSyncPal(sync, QSet<QString>(), QSet<QString>(), QSet<QString>(), /*start*/ true, startDelay,
+                                    /*resumedByUser*/ true, /*firstInit*/ false);
+    CPPUNIT_ASSERT(exitInfo);
+    CPPUNIT_ASSERT(syncIsActive(syncDbId));
+
+    // Stop SyncPal (cleanup)
+    exitInfo = _appPtr->stopSyncPal(syncDbId, /*pausedByUser*/ false, /*quit*/ true, /*clear*/ true);
+    CPPUNIT_ASSERT(exitInfo);
+    CPPUNIT_ASSERT(waitForSyncStatus(syncDbId, SyncStatus::Stopped));
+
+    // Stop Vfs
+    exitInfo = _appPtr->stopVfs(syncDbId, /*unregister*/ false);
+    CPPUNIT_ASSERT(exitInfo);
 }
 
 void TestAppServer::testStartAndStopSync() {
