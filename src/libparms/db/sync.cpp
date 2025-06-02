f69a45dae482fded472400ae5c02880a4be9c7e4
Herve Eruam <herve.eruam@infomaniak.com>
2025-06-02 17:10:53 +0200

    Add localNodeId in parmsDb

--- src/libparms/db/sync.cpp
+++ src/libparms/db/sync.cpp
@@ -24,6 +24,7 @@ Sync::Sync() :
     _dbId(0),
     _driveDbId(0),
     _localPath(std::filesystem::path()),
+    _localNodeId(NodeId()),
     _targetPath(std::filesystem::path()),
     _targetNodeId(NodeId()),
     _paused(false),
@@ -36,13 +37,15 @@ Sync::Sync() :
     _listingCursor(std::string()),
     _listingCursorTimestamp(0) {}
 
-Sync::Sync(int dbId, int driveDbId, const std::filesystem::path &localPath, const std::filesystem::path &targetPath,
-           const NodeId &targetNodeId, bool paused, bool supportVfs, VirtualFileMode virtualFileMode, bool notificationsDisabled,
-           const std::filesystem::path &dbPath, bool hasFullyCompleted, const std::string &navigationPaneClsid,
-           const std::string &listingCursor, int64_t listingCursorTimestamp) :
+Sync::Sync(int dbId, int driveDbId, const std::filesystem::path &localPath, const NodeId &localNodeId,
+           const std::filesystem::path &targetPath, const NodeId &targetNodeId, bool paused, bool supportVfs,
+           VirtualFileMode virtualFileMode, bool notificationsDisabled, const std::filesystem::path &dbPath,
+           bool hasFullyCompleted, const std::string &navigationPaneClsid, const std::string &listingCursor,
+           int64_t listingCursorTimestamp) :
     _dbId(dbId),
     _driveDbId(driveDbId),
     _localPath(localPath),
+    _localNodeId(localNodeId),
     _targetPath(targetPath),
     _targetNodeId(targetNodeId),
     _paused(paused),
