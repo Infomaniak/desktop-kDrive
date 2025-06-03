2017d87f9ed617cde2f9e5e360bd1376df6049a6
Romain Galland <romain.galland@infomaniak.com>
2025-06-03 11:51:59 +0200

    refactor(build-release-amd64.sh): update OpenSSL integration to use Conan for dependency management to match the `build-ci-amd64.sh` script.

--- infomaniak-build-tools/linux/build-release-amd64.sh
+++ infomaniak-build-tools/linux/build-release-amd64.sh
@@ -117,14 +117,9 @@ build_release() {
     exit 1
   fi
 
-  openssl_folder="$(base "$BASEPATH/infomaniak-build-tools/conan/find_conan_dep.sh" openssl 3.2.4 2>/dev/null)"
-  if [ -z "$openssl_folder" ]; then
-    echo "OpenSSL folder not found. Please ensure the OpenSSL package is built and available in the conan cache." >&2; exit 1
-  else
-    echo "OpenSSL folder found: $openssl_folder"
-  fi
 
-  source "$conan_generator_folder/conanrun.sh"
+
+  source "$conan_generator_folder/conanbuild.sh"
 
   # Set defaults
   export SUFFIX=""
@@ -138,10 +133,6 @@ build_release() {
   export KDRIVE_DEBUG=0
 
   cmake -B$build_dir/client -H$src_dir \
-      -DOPENSSL_ROOT_DIR="$openssl_folder" \
-      -DOPENSSL_INCLUDE_DIR="$openssl_folder/include" \
-      -DOPENSSL_CRYPTO_LIBRARY="$openssl_folder/lib/libcrypto.so" \
-      -DOPENSSL_SSL_LIBRARY="$openssl_folder/lib/libssl.so" \
       -DQT_FEATURE_neon=OFF \
       -DCMAKE_BUILD_TYPE=$build_type \
       -DCMAKE_INSTALL_PREFIX=/usr \
@@ -149,7 +140,7 @@ build_release() {
       -DKDRIVE_VERSION_SUFFIX=$SUFFIX \
       -DKDRIVE_THEME_DIR="$src_dir/infomaniak" \
       -DKDRIVE_VERSION_BUILD="$(date +%Y%m%d)" \
-      -DCONAN_DEP_DIR="$conan_dependencies_folder"
+      -DCONAN_DEP_DIR="$conan_dependencies_folder" \
       -DCMAKE_TOOLCHAIN_FILE="$conan_toolchain_file" \
       "${CMAKE_PARAMS[@]}" \
 
@@ -188,9 +179,6 @@ package_release() {
 
   mv $app_dir/usr/lib/x86_64-linux-gnu/* $app_dir/usr/lib/ || echo "The folder $app_dir/usr/lib/x86_64-linux-gnu/ might not exist." >&2
   cp -P "$conan_dependencies_folder/*" "$app_dir/usr/lib"
-
-  cp -P "$openssl_folder"/lib/libssl.so* $app_dir/usr/lib/
-  cp -P "$openssl_folder"/lib/libcrypto.so* $app_dir/usr/lib/
 
   cp -P -r /usr/lib/x86_64-linux-gnu/nss/ $app_dir/usr/lib/
 
