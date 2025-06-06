906c5e8444d62eff31306f712e6f855bdd330b2c
Herve Eruam <herve.eruam@infomaniak.com>
2025-06-06 12:18:20 +0200

    Fix sonar issue.

--- test/libcommonserver/io/testcheckifisdirectory.cpp
+++ test/libcommonserver/io/testcheckifisdirectory.cpp
@@ -268,6 +268,17 @@ void TestIo::testCreateDirectory() {
         CPPUNIT_ASSERT_EQUAL_MESSAGE(toString(ioError) + "!=" + toString(IoError::Success), IoError::Success, ioError);
     }
 
+    // Fails to create a directory because the parent path does not exists and recursive option is set to false.
+    {
+        const LocalTemporaryDirectory temporaryDirectory;
+        const SyncPath path = temporaryDirectory.path() / "non_existing_directory" / "subdir";
+
+        IoError ioError = IoError::Unknown;
+        CPPUNIT_ASSERT(!_testObj->createDirectory(path, false, ioError));
+        CPPUNIT_ASSERT_EQUAL_MESSAGE(toString(ioError) + "!=" + toString(IoError::NoSuchFileOrDirectory),
+                                     IoError::NoSuchFileOrDirectory, ioError);
+    }
+
     // Creates successfully a directory with missing parent when recursive option is set to true
     {
         const LocalTemporaryDirectory temporaryDirectory;
@@ -283,17 +294,6 @@ void TestIo::testCreateDirectory() {
         CPPUNIT_ASSERT(_testObj->checkIfIsDirectory(path, isDirectory, ioError));
         CPPUNIT_ASSERT(isDirectory);
         CPPUNIT_ASSERT_EQUAL_MESSAGE(toString(ioError) + "!=" + toString(IoError::Success), IoError::Success, ioError);
-    }
-
-    // Fails to create a directory because the parent path does not exists and recursive option is set to false.
-    {
-        const LocalTemporaryDirectory temporaryDirectory;
-        const SyncPath path = temporaryDirectory.path() / "non_existing_directory" / "subdir";
-
-        IoError ioError = IoError::Unknown;
-        CPPUNIT_ASSERT(!_testObj->createDirectory(path, false, ioError));
-        CPPUNIT_ASSERT_EQUAL_MESSAGE(toString(ioError) + "!=" + toString(IoError::NoSuchFileOrDirectory),
-                                     IoError::NoSuchFileOrDirectory, ioError);
     }
 
     // Fails to create a directory because the dir path indicates an existing directory
