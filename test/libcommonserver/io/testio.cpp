167358f4fb2fdf6e16f08757421f8a55cd185901
Herve Eruam <herve.eruam@infomaniak.com>
2025-06-18 15:44:04 +0200

    Fix tests.

--- test/libcommonserver/io/testio.cpp
+++ test/libcommonserver/io/testio.cpp
@@ -335,16 +335,11 @@ void TestIo::testSetFileDates() {
         CPPUNIT_ASSERT_EQUAL(IoError::Success, ioError);
 
         (void) IoHelper::getFileStat(filepath, &filestat, ioError);
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(_WIN32)
         // Creation date is set to modification date = 0
         CPPUNIT_ASSERT(filestat.creationTime == 0);
-        CPPUNIT_ASSERT(filestat.modificationTime == 0);
-#elif defined(_WIN32)
-        CPPUNIT_ASSERT_EQUAL(timestamp, filestat.creationTime);
-        CPPUNIT_ASSERT_GREATER(timestamp, filestat.modificationTime);
-#else
-        CPPUNIT_ASSERT(filestat.modificationTime == 0);
 #endif
+        CPPUNIT_ASSERT(filestat.modificationTime == 0);
     }
 
     // Test on a non-existing file.
