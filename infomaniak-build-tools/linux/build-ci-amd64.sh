c23f0977ccc4fb058c800226336fd33208902784
Romain Galland <romain.galland@infomaniak.com>
2025-06-26 11:16:32 +0200

    fix(build-scripts): remove unused Conan dependencies folder variable in Linux CI script
    This variable is only used when building the `.AppImage`, which is not the case in CI.

--- infomaniak-build-tools/linux/build-ci-amd64.sh
+++ infomaniak-build-tools/linux/build-ci-amd64.sh
@@ -81,7 +81,7 @@ export QT_BASE_DIR="$HOME/Qt/6.2.3"
 export QTDIR="$QT_BASE_DIR/gcc_64"
 export BASEPATH="$PWD"
 export CONTENTDIR="$BASEPATH/build-linux"
-export BUILD_DIR="$CONTENTDIR/build"
+export build_dir="$CONTENTDIR/build"
 export APPDIR="$CONTENTDIR/app"
 
 extract_debug () {
@@ -91,7 +91,7 @@ extract_debug () {
 }
 
 mkdir -p "$APPDIR"
-mkdir -p "$BUILD_DIR"
+mkdir -p "$build_dir"
 
 export QMAKE="$QTDIR/bin/qmake"
 export PATH="$QTDIR/bin:$QTDIR/libexec:/home/runner/.local/bin:$PATH"
@@ -101,8 +101,9 @@ export PKG_CONFIG_PATH="$QTDIR/lib/pkgconfig:$PKG_CONFIG_PATH"
 # Set defaults
 export SUFFIX=""
 
-conan_build_folder="$BUILD_DIR/conan"
-conan_dependencies_folder="$BUILD_DIR/conan/dependencies"
+mkdir -p "$build_dir/client"
+
+conan_build_folder="$build_dir/conan"
 
 bash "$BASEPATH/infomaniak-build-tools/conan/build_dependencies.sh" "$build_type" --output-dir="$conan_build_folder"
 
@@ -115,23 +116,22 @@ if [ ! -f "$conan_toolchain_file" ]; then
 fi
 
 # Build client
-cd "$BUILD_DIR"
+cd "$build_dir"
 
 source "$conan_generator_folder/conanbuild.sh"
 
 export KDRIVE_DEBUG=0
 
-cmake -B"$BUILD_DIR" -H"$BASEPATH" \
+cmake -B"$build_dir" -H"$BASEPATH" \
     -DQT_FEATURE_neon=OFF \
     -DCMAKE_BUILD_TYPE="$build_type" \
     -DCMAKE_PREFIX_PATH="$BASEPATH" \
     -DCMAKE_INSTALL_PREFIX=/usr \
-    -DBIN_INSTALL_DIR="$BUILD_DIR/bin" \
+    -DBIN_INSTALL_DIR="$build_dir/client" \
     -DKDRIVE_VERSION_SUFFIX="$SUFFIX" \
     -DKDRIVE_THEME_DIR="$BASEPATH/infomaniak" \
     -DKDRIVE_VERSION_BUILD="$(date +%Y%m%d)" \
     -DBUILD_UNIT_TESTS=$unit_tests \
-    -DCONAN_DEP_DIR="$conan_dependencies_folder" \
     -DCMAKE_TOOLCHAIN_FILE="$conan_toolchain_file" \
 
 make "-j$(nproc)"
@@ -141,4 +141,4 @@ extract_debug ./bin kDrive_client
 
 make DESTDIR="$APPDIR" install
 
-cp "$BASEPATH/sync-exclude-linux.lst" "$BUILD_DIR/bin/sync-exclude.lst"
+cp "$BASEPATH/sync-exclude-linux.lst" "$build_dir/bin/sync-exclude.lst"
