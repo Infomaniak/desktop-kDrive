086f15c6ad39240c0a22565d458e16a4fe38a458
Herve Eruam <herve.eruam@infomaniak.com>
2025-06-30 10:36:02 +0200

    ci(auto-release-windows): Upload installer

---
+++ infomaniak-build-tools/upload-ci.ps1
@@ -0,0 +1,51 @@
+<#
+ Infomaniak kDrive - Desktop App
+ Copyright (C) 2023-2024 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
+if (-not $env:KDRIVE_TOKEN) {
+    Write-Host "No token found to upload to kDrive." -f Red 
+    exit 1
+}
+
+$version = (Select-String -Path .\build-windows\build\version.h KDRIVE_VERSION_FULL | foreach-object { $data = $_ -split " "; echo $data[3]})
+$app = "kDrive-$version"
+Set-Location build-windows
+
+$headers = @{
+    Authorization="Bearer $env:KDRIVE_TOKEN"
+}
+
+$mainVersion = $version.Substring(0, 3)
+$minorVersion = $version.Substring(0, 5)
+$date = $version.Substring(6)
+
+$win_files = @(
+    $app".exe",
+    "kDrive.pdb",
+    "kDrive_client.pdb",
+    "kDrivecommonserver_vfs_win.pdb",
+
+)
+foreach ($file in $win_files)
+{
+    $size = (Get-ChildItem $file | % {[int]($_.length)})
+    $uri = "https://api.infomaniak.com/3/drive/$env:KDRIVE_ID/upload?directory_id=$env:KDRIVE_DIR_ID&total_size=$size&file_name=$file&directory_path=$minorVersion/$date/windows&conflict=version"
+
+    Invoke-RestMethod -Method "POST" -Uri $uri -Header $headers -ContentType 'application/octet-stream' -InFile $file
+}
+
+# TODO add Linux and macOS uploads
