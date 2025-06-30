cf3320b98be3e43cc3815433d1950074e24d135a
Romain Galland <romain.galland@infomaniak.com>
2025-06-30 11:08:41 +0200

    fix(matomo): move Matomo timeout and disable flags from .h to anonymous namespace in .cpp for better encapsulation

--- src/libcommongui/matomoclient.h
+++ src/libcommongui/matomoclient.h
@@ -92,11 +92,6 @@ namespace KDC {
             std::unordered_map<MatomoNameField, std::pair<QString, QString>> _nameFieldMap;
             void initNameFieldMap();
             void getPathAndAction(MatomoNameField name, QString &path, QString &action) const;
-
-            static constexpr int _matomoTimeout = 10000;  // Timeout for Matomo requests in milliseconds
-            int _matomoTimeoutCounter = 0;              // Counter to avoid sending too many Matomo requests in a short time
-            static constexpr int _matomoTimeoutMax = 2; // Maximum number of Matomo requests allowed
-            bool _matomoDisabled = false;               // if true, Matomo is disabled and no events / page visit will be sent.
     };
 
 } // namespace KDC
