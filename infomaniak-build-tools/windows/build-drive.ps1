7b8af07a9a770733c94ad7d5501e7af3053938ca
Romain Galland <romain.galland@infomaniak.com>
2025-06-30 09:59:08 +0200

    feat(build): Add -MakeRelease parameter to build_dependencies.ps1 script

--- infomaniak-build-tools/windows/build-drive.ps1
+++ infomaniak-build-tools/windows/build-drive.ps1
@@ -211,7 +211,7 @@ function CMake-Build-And-Install {
     if ($ci) {
         & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" Release -OutputDir $conanFolder -Ci
     } else {
-        & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" Release -OutputDir $conanFolder
+        & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" Release -OutputDir $conanFolder -MakeRelease
     }
 
     $conanToolchainFile = Get-ChildItem -Path $conanFolder -Filter "conan_toolchain.cmake" -Recurse -File |
