551c637c9199704a2283566c71630acd3042a27d
Romain Galland <romain.galland@infomaniak.com>
2025-07-01 16:18:00 +0200

    fix(build): add buildType parameter to script for dynamic dependency installation

--- infomaniak-build-tools/windows/build-drive.ps1
+++ infomaniak-build-tools/windows/build-drive.ps1
@@ -199,7 +199,8 @@ function CMake-Build-And-Install {
     param (
         [string] $path,
         [string] $installPath,
-        [string] $vfsDir
+        [string] $vfsDir,
+        [string] $buildType
     )
     Write-Host "1) Installing Conan dependencies…"
     $conanFolder = Join-Path $buildPath "conan"
@@ -209,9 +210,9 @@ function CMake-Build-And-Install {
     }
     Write-Host "Conan folder: $conanFolder"
     if ($ci) {
-        & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" Release -OutputDir $conanFolder -Ci
+        & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" $buildType -OutputDir $conanFolder -Ci
     } else {
-        & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" Release -OutputDir $conanFolder
+        & "$path\infomaniak-build-tools\conan\build_dependencies.ps1" $buildType -OutputDir $conanFolder
     }
 
     $conanToolchainFile = Get-ChildItem -Path $conanFolder -Filter "conan_toolchain.cmake" -Recurse -File |
@@ -655,7 +656,7 @@ if (!(Test-Path "$vfsDir\vfs.dll") -or $ext) {
 #                                                                                               #
 #################################################################################################
 
-CMake-Build-And-Install $path $installPath $vfsDir
+CMake-Build-And-Install $path $installPath $vfsDir $buildType
 
 if ($LASTEXITCODE -ne 0) {
     Write-Host "CMake build failed. Aborting." -f Red
