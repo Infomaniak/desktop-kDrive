dff9c98ac1bbd40ac29544f491ff83e09b56d021
Romain Galland <romain.galland@infomaniak.com>
2025-07-07 09:40:17 +0200

    test-ci(conan): restore origin recipe.

--- infomaniak-build-tools/conan/recipes/cppunit/all/conanfile-windows.py
+++ infomaniak-build-tools/conan/recipes/cppunit/all/conanfile-windows.py
@@ -1,9 +1,8 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.apple import is_apple_os, fix_apple_shared_install_name
 from conan.tools.build import stdcpp_library
 from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import copy, get, rename, rm, rmdir, replace_in_file
+from conan.tools.files import copy, get, rename, rm, rmdir
 from conan.tools.gnu import Autotools, AutotoolsToolchain
 from conan.tools.layout import basic_layout
 from conan.tools.microsoft import check_min_vs, is_msvc, unix_path
@@ -14,7 +13,6 @@ required_conan_version = ">=1.57.0"
 
 class CppunitConan(ConanFile):
     name = "cppunit"
-    version = "1.15.1"
     description = (
         "CppUnit is the C++ port of the famous JUnit framework for unit testing. "
         "Test output is in XML for automatic testing and GUI based for supervised tests."
@@ -34,16 +32,13 @@ class CppunitConan(ConanFile):
         "fPIC": True,
     }
 
-    def validate(self):
-        if not self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("This custom cppunit recipe is only supported on Windows.")
-
     @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
     def config_options(self):
-        del self.options.fPIC
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
         if self.options.shared:
@@ -53,9 +48,10 @@ class CppunitConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def build_requirements(self):
-        self.win_bash = True
-        if not self.conf.get("tools.microsoft.bash:path", check_type=str):
-            self.tool_requires("msys2/cci.latest")
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.tool_requires("msys2/cci.latest")
         if is_msvc(self):
             self.tool_requires("automake/1.16.5")
 
@@ -67,10 +63,10 @@ class CppunitConan(ConanFile):
         env.generate()
 
         tc = AutotoolsToolchain(self)
-        if self.options.shared:
+        if self.settings.os == "Windows" and self.options.shared:
             tc.extra_defines.append("CPPUNIT_BUILD_DLL")
         if is_msvc(self):
-            tc.extra_cxxflags.extend([ "-EHsc", "-std=c++20" ])
+            tc.extra_cxxflags.append("-EHsc")
             if check_min_vs(self, "180", raise_invalid=False):
                 tc.extra_cflags.append("-FS")
                 tc.extra_cxxflags.append("-FS")
@@ -82,16 +78,15 @@ class CppunitConan(ConanFile):
             "--enable-debug={}".format(yes_no(self.settings.build_type == "Debug")),
             "--enable-doxygen=no",
             "--enable-dot=no",
-            "--disable-werror",
+            "--enable-werror=no",
             "--enable-html-docs=no",
         ])
-        tc.extra_cxxflags.append("-Wno-error")
         env = tc.environment()
         if is_msvc(self):
             compile_wrapper = unix_path(self, self.conf.get("user.automake:compile-wrapper", check_type=str))
             ar_wrapper = unix_path(self, self.conf.get("user.automake:lib-wrapper", check_type=str))
-            env.define("CC", f"{compile_wrapper} cl -nologo -std=c++20")
-            env.define("CXX", f"{compile_wrapper} cl -nologo -std=c++20")
+            env.define("CC", f"{compile_wrapper} cl -nologo")
+            env.define("CXX", f"{compile_wrapper} cl -nologo")
             env.define("LD", "link -nologo")
             env.define("AR", f"{ar_wrapper} \"lib -nologo\"")
             env.define("NM", "dumpbin -symbols")
@@ -120,9 +115,11 @@ class CppunitConan(ConanFile):
     def package_info(self):
         self.cpp_info.set_property("pkg_config_name", "cppunit")
         self.cpp_info.libs = ["cppunit"]
-        if self.options.shared:
-            self.cpp_info.defines.append("CPPUNIT_DLL")
-        else:
+        if not self.options.shared:
             libcxx = stdcpp_library(self)
             if libcxx:
                 self.cpp_info.system_libs.append(libcxx)
+            if self.settings.os in ["Linux", "FreeBSD"]:
+                self.cpp_info.system_libs.extend(["dl", "m"])
+        if self.options.shared and self.settings.os == "Windows":
+            self.cpp_info.defines.append("CPPUNIT_DLL")
