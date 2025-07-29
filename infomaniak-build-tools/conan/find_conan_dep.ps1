442c3fa98b4d31858056867a1a868b0aabe7fd9f
Romain Galland <romain.galland@infomaniak.com>
2025-07-29 15:47:51 +0200

    fix(find_conan_dep.ps1): correct parameter definition for BuildDir
    Updated the BuildDir parameter to remove the default value, ensuring it is mandatory for script execution.

--- infomaniak-build-tools/conan/find_conan_dep.ps1
+++ infomaniak-build-tools/conan/find_conan_dep.ps1
@@ -16,7 +16,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #>
 param(
-    [Parameter(Mandatory=$true)][switch]$BuildDir = "",
+    [Parameter(Mandatory=$true)][switch]$BuildDir,
     [Parameter(Mandatory=$true)][string]$Package
 )
 
