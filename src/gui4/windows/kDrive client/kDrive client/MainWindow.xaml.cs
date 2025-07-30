156746e0dcc607cb4a53ce4c455d93e016ebeadb
Herve Eruam <herve.eruam@infomaniak.com>
2025-07-30 16:48:35 +0200

    feat: Update MainWindow with NavigationView
    Significantly modified `MainWindow.xaml` and `MainWindow.xaml.cs` to remove language selection UI elements.
    Added a `NavigationView` with custom content, including a logo and dropdown for kDrive options.

--- src/gui4/windows/kDrive client/kDrive client/MainWindow.xaml.cs
+++ src/gui4/windows/kDrive client/kDrive client/MainWindow.xaml.cs
@@ -26,60 +26,6 @@ namespace kDrive_client
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
-        private void EnglishSelected(object sender, RoutedEventArgs e)
-        {
-            var selectedLanguageCode = "en-US";
-            showLanguageSelectionInfoBarIfNeeded(selectedLanguageCode);
-            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = selectedLanguageCode;
-        }
-
-        private void GermanSelected(object sender, RoutedEventArgs e)
-        {
-            var selectedLanguageCode = "de-De";
-            showLanguageSelectionInfoBarIfNeeded(selectedLanguageCode);
-            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = selectedLanguageCode;
-        }
-
-        private void FrenchSelected(object sender, RoutedEventArgs e)
-        {
-            var selectedLanguageCode = "fr-FR";
-            showLanguageSelectionInfoBarIfNeeded(selectedLanguageCode);
-            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = selectedLanguageCode;
-        }
-
-        private void InfoBarRestartButton_Click(object sender, RoutedEventArgs e)
-        {
-            LanguageSelectionRestartNeededInfoBar.IsOpen = false;
-            // Restart the application
-            var currentProcess = System.Diagnostics.Process.GetCurrentProcess();
-            var startInfo = new System.Diagnostics.ProcessStartInfo
-            {
-                FileName = currentProcess.MainModule.FileName,
-                UseShellExecute = true
-            };
-            System.Diagnostics.Process.Start(startInfo);
-            // Close the current process
-            currentProcess.CloseMainWindow();
-            currentProcess.Kill();
-            Application.Current.Exit();
-            // Note: The application will restart with the new language setting.
-
-        }
-
-        private void AutoSelected(object sender, RoutedEventArgs e)
-        {
-            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = string.Empty;
-            LanguageSelectionRestartNeededInfoBar.IsOpen = true;
-        }
-
-        private void showLanguageSelectionInfoBarIfNeeded(string language)
-        {
-            if (language != Windows.Globalization.ApplicationLanguages.Languages.FirstOrDefault())
-            {
-                LanguageSelectionRestartNeededInfoBar.IsOpen = true;
-            }
         }
     }
 }
