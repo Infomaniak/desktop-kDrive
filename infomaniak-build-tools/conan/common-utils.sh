57510b789b91f469a1d95d23c0675d24f8ec10b4
Romain Galland <romain.galland@infomaniak.com>
2025-07-31 10:20:36 +0200

    refactor(common-utils.sh): improve LD_LIBRARY_PATH handling for Zsh compatibility
    Updated the script to enhance compatibility with Zsh by checking for the shwordsplit option and adjusting the handling of LD_LIBRARY_PATH accordingly. This change ensures that the script functions correctly in both Bash and Zsh environments.

--- infomaniak-build-tools/conan/common-utils.sh
+++ infomaniak-build-tools/conan/common-utils.sh
@@ -1,4 +1,4 @@
-if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
+if [[ "${BASH_SOURCE[0]}" -ef "$0" ]]; then
     echo "This script must be sourced, not executed."
     exit 1
 fi
@@ -6,7 +6,7 @@ fi
 find_conan_dependency_path() {
   local build_dir="$1"
   local origin_dep_name="${2}"
-  local dep_name="${origin_dep_name:0:5}" # Crop the name of the dependency to the first 5 characters to match the Conan package naming convention (folder ex: /home/user/.conan2/p/log4c6abc1234def/p for the dependency log4cplus)
+  local dep_name="$(echo "$origin_dep_name" | cut -c1-5)" # Crop the name of the dependency to the first 5 characters to match the Conan package naming convention (folder ex: /home/user/.conan2/p/log4c6abc1234def/p for the dependency log4cplus)
 
   [[ -z "$build_dir" ]] && { echo "Error: No build directory specified to the 'find_conan_dependency_path' function." >&2; exit 1; }
   [[ ! -d "$build_dir" ]] && { echo "Error: The specified build directory '$build_dir' does not exist." >&2; exit 1; }
@@ -26,30 +26,29 @@ find_conan_dependency_path() {
     fi
   fi
 
-  local conan_package_folder_regex="\.conan2/p/(b/)?${dep_name}[^/]*/p/lib$"
-  if [[ "$(bash --version | awk 'NR==1 {print $4}' | cut -d'.' -f1)" -lt 5 ]]; then # Check for bash version < 5
-    # On macOS, bash default version is 3.x which does not support the substring removal syntax / read -a
-    OLD_IFS="$IFS"
-    IFS=':'
-    for dir in $LD_LIBRARY_PATH; do
-      if [[ "$dir" =~ $conan_package_folder_regex ]]; then
-        echo "${dir%/lib}" # Supprime le suffixe "/lib"
-        [[ $sourced -eq 1 ]] && [[ -f "$conan_path/deactivate_conanrun.sh" ]] && source "$conan_path/deactivate_conanrun.sh" > /dev/null 2>&1
-        IFS="$OLD_IFS"
-        return 0
-      fi
-    done
-    IFS="$OLD_IFS"
-  else
-      IFS=':' read -ra path_dirs <<< "$LD_LIBRARY_PATH"
-      for dir in "${path_dirs[@]}"; do
-        if [[ "$dir" =~ $conan_package_folder_regex ]]; then
-          echo "${dir:0:-4}" # Remove the trailing "/lib" from the path
-          [[ $sourced -eq 1 ]] && [[ -f "$conan_path/deactivate_conanrun.sh" ]] && source "$conan_path/deactivate_conanrun.sh" > /dev/null 2>&1
-          return 0
-        fi
-      done
+  if [[ -n "$ZSH_VERSION" ]]; then
+    if setopt | grep -Fxq shwordsplit; then # Here we check if shwordsplit is set in Zsh because we need it to split the LD_LIBRARY_PATH correctly
+      was_shwordsplit_set=1
+    else
+      was_shwordsplit_set=0
+      setopt shwordsplit
+    fi
   fi
+  local old_IFS="$IFS"
+  IFS=':'
+  local path_dirs=()
+  path_dirs=($LD_LIBRARY_PATH)
+  IFS="$old_IFS"
+  [[ -n "$ZSH_VERSION" && "$was_shwordsplit_set" == 0 ]] && unsetopt shwordsplit # Restore the previous state of shwordsplit in Zsh
+
+  local conan_package_folder_regex="\.conan2/p/(b/)?${dep_name}[^/]*/p/lib$"
+  for dir in "${path_dirs[@]}"; do
+    if echo "$dir" | grep -qE "$conan_package_folder_regex"; then
+      echo "${dir:0:-4}" # Remove the trailing "/lib" from the path
+      [[ $sourced -eq 1 ]] && [[ -f "$conan_path/deactivate_conanrun.sh" ]] && source "$conan_path/deactivate_conanrun.sh" > /dev/null 2>&1
+      return 0
+    fi
+  done
 
 
   echo "Error: Could not find the $origin_dep_name($dep_name...)Conan package path in LD_LIBRARY_PATH='$LD_LIBRARY_PATH'." >&2
