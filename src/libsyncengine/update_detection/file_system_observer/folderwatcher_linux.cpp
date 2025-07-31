7916ec7a26cb7984f88a01fd60b3883c743cea94
Clément Kunz <clement.kunz@infomaniak.com>
2025-07-31 10:53:56 +0200

    fix: fix build errors

--- src/libsyncengine/update_detection/file_system_observer/folderwatcher_linux.cpp
+++ src/libsyncengine/update_detection/file_system_observer/folderwatcher_linux.cpp
@@ -44,7 +44,7 @@ SyncPath FolderWatcher_linux::makeSyncPath(const SyncPath &watchedFolderPath, co
 
 void FolderWatcher_linux::startWatching() {
     LOGW_DEBUG(_logger, L"Start watching folder " << Utility::formatSyncPath(_folder));
-    LOG_DEBUG(_logger, "File system format: " << Utility::fileSystemName(_folder));
+    LOG_DEBUG(_logger, "File system format: " << CommonUtility::fileSystemName(_folder));
     LOG_DEBUG(_logger, "Free space on disk: " << Utility::getFreeDiskSpace(_folder) << " bytes.");
 
     _fileDescriptor = inotify_init();
