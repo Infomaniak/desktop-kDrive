7012ac41c1dc32489d153a31e2282edea02919cd
Romain Galland <romain.galland@infomaniak.com>
2025-08-15 09:06:12 +0200

    Update infomaniak-build-tools/conan/build_dependencies.sh
    Co-authored-by: Luc Guyot <162997198+luc-guyot-infomaniak@users.noreply.github.com>

--- infomaniak-build-tools/conan/build_dependencies.sh
+++ infomaniak-build-tools/conan/build_dependencies.sh
@@ -220,7 +220,7 @@ log "Creating Qt package..."
 conan create "$conan_recipes_folder/qt/all/" --version="$qt_version" --build=missing $architecture -s:a=build_type="$build_type" -r=$local_recipe_remote_name -r=conancenter
 
 
-log "Creating package sentry..."
+log "Creating sentry package..."
 conan create "$conan_recipes_folder/sentry/all/" --build=missing $architecture -s:a=build_type="$build_type" -r=$local_recipe_remote_name -r=conancenter
 
 log "Installing dependencies..."
