fc9df8ad078c6ba4163456dd851ab604716d0fdd
Romain Galland <romain.galland@infomaniak.com>
2025-08-18 15:39:25 +0200

    fix(build): set Release build type for OpenSSL, Qt, and Sentry packages
    Updated the build_dependencies.sh script to enforce the Release build type for OpenSSL, Qt, and Sentry packages during the Conan create process. This ensures consistent build configurations across dependencies.

--- infomaniak-build-tools/conan/build_dependencies.sh
+++ infomaniak-build-tools/conan/build_dependencies.sh
@@ -141,6 +141,7 @@ has_profile() {
   conan profile list 2>/dev/null | grep -v '\.cmake$' | grep -qx "$1"
 }
 
+
 if [[ $use_release_profile == true ]]; then
   release_profile="infomaniak_release"
   if has_profile "$release_profile"; then
@@ -164,6 +165,7 @@ else
   log "Using default 'default' profile for Conan."
   conan_profile="default"
 fi
+
 
 set -euox pipefail
 conan_remote_base_folder="$PWD/infomaniak-build-tools/conan"
@@ -208,7 +210,7 @@ conan create "$conan_recipes_folder/xxhash/all/" --build=missing $architecture -
 
 if [ "$platform" = "darwin" ]; then
   log "Creating openssl package..."
-  conan create "$conan_recipes_folder/openssl-universal/all/" --build=missing --profile:all="$conan_profile" -r="$local_recipe_remote_name" -r=conancenter
+  conan create "$conan_recipes_folder/openssl-universal/all/" --build=missing -s:a=build_type=Release --profile:all="$conan_profile" -r="$local_recipe_remote_name" -r=conancenter
 fi
 
 qt_version="6.2.3"
@@ -217,13 +219,15 @@ if [[ "$platform" == "linux" ]] && [[ "$(uname -m)" == "aarch64" ]]; then
 fi
 
 log "Creating Qt package..."
-conan create "$conan_recipes_folder/qt/all/" --version="$qt_version" --build=missing $architecture -r=$local_recipe_remote_name -r=conancenter
+conan create "$conan_recipes_folder/qt/all/" --version="$qt_version" --build=missing $architecture -s:a=build_type=Release -r=$local_recipe_remote_name -r=conancenter
+
 
 log "Creating sentry package..."
-conan create "$conan_recipes_folder/sentry/all/" --build=missing $architecture -r=$local_recipe_remote_name -r=conancenter
+conan create "$conan_recipes_folder/sentry/all/" --build=missing $architecture -s:a=build_type=Release -r=$local_recipe_remote_name -r=conancenter
 
 log "Installing dependencies..."
 # Install this packet in the build folder.
+#                                                       Here: -s:b set the build type for the app itself, -s:h set the build type for the host (dependencies).
 conan install . --output-folder="$output_dir" --build=missing $architecture -s:b=build_type="$build_type" -s:h=build_type="Release" --profile:all="$conan_profile" -r=$local_recipe_remote_name -r=conancenter
 
 if [ $? -ne 0 ]; then
