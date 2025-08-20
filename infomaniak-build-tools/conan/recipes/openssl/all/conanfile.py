a664bde8246961c8eaa5a0cd0cee69d512299c2d
Romain Galland <romain.galland@infomaniak.com>
2025-08-20 12:54:20 +0200

    fix(conanfile): enforce Release build type for OpenSSL
    This change ensures that the OpenSSL library is always built in Release mode, regardless of the build type specified in the profile. A warning is issued to inform users of this behavior.

--- infomaniak-build-tools/conan/recipes/openssl/all/conanfile.py
+++ infomaniak-build-tools/conan/recipes/openssl/all/conanfile.py
@@ -30,6 +30,9 @@ class OpenSSLUniversalConan(ConanFile):
         if not self.options.shared:
             self.options.shared = True
             self.output.warning("This recipe only supports shared libraries on Apple platforms. The 'shared' option has been overwritten to True.")
+        if self.settings.build_type != "Release":
+            self.settings.build_type = "Release"
+            self.output.warning("This recipe will build OpenSSL in Release mode regardless of the build type specified in the profile.")
 
     def build(self):
         zlib_cpp_info = self.dependencies["zlib"].cpp_info.aggregated_components()
