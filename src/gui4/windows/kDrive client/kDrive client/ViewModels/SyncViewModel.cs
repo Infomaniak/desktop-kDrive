a9dc04324c120234b3f57cefa5e07fb2c35890e4
Herve Eruam <herve.eruam@infomaniak.com>
2025-09-01 15:40:57 +0200

    refactor(viewmodels): simplify LocalPath property setter
    The setter for the `LocalPath` property in `SyncViewModel.cs` has been simplified by removing the logic that ensured the path ends with a directory separator. It now directly calls `SetProperty(ref _localPath, value)` without additional checks (handle by Path API)

--- src/gui4/windows/kDrive client/kDrive client/ViewModels/SyncViewModel.cs
+++ src/gui4/windows/kDrive client/kDrive client/ViewModels/SyncViewModel.cs
@@ -49,14 +49,8 @@ namespace KDriveClient.ViewModels
         public string LocalPath
         {
             get => _localPath;
-            set
-            { // Ensure the path ends with a directory separator
-                if (!string.IsNullOrEmpty(_localPath) && !_localPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
-                {
-                    _localPath += Path.DirectorySeparatorChar;
-                }
-                SetProperty(ref _localPath, value);
-            }
+            set => SetProperty(ref _localPath, value);
+
         }
 
         public string RemotePath
