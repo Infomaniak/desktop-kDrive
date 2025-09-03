877869369f3633f31038665ae3084453b0b9fcba
Herve Eruam <herve.eruam@infomaniak.com>
2025-09-03 11:08:37 +0200

    feat: Add ErrorPage with navigation and localization
    Updated navigation in SyncErrorBar to ErrorPage. Added ErrorPage.xaml and ErrorPage.xaml.cs for layout and logic. Included new resource entries for localization and defined HeaderTextBlockStyle in InfomaniakStyles.xaml.

---
+++ src/gui4/windows/kDrive client/kDrive client/Pages/ErrorPage.xaml.cs
@@ -0,0 +1,41 @@
+using ABI.System;
+using KDrive.ViewModels;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+
+namespace KDrive
+{
+    public sealed partial class ErrorPage : Page
+    {
+        internal AppModel _viewModel = ((App)Application.Current).Data;
+        internal AppModel ViewModel { get { return _viewModel; } }
+        public ErrorPage()
+        {
+            InitializeComponent();
+            var resourceLoader = new Microsoft.Windows.ApplicationModel.Resources.ResourceLoader();
+            NavBar.ItemsSource = new string[] { resourceLoader.GetString("NavActivity/Content"), resourceLoader.GetString("ErrorPage_Title/Text") };
+        }
+
+        private void NavBar_ItemClicked(BreadcrumbBar sender, BreadcrumbBarItemClickedEventArgs args)
+        {
+            if (args.Index == 0)
+            {
+                Frame.Navigate(typeof(ActivityPage));
+            }
+        }
+    }
+}
