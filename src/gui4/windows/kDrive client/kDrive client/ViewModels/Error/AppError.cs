27536e591a88748ad675b6ef7c9c62f0efcb10c5
Herve Eruam <herve.eruam@infomaniak.com>
2025-09-03 12:56:23 +0200

    feat(error-handling): add AppError, NodeError, and SyncPalError classes
    Introduce new error handling classes that inherit from ErrorBase. Each class implements a Description method to provide detailed error information, and ErrorBase now includes properties for DbId, Time, ExitCode, and ExitCause, along with a DescriptionShort property. Licensing information has also been added to Error.cs.

---
+++ src/gui4/windows/kDrive client/kDrive client/ViewModels/Error/AppError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDrive.ViewModels.Error
+{
+    internal class AppError : ErrorBase
+    {
+        public AppError(int dbId) : base(dbId)
+        {
+        }
+
+        public override string Description()
+        {
+            // TODO: Implement a more detailed description based on ExitCode and ExitCause
+            return $"AppError: DbId={DbId}, Time={Time}, ExitCode={ExitCode}, ExitCause={ExitCause}";
+        }
+    }
+}
---
+++ src/gui4/windows/kDrive client/kDrive client/ViewModels/Error/SyncPalError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDrive.ViewModels.Error
+{
+    internal class SyncPalError : ErrorBase
+    {
+        public SyncPalError(int dbId) : base(dbId)
+        {
+        }
+
+        public override string Description()
+        {
+            // TODO: Implement a more detailed description based on ExitCode and ExitCause
+            return $"SyncPalError: DbId={DbId}, Time={Time}, ExitCode={ExitCode}, ExitCause={ExitCause}";
+        }
+    }
+}
