344c370b889d8d3811a83692bfdf370da62fec1f
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-09-18 13:48:32 +0200

    feat: Add main items in main menu

--- src/front/macOS/kDrive/AppDelegate.swift
+++ src/front/macOS/kDrive/AppDelegate.swift
@@ -18,10 +18,12 @@
 
 import Cocoa
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+@MainActor
+final class AppDelegate: NSObject, NSApplicationDelegate {
     private var mainWindow: MainWindowController?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        setupMainMenu()
         openMainWindow()
     }
 
@@ -29,9 +31,72 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
-    @MainActor
     private func openMainWindow() {
         mainWindow = MainWindowController()
         mainWindow?.showWindow(nil)
+    }
+}
+
+// MARK: - Main Menu
+
+extension AppDelegate {
+    private func setupMainMenu() {
+        let mainMenu = NSMenu(title: "MainMenu")
+
+        let applicationItem = mainMenu.addItem(withTitle: "Application", action: nil, keyEquivalent: "")
+        mainMenu.setSubmenu(createApplicationMenu(), for: applicationItem)
+
+        NSApp.mainMenu = mainMenu
+    }
+
+    private func createApplicationMenu() -> NSMenu {
+        let menu = NSMenu(title: "Application")
+
+        let aboutTitle = NSLocalizedString("About", comment: "Show About panel") + " kDrive"
+        let aboutItem = menu.addItem(
+            withTitle: aboutTitle,
+            action: #selector(NSApplication.orderFrontStandardAboutPanel(_:)),
+            keyEquivalent: ""
+        )
+        aboutItem.target = NSApp
+
+        menu.addItem(NSMenuItem.separator())
+
+        let servicesTitle = NSLocalizedString("Services", comment: "Services menu item")
+        let servicesItem = menu.addItem(withTitle: servicesTitle, action: nil, keyEquivalent: "")
+        let servicesMenu = NSMenu(title: "Services")
+        menu.setSubmenu(servicesMenu, for: servicesItem)
+        NSApp.servicesMenu = servicesMenu
+
+        menu.addItem(NSMenuItem.separator())
+
+        let hideTitle = NSLocalizedString("Hide", comment: "Hide menu item") + " KDrive"
+        let hideItem = menu.addItem(withTitle: hideTitle, action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
+        hideItem.target = NSApp
+
+        let hideOthersTitle = NSLocalizedString("Hide Others", comment: "Hide Others menu item")
+        let hideOthersItem = menu.addItem(
+            withTitle: hideOthersTitle,
+            action: #selector(NSApplication.hideOtherApplications(_:)),
+            keyEquivalent: "h"
+        )
+        hideOthersItem.keyEquivalentModifierMask = [.command, .option]
+        hideOthersItem.target = NSApp
+
+        let showAllTitle = NSLocalizedString("Show All", comment: "Show All menu item")
+        let showAllItem = menu.addItem(
+            withTitle: showAllTitle,
+            action: #selector(NSApplication.unhideAllApplications(_:)),
+            keyEquivalent: ""
+        )
+        showAllItem.target = NSApp
+
+        menu.addItem(NSMenuItem.separator())
+
+        let quitTitle = NSLocalizedString("Quit", comment: "Quit menu item") + " kDrive"
+        let quitItem = menu.addItem(withTitle: quitTitle, action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        quitItem.target = NSApp
+
+        return menu
     }
 }
