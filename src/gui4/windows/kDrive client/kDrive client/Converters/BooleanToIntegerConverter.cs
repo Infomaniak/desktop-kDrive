fbabc95bc3685cd8b39505fcd9116b1390a6253c
Herve Eruam <herve.eruam@infomaniak.com>
2025-10-02 14:22:49 +0200

    feat(Activity page winUI3): Add Activity filter combobox

--- src/gui4/windows/kDrive client/kDrive client/Converters/BooleanToIntegerConverter.cs
+++ src/gui4/windows/kDrive client/kDrive client/Converters/BooleanToIntegerConverter.cs
@@ -8,26 +8,28 @@ using Microsoft.UI.Xaml.Data;
 
 namespace Infomaniak.kDrive.Converters
 {
-    public class IntegerToBooleanConverter : IValueConverter
+    public class BooleanToIntegerConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            if (value is int intValue)
-            {
-                return intValue != 0;
-            }
-            Logger.Log(Logger.Level.Fatal, "IntegerToBooleanConverter: value is not an integer.");
-            throw new ArgumentException("Invalid value type", nameof(value));
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             if (value is bool boolValue)
             {
                 return boolValue ? 1 : 0;
             }
-            Logger.Log(Logger.Level.Fatal, "IntegerToBooleanConverter: value is not a boolean.");
+            Logger.Log(Logger.Level.Fatal, "BooleanToIntegerConverter: value is not a boolean.");
             throw new ArgumentException("Invalid value type", nameof(value));
         }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int intValue)
+            {
+                return intValue != 0;
+            }
+            Logger.Log(Logger.Level.Fatal, "BooleanToIntegerConverter: value is not an integer.");
+            throw new ArgumentException("Invalid value type", nameof(value));
+        }
+
+
     }
 }
