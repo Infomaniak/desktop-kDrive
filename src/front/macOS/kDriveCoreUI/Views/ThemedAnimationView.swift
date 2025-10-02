3a4b23da7e0c3e9d2357530c7822b14faf763f5b
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-10-02 11:53:39 +0200

    feat: Add themed animations

---
+++ src/front/macOS/kDriveCoreUI/Views/ThemedAnimationView.swift
@@ -0,0 +1,41 @@
+//
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Cocoa
+import Lottie
+
+final class ThemedAnimationView: LottieAnimationView {
+    private var themedAnimation: ThemedAnimation?
+
+    override func viewDidChangeEffectiveAppearance() {
+        guard let themedAnimation else { return }
+        Task {
+            try await loadAnimation(themedAnimation: themedAnimation)
+        }
+    }
+
+    func loadAnimation(themedAnimation: ThemedAnimation) async throws {
+        self.themedAnimation = themedAnimation
+
+        let animationName = themedAnimation.animation(forAppearance: NSApp.appearance)
+
+        let dotLottieFile = try await DotLottieFile.loadedFromBundle(forResource: animationName)
+        loadAnimation(from: dotLottieFile)
+    }
+}
