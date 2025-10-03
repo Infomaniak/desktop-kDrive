558ae0d5c5eb1dd2454a49878be033cbfe42e630
Herve Eruam <herve.eruam@infomaniak.com>
2025-10-03 18:12:02 +0200

    feat(Settings page WinUI3): Setup layer

---
+++ src/gui4/windows/kDrive client/kDrive client/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,71 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using Infomaniak.kDrive.Types;
+using Infomaniak.kDrive.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
+
+namespace Infomaniak.kDrive.Pages
+{
+    public sealed partial class SettingsPage : Page
+    {
+        private AppModel _viewModel = ((App)Application.Current).Data;
+        public AppModel ViewModel => _viewModel;
+        public SettingsPage()
+        {
+            Logger.Log(Logger.Level.Info, "Navigated to SettingsPage - Initializing SettingsPage components");
+            InitializeComponent();
+            Logger.Log(Logger.Level.Debug, "SettingsPage components initialized");
+        }
+
+        private void SyncUpToDateHyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).CurrentWindow?.AppWindow.Hide();
+        }
+
+        private void SyncInProgressHyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ActivityPage));
+        }
+
+        private void SyncInPauseHyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(ViewModel.SelectedSync == null)
+            {
+                Logger.Log(Logger.Level.Warning, "No sync is selected, cannot resume sync.");
+                return;
+            }
+            ViewModel.SelectedSync.SyncStatus = SyncStatus.Running; // Todo: Replace with actual resume logic
+        }
+    }
+}
