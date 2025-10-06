c4687a872db0b26f9f7c31adacaba50536421a70
Clément Kunz <clement.kunz@infomaniak.com>
2025-10-06 15:51:25 +0200

    chore: address PR comment
    This reverts commit a7481f30b5d652d7e3426d99975735e0adda7b8d.

--- src/libcommon/log/sentry/handler.cpp
+++ src/libcommon/log/sentry/handler.cpp
@@ -303,6 +303,9 @@ void Handler::init(AppType appType, int breadCrumbsSize) {
 
     // Init sentry
     int res = sentry_init(options);
+    if (res) {
+        std::cerr << "sentry_init returned " << res << std::endl;
+    }
     assert(res == 0);
     _instance->_isSentryActivated = true;
     _instance->setDistributionChannel(VersionChannel::Unknown);
