f19d5fb0770c902307045f54e3a3f352299af82c
Herve Eruam <herve.eruam@infomaniak.com>
2025-10-10 17:39:39 +0200

    refactor(UnknownRequestJob): update method return types
    Changed return types of methods in `UnknownRequestJob` from `bool` to `ExitInfo` for better error handling.
    Added `hasError` parameter to `serializeOutputParms`. Updated error reporting in `process` method.

--- src/server/comm/guijobs/unknownrequestjob.cpp
+++ src/server/comm/guijobs/unknownrequestjob.cpp
@@ -38,22 +38,21 @@ UnknownRequestJob::UnknownRequestJob(std::shared_ptr<CommManager> commManager, i
     _requestNum = RequestNum::Unknown;
 }
 
-bool UnknownRequestJob::deserializeInputParms() {
-    return true;
+ExitInfo UnknownRequestJob::deserializeInputParms() {
+    return ExitCode::Ok;
 }
 
-bool UnknownRequestJob::serializeOutputParms() {
+ExitInfo UnknownRequestJob::serializeOutputParms(bool hasError) {
     // Output parameters serialization
     writeParamValue(outParamsError, CommonUtility::str2CommString(_error));
     writeParamValue(outParamsErrorDescr, CommonUtility::str2CommString(_errorDescr));
-    return true;
+    return ExitCode::Ok;
 }
 
-bool UnknownRequestJob::process() {
-    _exitInfo = ExitCode::LogicError;
+ExitInfo UnknownRequestJob::process() {
     _error = "unknown_request";
     _errorDescr = "The request is unknown by the server application.";
-    return _exitInfo;
+    return ExitCode::LogicError;
 }
 
 } // namespace KDC
