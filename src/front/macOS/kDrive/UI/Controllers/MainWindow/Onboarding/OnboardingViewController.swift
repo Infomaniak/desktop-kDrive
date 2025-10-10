64680b7c02e951d1add4f49bd81c3d892cd9ab58
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-10-10 13:46:14 +0200

    feat: Transition to next step

--- src/front/macOS/kDrive/UI/Controllers/MainWindow/Onboarding/OnboardingViewController.swift
+++ src/front/macOS/kDrive/UI/Controllers/MainWindow/Onboarding/OnboardingViewController.swift
@@ -39,6 +39,7 @@ final class OnboardingViewController: NSViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
