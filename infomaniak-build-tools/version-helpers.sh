c585371b00ffc2547dd230b6e569c508d1cbd510
Hervé Eruam <herve.eruam@infomaniak.com>
2025-10-28 13:59:14 +0100

    feat: Update GetVersionFromJson to avoid the usage of jq.

--- infomaniak-build-tools/version-helpers.sh
+++ infomaniak-build-tools/version-helpers.sh
@@ -1,34 +1,10 @@
 #!/usr/bin/env bash
-#------------------------------------------------------------------------------
-# Infomaniak kDrive - Desktop
-# Copyright (C) 2023-2025 Infomaniak Network SA
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------  
+# Infomaniak kDrive - Desktop  
+# Get version from version.json without external dependencies  
+#------------------------------------------------------------------------------  
 
-# Relative path to version.json from repository root
 VERSION_JSON_RELATIVE_PATH="version.json"
-
-# Usage:
-#   GetVersionFromJson <repository_root_path> [include_build_version=true]
-#
-# Example:
-#   version=$(GetVersionFromJson "/home/user/projects/desktop-kDrive" false)
-#   echo "Version: $version"
-#
-# Requires: jq
-#------------------------------------------------------------------------------
 
 GetVersionFromJson() {
     local repository_root_path="$1"
@@ -46,12 +22,12 @@ GetVersionFromJson() {
         return 1
     fi
 
-    # Parse the JSON using jq
+    # Extract version fields using grep + sed
     local major minor patch build
-    major=$(jq -r '.NextVersion.major' "$version_json_path")
-    minor=$(jq -r '.NextVersion.minor' "$version_json_path")
-    patch=$(jq -r '.NextVersion.patch' "$version_json_path")
-    build=$(jq -r '.NextVersion.build // empty' "$version_json_path")
+    major=$(grep -Po '"major"\s*:\s*\K\d+' "$version_json_path")
+    minor=$(grep -Po '"minor"\s*:\s*\K\d+' "$version_json_path")
+    patch=$(grep -Po '"patch"\s*:\s*\K\d+' "$version_json_path")
+    build=$(grep -Po '"build"\s*:\s*\K\d+' "$version_json_path")
 
     if [[ -z "$major" || -z "$minor" || -z "$patch" ]]; then
         echo "Error: Missing version fields in $version_json_path" >&2
