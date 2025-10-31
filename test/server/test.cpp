2468adda10537ef4d8705359bb6bc7268fc6858c
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-10-31 07:28:21 +0100

    Fix build

--- test/server/test.cpp
+++ test/server/test.cpp
@@ -32,7 +32,9 @@
 #include "appserver/testappserver.h"
 #include "comm/testguicommchannel.h"
 #include "comm/testsocketcomm.h"
+#if defined(KD_WINDOWS)
 #include "comm/testpipecomm.h"
+#endif
 #include "comm/guijobs/testabstractguijob.h"
 
 namespace KDC {
@@ -50,7 +52,9 @@ CPPUNIT_TEST_SUITE_REGISTRATION(TestWindowsUpdater);
 CPPUNIT_TEST_SUITE_REGISTRATION(TestServerRequests);
 CPPUNIT_TEST_SUITE_REGISTRATION(TestAppServer);
 CPPUNIT_TEST_SUITE_REGISTRATION(TestSocketComm);
+#if defined(KD_WINDOWS)
 CPPUNIT_TEST_SUITE_REGISTRATION(TestPipeComm);
+#endif
 CPPUNIT_TEST_SUITE_REGISTRATION(TestGuiCommChannel);
 CPPUNIT_TEST_SUITE_REGISTRATION(TestAbstractGuiJob);
 
