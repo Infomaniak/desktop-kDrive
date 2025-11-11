12f4b37d67951ec6464b0ac44951ccdbac767d9a
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-11-11 11:19:15 +0100

    refactor(Drive): Added new properties to this type

--- src/front/macOS/kDriveCore/ServerBridge/Cache/DTOs/Drive.swift
+++ src/front/macOS/kDriveCore/ServerBridge/Cache/DTOs/Drive.swift
@@ -19,7 +19,15 @@
 import Foundation
 
 public struct Drive: Identifiable, Hashable, Sendable {
-    public let id: Int32
-    public var name: String
+    public var id: Int32 {
+        driveId
+    }
+
+    public let driveId: Int32
+    public let accountId: Int32
+    public let userDbId: Int32
+    public let userId: Int32
+    public let name: String
+    public let color: HexColor?
     public var syncros: [Int32: Synchro]
 }
