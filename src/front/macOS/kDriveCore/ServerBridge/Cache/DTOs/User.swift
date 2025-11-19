e5368ad1338f140dadd4577d722eac600813c7a3
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-11-19 13:00:13 +0100

    fix: Merging user cache object preserves the accounts as they are not present in a typical account update signal

--- src/front/macOS/kDriveCore/ServerBridge/Cache/DTOs/User.swift
+++ src/front/macOS/kDriveCore/ServerBridge/Cache/DTOs/User.swift
@@ -78,7 +78,7 @@ public extension User {
             userId: userId,
             name: other.name.isEmpty ? name : other.name,
             email: other.email.isEmpty ? email : other.email,
-            accounts: other.accounts, // TODO: Actual merge of accounts - This is incorrect
+            accounts: other.accounts.isEmpty ? accounts : other.accounts,
             avatar: other.avatar,
             isConnected: other.isConnected,
             isStaff: other.isStaff
