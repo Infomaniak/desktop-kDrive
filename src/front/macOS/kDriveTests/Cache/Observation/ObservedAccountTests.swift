daf76e8e91dc720bed92223ad3c84ff4e3656a1d
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-11-28 15:26:55 +0100

    test(ObservedAccountTests): Checking that ObservedAccount tracking with accountDbId only is working

--- src/front/macOS/kDriveTests/Cache/Observation/ObservedAccountTests.swift
+++ src/front/macOS/kDriveTests/Cache/Observation/ObservedAccountTests.swift
@@ -26,7 +26,7 @@ final class ObservedAccountTests: XCTestCase {
     static let expectedUserDbId: Int32 = 456
     static let expectedAccountDbId: Int32 = 3
 
-    func testSetGetAccountFromPropertyWrapper() async throws {
+    func testSetGetAccountFromPropertyWrapper_userDbId_accountDbId() async throws {
         // GIVEN
         let cache = ServerCoherentCache()
         let initialUser = await cache.getUser(dbId: Self.expectedUserDbId)
@@ -34,6 +34,50 @@ final class ObservedAccountTests: XCTestCase {
 
         @ObservedAccount(userDbId: Self.expectedUserDbId,
                          accountDbId: Self.expectedAccountDbId,
+                         cacheObservation: cache) var observedAccount: Account?
+        XCTAssertNil(observedAccount, "Account should initially be nil")
+
+        let expectedAccount = Account(dbId: Self.expectedAccountDbId, name: "3", drives: [:])
+
+        // WHEN
+        let indexedAccounts: IndexedAccounts = [
+            1: Account(dbId: 1, name: "1", drives: [:]),
+            2: Account(dbId: 2, name: "2", drives: [:]),
+            Self.expectedAccountDbId: expectedAccount,
+            4: Account(dbId: 4, name: "4", drives: [:])
+        ]
+        let user = User(
+            dbId: Self.expectedUserDbId,
+            userId: Self.expectedUserId,
+            name: "appleseed",
+            email: "ja@apple.com",
+            accounts: indexedAccounts,
+            availableDrives: [:],
+            avatar: Data(),
+            isConnected: true,
+            isStaff: true
+        )
+
+        await cache.addUser(user)
+
+        // Give time for observation to propagate
+        try await Task.sleep(nanoseconds: 10_000_000_000)
+
+        // THEN
+        let cachedUser = await cache.getUser(dbId: Self.expectedUserDbId)
+        let cachedAccount = await cache.getAccount(accountDbId: Self.expectedAccountDbId, userDbId: Self.expectedUserDbId)
+        XCTAssertEqual(cachedUser, user, "The cache user should have been updated")
+        XCTAssertEqual(cachedAccount, cachedAccount, "The cache account should have been updated")
+        XCTAssertEqual(observedAccount, expectedAccount, "The observed object should have been updated")
+    }
+    
+    func testSetGetAccountFromPropertyWrapper_accountDbId() async throws {
+        // GIVEN
+        let cache = ServerCoherentCache()
+        let initialUser = await cache.getUser(dbId: Self.expectedUserDbId)
+        XCTAssertNil(initialUser, "Cache should initially be empty")
+
+        @ObservedAccount(accountDbId: Self.expectedAccountDbId,
                          cacheObservation: cache) var observedAccount: Account?
         XCTAssertNil(observedAccount, "Account should initially be nil")
 
