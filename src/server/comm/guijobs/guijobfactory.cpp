301a5402c59b59fc347a454bac80097a0de54255
Christophe Larchier <christophe.larchier@infomaniak.com>
2025-12-11 11:55:26 +0100

    UtilitySendLogToSupportJob

--- src/server/comm/guijobs/guijobfactory.cpp
+++ src/server/comm/guijobs/guijobfactory.cpp
@@ -52,6 +52,7 @@
 #include "nodecreatemissingfoldersjob.h"
 #include "parametersinfojob.h"
 #include "parametersupdatejob.h"
+#include "utilitysendlogtosupportjob.h"
 
 namespace KDC {
 
@@ -92,7 +93,8 @@ GuiJobFactory::GuiJobFactory() {
                 {RequestNum::NODE_INFO, makeShared<NodeInfoJob>},
                 {RequestNum::NODE_CREATEMISSINGFOLDERS, makeShared<NodeCreateMissingFoldersJob>},
                 {RequestNum::PARAMETERS_INFO, makeShared<ParametersInfoJob>},
-                {RequestNum::PARAMETERS_UPDATE, makeShared<ParametersUpdateJob>}};
+                {RequestNum::PARAMETERS_UPDATE, makeShared<ParametersUpdateJob>},
+                {RequestNum::UTILITY_SEND_LOG_TO_SUPPORT, makeShared<UtilitySendLogToSupportJob>}};
 }
 
 std::shared_ptr<AbstractGuiJob> GuiJobFactory::make(RequestNum requestNum, std::shared_ptr<CommManager> commManager,
