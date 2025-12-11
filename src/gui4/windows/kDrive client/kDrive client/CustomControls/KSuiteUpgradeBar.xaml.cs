2b11c6b713bf1592c7425e6603e9da2e5457727e
Hervé Eruam <herve.eruam@infomaniak.com>
2025-12-11 11:35:38 +0100

    fix: improve URI handling and localization fallback
    Refactored KSuiteUpgradeBar to use Uri.TryCreate for safer URL assignment.
    Enhanced Utility.GetLocalizedString to log missing localization keys and
    fallback to the key itself if no localized string is found, aiding debugging
    and preventing empty strings.

--- src/gui4/windows/kDrive client/kDrive client/CustomControls/KSuiteUpgradeBar.xaml.cs
+++ src/gui4/windows/kDrive client/kDrive client/CustomControls/KSuiteUpgradeBar.xaml.cs
@@ -1,18 +1,5 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Windows.Automation.Peers;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,15 +12,11 @@ namespace Infomaniak.kDrive.CustomControls
         {
             InitializeComponent();
 
+            Uri? upgradekSuiteUri;
             // TODO: Fetch the type of the kSuite the user is using (my or pro)
-            //if (true /* Selected kSuite is "my"*/)
-            //{
-            UpgradeHyperLinkButton.NavigateUri = new Uri(Utility.GetLocalizedString("Global_UpgradeOfferMy_Url"));
-            //}
-            //else
-            //{
-            //  UpgradeHyperLinkButton.NavigateUri = new Uri(Utility.GetLocalizedString("Global_UpgradeOfferProUrl"));
-            //}
+            Uri.TryCreate(Utility.GetLocalizedString("Global_UpgradeOfferMy_Url"), UriKind.Absolute, out upgradekSuiteUri);
+            //Uri.TryCreate(Utility.GetLocalizedString("Global_UpgradeOfferProUrl"), UriKind.Absolute, out upgradekSuiteUri);
+            UpgradeHyperLinkButton.NavigateUri = upgradekSuiteUri;
         }
     }
 }
