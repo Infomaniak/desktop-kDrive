176df320b424e8ff53cc74c54edb435e0d689f1b
Hervé Eruam <herve.eruam@infomaniak.com>
2025-12-11 14:54:02 +0100

    feat(ui): disable several UI controls, update drive status
    Disabled storage button, sync mode radios, Matomo/Sentry/support cards.
    Changed search placeholder to "Rechercher dans mon drive".
    Set drive _isPaidOffer default to true.
    Minor XAML formatting fixes and code cleanup.

--- src/gui4/windows/kDrive client/kDrive client/ViewModels/App/Drive.cs
+++ src/gui4/windows/kDrive client/kDrive client/ViewModels/App/Drive.cs
@@ -35,7 +35,7 @@ namespace Infomaniak.kDrive.ViewModels
         private DriveId _driveId = -1;
         private string _name = "";
         private Color _color = Color.Blue;
-        private bool _isPaidOffer = false; // Indicates if the drive is a paid offer (i.e. myKsuite+/pro +, ...)
+        private bool _isPaidOffer = true; // Indicates if the drive is a paid offer (i.e. myKsuite+/pro +, ...)
         private ObservableCollection<Sync> _syncs = new ObservableCollection<Sync>();
         private Sync? _mainSync;
         private bool _isConfigured = false; // Indicates if at least one sync (which is not an advanced sync) is set up for this drive
