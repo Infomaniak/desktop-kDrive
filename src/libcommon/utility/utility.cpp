b94e81e4a98955f56308a41f8f8ee62f35c22e68
Clément Kunz <clement.kunz@infomaniak.com>
2025-12-15 16:23:23 +0100

    chore: refactor duplicated includes

--- src/libcommon/utility/utility.cpp
+++ src/libcommon/utility/utility.cpp
@@ -17,6 +17,7 @@
  */
 
 #include "utility.h"
+#include "utility_base.h"
 #include "libcommon/log/sentry/handler.h"
 #include "config.h"
 #include "version.h"
@@ -24,37 +25,11 @@
 #include <system_error>
 #include <sys/types.h>
 
-#if defined(KD_MACOS)
-#include <sys/statvfs.h>
-#include <sys/mount.h>
-#include <uuid/uuid.h>
-#elif defined(KD_LINUX)
-#include <sys/statvfs.h>
-#include <sys/statfs.h>
-#include <uuid/uuid.h>
-#elif defined(KD_WINDOWS)
-#include <fileapi.h>
-#endif
-
 #include <fstream>
 #include <random>
 #include <regex>
 #include <sstream>
 #include <signal.h>
-
-#if defined(KD_WINDOWS)
-#include <Poco/Util/WinRegistryKey.h>
-#endif
-
-#ifndef KD_WINDOWS
-#include <utf8proc.h>
-#endif
-
-#ifdef ZLIB_FOUND
-#include <zlib.h>
-#endif
-
-#include "utility_base.h"
 
 #include <QDir>
 #include <QTranslator>
@@ -67,14 +42,33 @@
 #include <QSqlQuery>
 #include <QOperatingSystemVersion>
 
-#if defined(Q_OS_MAC)
-#include <mach-o/dyld.h>
-#endif
-
 #include <Poco/UnicodeConverter.h>
 #include <Poco/Base64Decoder.h>
 #include <Poco/Base64Encoder.h>
 #include <Poco/StreamCopier.h>
+
+#if defined(KD_MACOS) || defined(KD_LINUX)
+#include <sys/statvfs.h>
+#include <uuid/uuid.h>
+#endif
+
+#if defined(KD_MACOS)
+#include <sys/mount.h>
+#include <mach-o/dyld.h>
+#elif defined(KD_LINUX)
+#include <sys/statvfs.h>
+#elif defined(KD_WINDOWS)
+#include <fileapi.h>
+#include <Poco/Util/WinRegistryKey.h>
+#endif
+
+#ifndef KD_WINDOWS
+#include <utf8proc.h>
+#endif
+
+#ifdef ZLIB_FOUND
+#include <zlib.h>
+#endif
 
 #define MAX_PATH_LENGTH_WIN_LONG 32767
 #define MAX_PATH_LENGTH_WIN_SHORT 259
