c8082ce2eca27e5e1ca2a82406ce780a23356621
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-12-29 17:05:23 +0100

    test(ObservedDrivesTests): Testing drive insertion via the new Drive observation mechanism

---
+++ src/front/macOS/kDriveTests/Cache/Observation/ObservedDrivesTests.swift
@@ -0,0 +1,71 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Combine
+import Foundation
+@testable import InfomaniakDI
+@testable import kDriveCore
+import Testing
+
+extension ObservedDrives {
+    var receivedValues: AsyncStream<[DriveContext]> {
+        AsyncStream { continuation in
+            let cancellable = $wrappedValue
+                .sink { value in continuation.yield(value) }
+
+            continuation.onTermination = { _ in cancellable.cancel() }
+        }
+    }
+}
+
+@MainActor
+struct ObservedDrivesTests {
+    @Test(.timeLimit(.minutes(1)))
+    func setObservedDrives() async throws {
+        // GIVEN
+        let cache = ServerCoherentCache()
+        let initialUser = await cache.getUser(dbId: ObservableData.expectedUserDbId)
+        #expect(initialUser == nil, "Cache should initially be empty")
+
+        @ObservedDrives(cacheObservation: cache) var observedDrives: [DriveContext]
+        let receivedValues = $observedDrives.receivedValues // Start to save the received values
+
+        #expect(observedDrives == [], "Drives should initially be empty")
+        await cache.addUser(ObservableData.expectedUserWithAccounts)
+
+        // WHEN
+        let expectedDrive = ObservableData.expectedDrive
+        try await cache.addDrive(expectedDrive, accountDbId: ObservableData.expectedAccountDbId)
+
+        // THEN
+        _ = await receivedValues.first(where: { _ in true })
+
+        let cachedDrive = await cache.getDrive(driveDbId: ObservableData.expectedDriveDbId)
+        #expect(cachedDrive == expectedDrive, "The cache should have been updated with a Drive")
+        #expect(observedDrives.count == 1, "We should have one Drive in the array")
+
+        guard let firstDrive = observedDrives.first else {
+            Issue.record("Failed to unwrap the first Drive")
+            return
+        }
+
+        #expect(firstDrive.drive.driveDbId == expectedDrive.driveDbId, "The drive should match the one we just added")
+        #expect(firstDrive.account.dbId == ObservableData.expectedAccount.dbId, "The account should match the one we just added")
+        #expect(firstDrive.user.dbId == ObservableData.expectedUser.dbId, "The user should match the one we just added")
+    }
+}
