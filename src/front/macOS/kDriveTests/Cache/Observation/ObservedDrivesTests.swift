87cd02c78dd5af47666eab6783af3f4b02cc2c43
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-12-30 09:22:17 +0100

    test(ObservedDrivesTests): New setMultipleObservedDrives test

--- src/front/macOS/kDriveTests/Cache/Observation/ObservedDrivesTests.swift
+++ src/front/macOS/kDriveTests/Cache/Observation/ObservedDrivesTests.swift
@@ -68,4 +68,53 @@ struct ObservedDrivesTests {
         #expect(firstDrive.account.dbId == ObservableData.expectedAccount.dbId, "The account should match the one we just added")
         #expect(firstDrive.user.dbId == ObservableData.expectedUser.dbId, "The user should match the one we just added")
     }
+
+    @Test(.timeLimit(.minutes(1)))
+    func setMultipleObservedDrives() async throws {
+        // GIVEN
+        let cache = ServerCoherentCache()
+        let initialUser = await cache.getUser(dbId: ObservableData.expectedUserDbId)
+        #expect(initialUser == nil, "Cache should initially be empty")
+
+        @ObservedDrives(cacheObservation: cache) var observedDrives: [DriveContext]
+        let receivedValues = $observedDrives.receivedValues // Start to save the received values
+
+        #expect(observedDrives == [], "Observed Drives should initially be empty")
+        await cache.addUser(ObservableData.expectedUserWithAccounts)
+
+        // WHEN
+        let expectedDrive = ObservableData.expectedDrive
+        try await cache.addDrive(expectedDrive, accountDbId: ObservableData.expectedAccountDbId)
+
+        let secondaryDrive = ObservableData.secondaryDrive
+        try await cache.addDrive(secondaryDrive, accountDbId: ObservableData.expectedAccountDbId)
+
+        // THEN
+        _ = await receivedValues.dropFirst().first(where: { _ in true })
+
+        let cachedDrive = await cache.getDrive(driveDbId: ObservableData.expectedDriveDbId)
+        #expect(cachedDrive == expectedDrive, "The cache should have been updated with a Drive")
+        let secondaryCachedDrive = await cache.getDrive(driveDbId: ObservableData.secondaryDriveDbId)
+        #expect(secondaryCachedDrive == secondaryDrive, "The cache should have been updated with another Drive")
+        #expect(observedDrives.count == 2, "We should have two Drives in the array")
+
+        guard let firstObservedDrive = observedDrives.first(where: { $0.drive.driveDbId == ObservableData.expectedDriveDbId }) else {
+            Issue.record("Failed to unwrap the first Drive")
+            return
+        }
+
+        #expect(firstObservedDrive.drive.driveDbId == expectedDrive.driveDbId, "The drive should match")
+        #expect(firstObservedDrive.account.dbId == ObservableData.expectedAccount.dbId, "The account should match")
+        #expect(firstObservedDrive.user.dbId == ObservableData.expectedUser.dbId, "The user should match")
+
+        guard let secondaryObservedDrive = observedDrives.first(where: { $0.drive.driveDbId == ObservableData.secondaryDriveDbId }) else {
+            Issue.record("Failed to unwrap the secondary Drive")
+            return
+        }
+
+        #expect(firstObservedDrive.drive != secondaryObservedDrive.drive, "The two drives should not match")
+        #expect(secondaryObservedDrive.drive.driveDbId == secondaryDrive.driveDbId, "The drive should match")
+        #expect(secondaryObservedDrive.account.dbId == ObservableData.expectedAccount.dbId, "The account should match")
+        #expect(secondaryObservedDrive.user.dbId == ObservableData.expectedUser.dbId, "The user should match")
+    }
 }
