e36b426618dc22187ab7f14731608852ddb5be83
Romain Galland <romain.galland@infomaniak.com>
2026-01-05 17:49:38 +0100

    fix(conanfile): update Qt version requirement for Linux
    Changed the Qt version requirement for Linux to always use 6.10.1, removing the previous condition that specified 6.7.3 for ARM architecture.

--- conanfile.py
+++ conanfile.py
@@ -55,7 +55,7 @@ class KDriveDesktop(ConanFile):
         """
         self.requires("zlib/[>=1.2.11 <2]", transitive_headers=True, options={"shared": True})
         # From local recipe, using the qt online installer.
-        if self.settings.os == "Linux" and str(self.settings.arch).startswith("arm"): # linux arm64
+        if self.settings.os == "Linux":
             self.requires("qt/6.7.3")
         else:
             self.requires("qt/6.10.1")
