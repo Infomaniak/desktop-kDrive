04fed09ca61cd93b12930bcbfb40f78d22ccdc7a
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-01-08 09:33:50 +0100

    feat: Add UserAndDriveView

---
+++ src/front/macOS/kDriveCoreUI/UI/Views/UserAndDriveView.swift
@@ -0,0 +1,56 @@
+//
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import SwiftUI
+import kDriveResources
+
+public struct UserAndDriveView: View {
+    let avatar: Image
+    let driveColor: Color
+
+    public var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            avatar
+                .resizable()
+                .scaledToFill()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+                .overlay {
+                    Circle()
+                        .stroke(ColorToken.Action.primary.asColor, lineWidth: 1)
+                }
+                .padding(AppPadding.padding4)
+
+            Circle()
+                .fill(Color(nsColor: .windowBackgroundColor))
+                .frame(width: 20, height: 20)
+                .overlay {
+                    KDriveResources.kdriveFoldersStacked.swiftUIImage
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 12, height: 12)
+                        .foregroundStyle(driveColor)
+                }
+        }
+    }
+}
+
+#Preview {
+    UserAndDriveView(avatar: Image(nsImage: PreviewHelper.userImage), driveColor: .yellow)
+}
