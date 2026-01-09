5522fc35513d2d3976d04396b883a4805f390a3a
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-01-09 06:52:02 +0100

    feat: Add DriveShortcutsView

---
+++ src/front/macOS/kDrive/UI/Views/MainWindow/Home/DriveShortcutsView.swift
@@ -0,0 +1,71 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import kDriveCore
+import kDriveCoreUI
+import kDriveResources
+import SwiftUI
+
+struct RemoteFolder: Sendable, Identifiable {
+    let id: Int
+    let name: String
+    let icon: Image
+
+    static let favorites = RemoteFolder(id: 0, name: "Favoris", icon: KDriveResources.star.swiftUIImage)
+    static let sharedWithMe = RemoteFolder(id: 1, name: "Partages", icon: KDriveResources.folderShare.swiftUIImage)
+    static let kDriveHome = RemoteFolder(id: 2, name: "kDrive en ligne", icon: KDriveResources.kdriveFoldersStacked.swiftUIImage)
+    static let trash = RemoteFolder(id: 3, name: "Corbeille", icon: KDriveResources.trash.swiftUIImage)
+}
+
+struct DriveShortcutsView: View {
+    let user: UIUser
+    let drive: UIDrive
+
+    private var topFolders: [RemoteFolder] {
+        return [.favorites, .sharedWithMe, .kDriveHome]
+    }
+
+    var body: some View {
+        VStack(spacing: AppPadding.padding16) {
+            UserAndDriveView(avatar: nil, driveColor: nil)
+
+            VStack(spacing: AppPadding.padding8) {
+                ForEach(topFolders) { folder in
+                    ExternalLinkButton(icon: folder.icon, title: folder.name) {
+                        didTapFolder(folder)
+                    }
+                }
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+
+            ExternalLinkButton(icon: RemoteFolder.trash.icon, title: RemoteFolder.trash.name) {
+                didTapFolder(RemoteFolder.trash)
+            }
+        }
+        .padding(AppPadding.padding16)
+        .background(ColorToken.Surface.secondary.asColor)
+        .clipShape(RoundedRectangle(cornerRadius: AppRadius.radius16))
+    }
+
+    private func didTapFolder(_ folder: RemoteFolder) {
+    }
+}
+
+#Preview {
+    DriveShortcutsView(user: PreviewHelper.user, drive: PreviewHelper.drive1)
+}
