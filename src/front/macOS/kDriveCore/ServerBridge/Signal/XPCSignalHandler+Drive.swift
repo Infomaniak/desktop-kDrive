d79825153335fcf2c77eebef74b0b97a5ef5f653
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-01-12 14:16:09 +0100

    refactor(XPCSignalHandler+Drive): Split Drive signals handling to a dedicated file

---
+++ src/front/macOS/kDriveCore/ServerBridge/Signal/XPCSignalHandler+Drive.swift
@@ -0,0 +1,41 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+
+extension XPCSignalHandler {
+    // MARK: Drive
+
+    func handleDrive(_ signal: Data) async throws {
+        guard let driveInfoSignal = try? decoder.decode(SignalMessage<DriveInfoSignal>.self, from: signal) else {
+            throw SignalError.unableToGetDriveFromSignal
+        }
+
+        let driveInfo = driveInfoSignal.body.driveInfo
+        try await coherentCache.addOrUpdateDriveSignal(driveInfo)
+    }
+
+    func handleDriveRemoved(_ signal: Data) async throws {
+        guard let driveInfoSignal = try? decoder.decode(SignalMessage<DriveRemoveSignal>.self, from: signal) else {
+            throw SignalError.unableToGetDriveDbIdFromSignal
+        }
+
+        let driveDbId = driveInfoSignal.body.driveDbId
+        try await coherentCache.removeDrive(driveDbId: driveDbId)
+    }
+}
