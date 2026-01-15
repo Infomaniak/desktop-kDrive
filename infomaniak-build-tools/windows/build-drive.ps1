68947f78be5ea3c6b56fe04acc91f73b6083144b
Luc Guyot <luc.guyot@infomaniak.com>
2026-01-15 12:15:25 +0100

    fix(windows-msi-build): do not use CAB compression (2nd attempt)

--- infomaniak-build-tools/windows/build-drive.ps1
+++ infomaniak-build-tools/windows/build-drive.ps1
@@ -607,6 +607,7 @@ function Create-MSI-Package {
 	
     Write-Host "Executing the dotnet build command ..."
     $env:WIX_CABINET_THREAD_COUNT = 1
+    $env:WIX_USE_COMPRESSION = false
 	dotnet build -v diag "$msiInstallerFolderPath\kDriveInstaller.sln" /p:WixUseCompression=false /p:WixCabinetThreadCount=1 /p:Configuration="Release" /p:Platform="x64" /p:OutputName=$appName
     if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
