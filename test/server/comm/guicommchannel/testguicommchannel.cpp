3aaed9881207ac5e994eaabf0068c9a0b2641031
Clément Kunz <clement.kunz@infomaniak.com>
2026-01-16 10:19:24 +0100

    fix: fix build error

--- test/server/comm/guicommchannel/testguicommchannel.cpp
+++ test/server/comm/guicommchannel/testguicommchannel.cpp
@@ -43,7 +43,7 @@ using namespace testcommhelpers;
 
 uint64_t GuiCommChannelTest::readData(CommChar *data, uint64_t maxlen) {
     std::scoped_lock lock(_bufferMutex);
-    uint64_t toRead = (std::min) (maxlen, static_cast<uint64_t>(_buffer.size()));
+    uint64_t toRead = (std::min)(maxlen, static_cast<uint64_t>(_buffer.size()));
     if (toRead > 0) {
         std::memcpy(data, _buffer.data(), toRead * sizeof(CommChar));
         _buffer.erase(0, toRead);
@@ -587,7 +587,6 @@ void TestGuiCommChannel::testDriveInfoListJob() {
 
     // Job expected answers
     const auto answerStr = stringifyAnswerObj(answerObjWithNumAndType);
-    const auto cbkAnswerStr = stringifyCbkAnswerObj(answerObj);
 
     // Job expected answer
     auto processFct = [](std::shared_ptr<AbstractGuiJob> job) {
