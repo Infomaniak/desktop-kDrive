e3cee9b69409a2812ca8166e25f810a9f4fd8981
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-01-16 10:08:44 +0100

    chore: Add missing radius

--- src/front/macOS/kDriveCoreUI/Tokens/AppRadius.swift
+++ src/front/macOS/kDriveCoreUI/Tokens/AppRadius.swift
@@ -20,6 +20,7 @@ import Foundation
 
 public enum AppRadius {
     public static let radius16: CGFloat = 16
+    public static let radius12: CGFloat = 12
     public static let radius8: CGFloat = 8
     public static let radius4: CGFloat = 4
 }
