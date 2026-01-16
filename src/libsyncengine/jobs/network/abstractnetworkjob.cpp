d9aec2514b524538a3a302e39af2be4bcea01bb1
Hervé Eruam <herve.eruam@infomaniak.com>
2026-01-16 13:02:26 +0100

    test

--- src/libsyncengine/jobs/network/abstractnetworkjob.cpp
+++ src/libsyncengine/jobs/network/abstractnetworkjob.cpp
@@ -387,7 +387,6 @@ ExitInfo AbstractNetworkJob::sendRequest(const Poco::URI &uri) {
     }
 
     // Send data
-    uint i = 0;
     std::string::const_iterator itBegin = _data.begin();
     while (itBegin != _data.end()) {
         const std::scoped_lock lock(_mutexSession);
@@ -398,14 +397,12 @@ ExitInfo AbstractNetworkJob::sendRequest(const Poco::URI &uri) {
 
         std::string::const_iterator itEnd = (_data.end() - itBegin > BUF_SIZE ? itBegin + BUF_SIZE : _data.end());
         try {
-            if (i % 1000 == 0) LOG_DEBUG(_logger, "Request " << jobId() << ": sending data chunk");
+            LOG_DEBUG(_logger, "Request " << jobId() << ": sending data chunk");
             stream[0].get() << std::string(itBegin, itEnd);
             if (ioOrLogicalErrorOccurred(stream[0].get())) {
                 return processSocketError("stream write error", jobId());
             }
-            if (i % 1000 == 0) LOG_DEBUG(_logger, "Request " << jobId() << ": data chunk sent");
-
-            ++i;
+            LOG_DEBUG(_logger, "Request " << jobId() << ": data chunk sent");
 
         } catch (Poco::Exception &e) {
             return processSocketError("send data exception", jobId(), e);
