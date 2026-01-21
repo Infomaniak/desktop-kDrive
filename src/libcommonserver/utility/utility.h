b0cd3d8a0ee8e2fa6761c76ce28bdfc72ad4f8ec
Clément Kunz <clement.kunz@infomaniak.com>
2026-01-21 08:35:37 +0100

    fix: fix build error

--- src/libcommonserver/utility/utility.h
+++ src/libcommonserver/utility/utility.h
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Desktop
- * Copyright (C) 2023-2025 Infomaniak Network SA
+ * Copyright (C) 2023-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 #pragma once
 
 #include "libcommonserver/commonserverlib.h"
-#include "db/dbdefs.h"
+#include "libcommonserver/db/dbdefs.h"
 
 #include "libcommon/log/log.h"
 #include "libcommon/utility/filename.h"
