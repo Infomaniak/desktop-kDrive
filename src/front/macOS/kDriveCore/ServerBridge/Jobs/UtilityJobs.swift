db473651d9ae88ed6a992843356d4f87f11c8270
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-01-30 10:45:48 +0100

    feat(UtilityJobs): Support for new utility job UTILITY_BESTVFSAVAILABLEMODE

---
+++ src/front/macOS/kDriveCore/ServerBridge/Jobs/UtilityJobs.swift
@@ -0,0 +1,43 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+import InfomaniakDI
+
+public struct UtilityJobs: Sendable {
+    @LazyInjectService private var queryFetcher: XPCQueryFetcherProtocol
+
+    enum UtilityError: Error {
+        case noGoodNewSynchPath(message: String)
+    }
+    
+    public init() {}
+
+    public func getBestVirtualFileSystemMode(path: String, driveDbId: Int32) async throws -> KDC.VirtualFileMode {
+        IKLogger.data.log("Query for best VFS mode")
+        let query = UtilityBestVFSQuery(path: path, driveDbId: driveDbId)
+        let request = await RequestMessage<UtilityBestVFSQuery>(num: RequestNum.UTILITY_BESTVFSAVAILABLEMODE, body: query)
+        print("woops \(RequestNum.EXCLAPP_GETLIST)")
+
+        let decodedMessage = try await queryFetcher.query(request, responseType: CallbackMessage<UtilityBestVFSResponse>.self)
+
+        try decodedMessage.validate()
+
+        return decodedMessage.body.bestMode
+    }
+}
