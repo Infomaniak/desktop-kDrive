57e706b0fd962690460b387cbbf058ef699e4318
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-02-03 09:37:16 +0100

    feat(macOS): Add ActivityHeaderView with activity filter picker

---
+++ src/front/macOS/kDrive/UI/Views/MainWindow/Activity/Components/ActivityHeaderView.swift
@@ -0,0 +1,69 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import kDriveCoreUI
+import kDriveResources
+import SwiftUI
+
+extension VisibleActivities {
+    var icon: Image {
+        switch self {
+        case .myActivityOnly:
+            return KDriveResources.person.swiftUIImage
+        case .allActivities:
+            return KDriveResources.people.swiftUIImage
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .myActivityOnly:
+            return "Mon activité uniquement"
+        case .allActivities:
+            return "Toutes les activités"
+        }
+    }
+}
+
+struct ActivityHeaderView: View {
+    @Binding var visibleActivities: VisibleActivities
+
+    var body: some View {
+        HStack(spacing: AppPadding.padding8) {
+            Text("Synchronisation en cours")
+                .font(.Tokens.title2)
+                .foregroundStyle(ColorToken.Text.primary.asColor)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Picker("Visible activities", selection: $visibleActivities) {
+                ForEach(VisibleActivities.allCases) { activity in
+                    Label { Text(activity.title) } icon: { activity.icon }
+                        .tag(activity)
+                }
+            }
+            .pickerStyle(.menu)
+            .labelsHidden()
+        }
+    }
+}
+
+@available(macOS 14.0, *)
+#Preview {
+    @Previewable @State var visibleActivities: VisibleActivities = .allActivities
+    ActivityHeaderView(visibleActivities: $visibleActivities)
+}
