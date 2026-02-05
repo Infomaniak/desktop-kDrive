9c5f4200ba01088f20fa7b63deadbbdea5764245
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-02-05 17:40:03 +0100

    feat(ObservedSynchroNodes): New observation mechanism to display all activities GUI side

---
+++ src/front/macOS/kDriveCore/ServerBridge/Cache/Observation/ObservedSynchroNodes.swift
@@ -0,0 +1,88 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Combine
+import Foundation
+import InfomaniakDI
+import OrderedCollections
+
+@MainActor
+@propertyWrapper
+public final class ObservedSynchroNodes: ObservableObject {
+    @Published public private(set) var wrappedValue: [SynchroNodeContext] = []
+    private var cancellable: AnyCancellable?
+
+    public init(
+        cacheObservation: CoherentCacheObservable? = nil
+    ) {
+        let cacheObservation =
+            cacheObservation ?? InjectService<CoherentCacheObservable>().wrappedValue
+
+        cancellable = cacheObservation.usersPublisher
+            .allSynchroNodesPublisher()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] nodes in
+                self?.wrappedValue = nodes
+            }
+    }
+
+    deinit { cancellable?.cancel() }
+
+    public var projectedValue: ObservedSynchroNodes {
+        self
+    }
+}
+
+public struct SynchroNodeContext: Sendable, Equatable {
+    public let node: SynchroNode
+    public let synchro: Synchro
+    public let drive: Drive
+    public let account: Account
+    public let user: User
+}
+
+public extension AnyPublisher where Output == IndexedUsers, Failure == Never {
+    func allSynchroNodesPublisher() -> AnyPublisher<[SynchroNodeContext], Never> {
+        map { usersDict in
+            var allNodes: [SynchroNodeContext] = []
+
+            for user in usersDict.values {
+                for account in user.accounts.values {
+                    for drive in account.drives.values {
+                        for synchro in drive.synchros.values {
+                            for node in synchro.synchNodes.values {
+                                let context = SynchroNodeContext(
+                                    node: node,
+                                    synchro: synchro,
+                                    drive: drive,
+                                    account: account,
+                                    user: user
+                                )
+                                allNodes.append(context)
+                            }
+                        }
+                    }
+                }
+            }
+
+            return allNodes
+        }
+        .removeDuplicates()
+        .eraseToAnyPublisher()
+    }
+}
