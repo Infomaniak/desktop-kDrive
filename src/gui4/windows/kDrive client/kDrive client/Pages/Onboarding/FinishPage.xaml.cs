7ad6fb83ea7a4b90c5bd3a007960e74a5e78717e
Hervé Eruam <herve.eruam@infomaniak.com>
2026-02-10 10:28:04 +0100

    refactor(onboarding): improve FinishPage localization
    Replaced x:Uid keys with Localizer bindings in FinishPage for better localization management. Removed unused onboarding ViewModel and navigation logic from code-behind to simplify the page.

--- src/gui4/windows/kDrive client/kDrive client/Pages/Onboarding/FinishPage.xaml.cs
+++ src/gui4/windows/kDrive client/kDrive client/Pages/Onboarding/FinishPage.xaml.cs
@@ -1,9 +1,7 @@
-using Infomaniak.kDrive.OnBoarding;
 using Infomaniak.kDrive.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 
 namespace Infomaniak.kDrive.Pages.Onboarding
@@ -11,23 +9,12 @@ namespace Infomaniak.kDrive.Pages.Onboarding
     public sealed partial class FinishPage : Page
     {
         private AppModel _viewModel = App.ServiceProvider.GetRequiredService<AppModel>();
-        private ViewModels.Onboarding? _onBoardingViewModel;
         public AppModel ViewModel { get { return _viewModel; } }
         public FinishPage()
         {
             Logger.Log(Logger.Level.Info, "Navigated to FinishPage - Initializing FinishPage components");
             InitializeComponent();
             Logger.Log(Logger.Level.Debug, "FinishPage components initialized");
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            if (e.Parameter is ViewModels.Onboarding obvm)
-            {
-                _onBoardingViewModel = obvm;
-                if ((App.Current as App)?.CurrentWindow is OnBoardingWindow onBoardingWindow)
-                    onBoardingWindow.UpdateLottieSource("Infomaniak.Custom.Animations.loader-stroke", 130);
-            }
         }
 
         private void FinishButton_Click(object sender, RoutedEventArgs e)
