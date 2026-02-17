2df7d54a29963a9cf572184f54f6828515599fa7
Luc Guyot <luc.guyot@infomaniak.com>
2026-02-17 14:43:16 +0100

    chore(constness): minor changes impacting constness and spacing

--- src/libcommon/theme/theme.cpp
+++ src/libcommon/theme/theme.cpp
@@ -99,11 +99,11 @@ QIcon Theme::themeIcon(const QString &name, bool sysTray, bool sysTrayMenuVisibl
 }
 
 void Theme::updateIconWithText(QIcon &icon, QString text) const {
-    QList<QSize> sizes = icon.availableSizes();
-    for (QSize size: sizes) {
+    const QList<QSize> &sizes = icon.availableSizes();
+    for (const QSize &size: sizes) {
         QPixmap px = icon.pixmap(size);
         QPainter painter(&px);
-        int pictSize = size.width() / 2;
+        const int pictSize = size.width() / 2;
         QRect rect(size.width() - pictSize, size.height() - pictSize, pictSize, pictSize);
 
         // Draw red circle
@@ -199,9 +199,7 @@ QIcon Theme::syncStateIcon(KDC::SyncStatus status, bool sysTray, bool sysTrayMen
 
     QIcon icon = themeIcon(statusIcon, sysTray, sysTrayMenuVisible);
 
-    if (sysTray && alert) {
-        updateIconWithText(icon, QString("!"));
-    }
+    if (sysTray && alert) updateIconWithText(icon, QString("!"));
 
     return icon;
 }
@@ -225,6 +223,7 @@ QString Theme::versionSwitchOutput() const {
     stream << QString::fromStdString(appName()) << QLatin1String(" version ") << QString::fromStdString(version()) << Qt::endl;
     stream << "Using Qt " << qVersion() << ", built against Qt " << QT_VERSION_STR << Qt::endl;
     stream << "Using '" << QSslSocket::sslLibraryVersionString() << "'" << Qt::endl;
+
     return helpText;
 }
 
