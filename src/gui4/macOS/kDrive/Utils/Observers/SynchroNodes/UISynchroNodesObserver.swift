980a1e5ec6e763da9b62f82468fd4c8a0994a50e
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-02-26 11:15:56 +0100

    refactor(observers): Add UI-prefixed observer types in SynchroNodes/ and SynchroState/
    Introduce UISynchroStateObserver.swift and UISynchroNodesObserver.swift
    in dedicated subdirectories, renaming protocols and classes with a UI
    prefix (UISynchroStateObserving, UISynchroNodesObserving, etc.) for
    consistency with the rest of the UI model layer.
    
    Add ObservedUISynchroState and ObservedUISynchroNodes as proper SwiftUI
    DynamicProperty wrappers that handle observer subscription internally,
    removing the need for manual onAppear/onReceive boilerplate in views.

---
+++ src/gui4/macOS/kDrive/Utils/Observers/SynchroNodes/UISynchroNodesObserver.swift
@@ -0,0 +1,72 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Combine
+import InfomaniakDI
+import kDriveCore
+import kDriveCoreUI
+import OrderedCollections
+import SwiftUI
+
+public protocol UISynchroNodesObserving: Sendable {
+    var synchroNodes: [UISynchroNodeContext] { get }
+    var synchroNodesPublisher: AnyPublisher<[UISynchroNodeContext], Never> { get }
+
+    func observeSynchro(_ synchroDbId: UISynchro.ID)
+}
+
+public final class UISynchroNodesObserver: UISynchroNodesObserving {
+    @MainActor public private(set) var synchroNodes: [UISynchroNodeContext] = [] {
+        didSet {
+            synchroNodesSubject.send(synchroNodes)
+        }
+    }
+
+    @MainActor private let synchroNodesSubject = PassthroughSubject<[UISynchroNodeContext], Never>()
+    @MainActor public var synchroNodesPublisher: AnyPublisher<[UISynchroNodeContext], Never> {
+        synchroNodesSubject.eraseToAnyPublisher()
+    }
+
+    @MainActor private var cancellable: AnyCancellable?
+
+    public init() {}
+
+    deinit {
+        cancellable?.cancel()
+    }
+
+    @MainActor
+    public func observeSynchro(_ synchroDbId: UISynchro.ID) {
+        cancellable?.cancel()
+
+        Task {
+            @InjectService var cache: CoherentCache
+            let nodeContexts = await cache.getSynchroNodeContexts()
+            synchroNodes = nodeContexts.map(UISynchroNodeContext.init)
+        }
+
+        @InjectService var cacheObservable: CoherentCacheObservable
+        cancellable = cacheObservable.usersPublisher.synchroNodesPublisher(for: Int32(synchroDbId))
+            .throttle(for: 1, scheduler: RunLoop.main, latest: true)
+            .map { $0.map(UISynchroNodeContext.init) }
+            .receive(on: RunLoop.main)
+            .sink { [self] nodeContexts in
+                synchroNodes = nodeContexts
+            }
+    }
+}
