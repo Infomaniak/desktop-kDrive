5d8fe1f9b3de899e0d9923ca150c5de57fa34b8d
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-02-26 10:40:19 +0100

    refactor(observers): Extract SynchroStateObserver and refocus SynchroNodesObserver
    Move UISynchroState, SynchroStateObserving, and SynchroStateObserver
    into their own SynchroStateObserver.swift file. Rewrite
    SynchroNodesObserver.swift to only contain SynchroNodesObserving and
    SynchroNodesObserver, removing the now-separate state types.

---
+++ src/gui4/macOS/kDrive/Utils/Observers/SynchroStateObserver.swift
@@ -0,0 +1,89 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Combine
+import InfomaniakDI
+import kDriveCore
+import kDriveCoreUI
+import OrderedCollections
+import SwiftUI
+
+public struct UISynchroState: Sendable {
+    public let errorCount: Int
+    public let status: UISynchroStatus
+
+    public init(errorCount: Int, status: UISynchroStatus) {
+        self.errorCount = errorCount
+        self.status = status
+    }
+}
+
+public protocol SynchroStateObserving: Sendable {
+    var synchroState: UISynchroState { get }
+    var synchroStatePublisher: AnyPublisher<UISynchroState, Never> { get }
+
+    func observeSynchro(_ synchroDbId: UISynchro.ID)
+}
+
+public final class SynchroStateObserver: SynchroStateObserving {
+    @MainActor public private(set) var synchroState = UISynchroState(errorCount: 0, status: .idle) {
+        didSet {
+            synchroStateSubject.send(synchroState)
+        }
+    }
+
+    @MainActor private let synchroStateSubject = PassthroughSubject<UISynchroState, Never>()
+    @MainActor public var synchroStatePublisher: AnyPublisher<UISynchroState, Never> {
+        synchroStateSubject.eraseToAnyPublisher()
+    }
+
+    @MainActor private var cancellable: AnyCancellable?
+
+    public init() {}
+
+    deinit {
+        cancellable?.cancel()
+    }
+
+    @MainActor
+    public func observeSynchro(_ synchroDbId: UISynchro.ID) {
+        cancellable?.cancel()
+
+        @InjectService var cache: CoherentCache
+        Task {
+            let synchro = await cache.getSynchro(synchroDbId: Int32(synchroDbId))
+            updateStateFromSynchro(synchro)
+        }
+
+        @InjectService var cacheObservable: CoherentCacheObservable
+        cancellable = cacheObservable.usersPublisher.synchroPublisher(dbId: Int32(synchroDbId))
+            .throttle(for: 1, scheduler: RunLoop.main, latest: true)
+            .receive(on: RunLoop.main)
+            .sink { [weak self] synchro in
+                self?.updateStateFromSynchro(synchro)
+            }
+    }
+
+    @MainActor
+    private func updateStateFromSynchro(_ synchro: Synchro?) {
+        let errorCount = synchro?.errors.count ?? 0
+        let status = synchro?.progress?.syncStatus ?? .Idle
+
+        synchroState = UISynchroState(errorCount: errorCount, status: UISynchroStatus(syncStatus: status) ?? .idle)
+    }
+}
