9488f39bdfaef8329fa81a83b359defd4d346125
Hervé Eruam <herve.eruam@infomaniak.com>
2026-03-02 13:38:10 +0100

    feat(errors): add conflict error and dialog components
    Add ConflictError and ConflictDialog XAML components for file conflict management
    Enable interactive selection between online and local file versions during sync conflicts
    Handle folder opening and error cases in dialog actions

---
+++ src/gui4/windows/kDrive client/kDrive client/CustomControls/Errors/Templates/Node/ConflictError.xaml.cs
@@ -0,0 +1,60 @@
+using Infomaniak.kDrive.Types;
+using Infomaniak.kDrive.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Infomaniak.kDrive.CustomControls.Errors.Templates.Node
+{
+    [ErrorMetadata(
+        Levels = new[] { ErrorLevel.Node },
+        NodeTypes = new[] { NodeType.File, NodeType.Directory },
+        ConflictTypes = new[] { ConflictType.CreateCreate, ConflictType.EditEdit }
+    )]
+    public sealed partial class ConflictError : UserControl
+    {
+        private Error Error { get; init; }
+        public ConflictError(Error error)
+        {
+            this.InitializeComponent();
+            Error = error;
+        }
+
+        private async void ErrorCard_ActionClick(object sender, RoutedEventArgs e)
+        {
+            var xamlRoot = this.XamlRoot;
+            if (xamlRoot is null)
+            {
+                return;
+            }
+            ContentDialog dialog = new ContentDialog
+            {
+                XamlRoot = xamlRoot,
+                Title = "Choisir la version à conserver", //Localizer.Instance.GetString("localFileAccessErrorTitle"),
+                Content = Localizer.Instance.GetString("localFileAccessErrorDialogOpenParentFolder"),
+                DefaultButton = ContentDialogButton.Primary,
+                SecondaryButtonText = Localizer.Instance.GetString("buttonClose"),
+                PrimaryButtonText = Localizer.Instance.GetString("buttonOpenParentFolder"),
+            };
+            dialog.Content = new ConflictDialog(Error) { XamlRoot = xamlRoot };
+
+            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+            {
+                string absolutPath = Path.Combine(Error.Sync?.LocalPath ?? "", Error.Path);
+                if (string.IsNullOrEmpty(Path.GetDirectoryName(absolutPath)))
+                {
+                    Utility.ShowUnexpectedErrorTeachingTip();
+                }
+                else
+                {
+                    await Utility.OpenFolderSecurely(absolutPath);
+                }
+            }
+        }
+    }
+}
