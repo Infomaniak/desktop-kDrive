278b36c6fbc659699e3889482ed615f96e155634
Luc Guyot <luc.guyot@infomaniak.com>
2026-03-03 16:13:27 +0100

    fix(debug-build): Fixes build error

--- test/libsyncengine/syncpal/testsyncpalworker.h
+++ test/libsyncengine/syncpal/testsyncpalworker.h
@@ -111,7 +111,7 @@ class TestSyncPalWorker : public CppUnit::TestFixture {
             private:
                 bool _networkAvailable{true};
                 std::chrono::steady_clock::duration _longPollDuration = std::chrono::seconds(50);
-                ExitInfo sendLongPoll(bool &changes) override;
+                ExitInfo sendLongPoll(const NodeId &, bool &changes) override;
                 ExitInfo generateInitialSnapshot() override;
         };
 
