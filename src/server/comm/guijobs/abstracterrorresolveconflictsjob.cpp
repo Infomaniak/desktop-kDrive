d629a590b7a5440dcf6eb1494ed5397e48a072dc
Hervé Eruam <herve.eruam@infomaniak.com>
2026-03-05 11:41:37 +0100

    chore(license): update copyright years to 2026
    Update copyright headers to 2026 in all relevant files
    Add comment in abstracterrorresolveconflictsjob.cpp to clarify why GUI notification is sent for removed errors due to SyncPal's lack of CommManager access

--- src/server/comm/guijobs/abstracterrorresolveconflictsjob.cpp
+++ src/server/comm/guijobs/abstracterrorresolveconflictsjob.cpp
@@ -1,7 +1,6 @@
-#include "abstracterrorresolveconflictsjob.h"
 /*
  * Infomaniak kDrive - Desktop
- * Copyright (C) 2023-2025 Infomaniak Network SA
+ * Copyright (C) 2023-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -99,6 +98,7 @@ ExitInfo AbstractErrorResolveConflictsJob::fixConflictsAndNotify(const std::shar
     }
 
     for (const auto &dbId: removedErrorsDbIds) {
+        // SyncPal doesn't have access to the CommManager, so we notify the GUI about removed errors here.
         _commManager->sendGuiSignal(std::make_shared<SignalErrorRemovedJob>(dbId));
     }
 
