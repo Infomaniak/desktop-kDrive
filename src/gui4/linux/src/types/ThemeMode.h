7e3645649f8ed534fa7885eb83f315ecbea7ee0b
Romain Galland <romain.galland@infomaniak.com>
2026-03-17 12:26:57 +0100

    feat(types): add ThemeMode class for theme management

---
+++ src/gui4/linux/src/types/ThemeMode.h
@@ -0,0 +1,33 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef THEMEMODE_H
+#define THEMEMODE_H
+#include <QObject>
+#include <QtQmlIntegration/qqmlintegration.h>
+
+class ThemeMode : public QObject {
+    Q_OBJECT
+    QML_ELEMENT
+    QML_UNCREATABLE("ThemeMode is an enum type and cannot be instantiated")
+public:
+    enum class Value { System, Light, Dark };
+    Q_ENUM(Value)
+};
+
+#endif //THEMEMODE_H
