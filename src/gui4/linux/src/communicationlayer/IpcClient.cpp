6a61d8c89429d4c578ead3444376c12a616570eb
Romain Galland <romain.galland@infomaniak.com>
2026-03-18 11:10:59 +0100

    docs(ipc): comment the readPortFromCommFile

--- src/gui4/linux/src/communicationlayer/IpcClient.cpp
+++ src/gui4/linux/src/communicationlayer/IpcClient.cpp
@@ -1,5 +1,23 @@
 /*
  * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Infomaniak kDrive - Desktop
  * Copyright (C) 2023-2025 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
@@ -48,6 +66,11 @@ void IpcClient::connectToServer() {
     }
     _socket->connectToHost(QHostAddress::LocalHost, port);
 }
+
+/**
+ * Get the port number from the .comm file located in the application support directory.
+ * @return the port read from the .comm file, or 0 if the file cannot be read
+ */
 quint16 IpcClient::readPortFromCommFile() {
     const std::filesystem::path commPath = CommonUtility::getAppSupportDir() / ".comm";
     std::ifstream commFile(commPath);
