d852117ef326279837efb722a596ec1929c8a83d
Christophe Larchier <christophe.larchier@infomaniak.com>
2026-03-18 15:51:54 +0100

    fix: Address comments

--- src/libsyncengine/update_detection/update_detector/node.cpp
+++ src/libsyncengine/update_detection/update_detector/node.cpp
@@ -179,7 +179,13 @@ bool Node::insertChildren(std::shared_ptr<Node> child) {
     }
 
     if (child->parentNode() != shared_from_this()) {
-        child->setParentNode(shared_from_this());
+        if (!child->setParentNode(shared_from_this())) {
+            LOGW_WARN(Log::instance()->getLogger(), L"Error in Node::setParentNode: node name="
+                                                            << Utility::formatSyncName(child->name()) << L" parent node name="
+                                                            << Utility::formatSyncName(name()));
+
+            return false;
+        }
     }
 
     _childrenById[*child->id()] = child;
