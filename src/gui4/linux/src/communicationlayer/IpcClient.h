73bd6fb0c9a967adcae8548250ce4591c49973e8
Romain Galland <romain.galland@infomaniak.com>
2026-03-18 10:38:04 +0100

    feat(network): implement IPC client for server communication

---
+++ src/gui4/linux/src/communicationlayer/IpcClient.h
@@ -0,0 +1,78 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "libcommon/comm.h"
+
+#include <QTcpSocket>
+
+#include <Poco/Dynamic/Struct.h>
+
+#include <string>
+
+namespace KDC {
+
+class IpcClient : public QObject {
+        Q_OBJECT
+
+    public:
+        explicit IpcClient(QObject *parent = nullptr);
+
+        void connectToServer();
+        int sendRequest(RequestNum num, const Poco::DynamicStruct &params = {});
+
+    signals:
+        void connected();
+        void disconnected();
+        void messageReceived(int type, int id, int num, const Poco::DynamicStruct &params);
+
+    private slots:
+        void onConnected();
+        void onDisconnected();
+        void onReadyRead();
+
+    private:
+        QTcpSocket *_socket;
+        std::string _readBuffer;
+        QAtomicInt _nextId{0};
+
+        static quint16 readPortFromCommFile();
+        void processBuffer();
+        bool extractNextMessage(std::string &buffer, std::string &outMessage);
+};
+
+} // namespace KDC
