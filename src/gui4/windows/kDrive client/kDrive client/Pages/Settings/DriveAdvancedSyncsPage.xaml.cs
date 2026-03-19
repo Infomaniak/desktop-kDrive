c68dac12f3fb9969f42e7b6a93c2d981bb8f7bf1
Hervé Eruam <herve.eruam@infomaniak.com>
2026-03-19 12:23:39 +0100

    feat(sync): open folder exclusion in dialog from advanced syncs
    Refactor folder exclusion management in DriveAdvancedSyncsPage:
    replace inline SyncExclusionSelector with a "Select" button that
    opens SyncExclusionPage in a ContentDialog. Allow SyncExclusionPage
    to be constructed with a Sync object and expose SaveChanges for
    external save. Improve remote path display with tooltip. Clean up
    event handling and logging for robustness.

--- src/gui4/windows/kDrive client/kDrive client/Pages/Settings/DriveAdvancedSyncsPage.xaml.cs
+++ src/gui4/windows/kDrive client/kDrive client/Pages/Settings/DriveAdvancedSyncsPage.xaml.cs
@@ -1,4 +1,5 @@
 using Infomaniak.kDrive.CustomControls;
+using Infomaniak.kDrive.Pages.DriveSetupContentDialog;
 using Infomaniak.kDrive.ServerCommunication.Interfaces;
 using Infomaniak.kDrive.Types;
 using Infomaniak.kDrive.ViewModels;
@@ -250,5 +251,46 @@ public sealed partial class DriveAdvancedSyncsPage : Page
 
         if (control is not null)
             control.IsEnabled = true;
+    }
+
+    private async void SyncExclusionButton_Click(object sender, RoutedEventArgs e)
+    {
+        Control? control = sender as Control;
+        if (control is null)
+        {
+            Logger.Log(Logger.Level.Error, "Sync exclusion button is null when clicking on sync exclusion button");
+            Utility.ShowUnexpectedErrorTeachingTip();
+            return;
+        }
+
+        Sync? sync = control.DataContext as Sync;
+        if (sync is null)
+        {
+            Logger.Log(Logger.Level.Error, "Could not get sync from DataContext when clicking on sync exclusion button");
+            Utility.ShowUnexpectedErrorTeachingTip();
+            return;
+        }
+
+        ContentDialog dialog = new ContentDialog
+        {
+            XamlRoot = this.XamlRoot,
+            SecondaryButtonText = Localizer.Instance.GetString("buttonConfirm"),
+            PrimaryButtonText = Localizer.Instance.GetString("buttonCancel"),
+            DefaultButton = ContentDialogButton.Secondary
+        };
+        var exclusionPage = new SyncExclusionPage(sync);
+        dialog.Content = exclusionPage;
+
+        var result = await dialog.ShowAsync();
+        if (result == ContentDialogResult.Secondary)
+        {
+            Logger.Log(Logger.Level.Info, "User confirmed sync exclusion changes");
+            await exclusionPage.SaveChanges();
+        }
+        else
+        {
+            Logger.Log(Logger.Level.Info, "User canceled sync exclusion changes");
+        }
+
     }
 }
