cc1e876de796983fac933e4da1ac6ff87c6de795
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-03-20 08:31:59 +0100

    feat: Add SynchroConfigurationPickerView dummy UI

--- src/gui4/macOS/kDrive/UI/Views/SynchroConfiguration/SynchroConfigurationPickerView.swift
+++ src/gui4/macOS/kDrive/UI/Views/SynchroConfiguration/SynchroConfigurationPickerView.swift
@@ -16,14 +16,77 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import kDriveCoreUI
+import kDriveResources
 import SwiftUI
 
 struct SynchroConfigurationPickerView: View {
+    @EnvironmentObject private var viewModel: SynchroConfigurationFlowViewModel
+
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Section {} header: {
+                VStack(alignment: .leading, spacing: AppPadding.padding12) {
+                    Text("!Configurer vos kDrive")
+                    Text("!Pour chaque kDrive, vous pourrez choisir où il est synchronisé sur votre ordinateur et quels dossiers synchroniser.")
+                }
+            }
+
+            ForEach(Array(viewModel.configurations.values)) { configuration in
+                Section {
+                    HStack {
+                        HStack(alignment: .top) {
+                            BadgeView(
+                                image: KDriveResources.kdriveFoldersStacked.swiftUIImage,
+                                color: configuration.drive.color ?? ColorToken.Drive.defaultColor.asColor
+                            )
+
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("!Emplacement:")
+                                    Text("")
+                                }
+
+                                HStack {
+                                    Text("!Contenu synchronisé:")
+                                    Text("")
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+
+                            Button("!Configurer") {
+                                viewModel.navigate(to: .configureSynchro(configuration))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .groupedFormatStyle()
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                LoadingButton {
+                    await viewModel.onConfirm?(Array(viewModel.configurations.values))
+                } label: {
+                    Text(KDriveLocalizable.buttonValidate)
+                }
+            }
+
+            ToolbarItem(placement: .cancellationAction) {
+                Button(KDriveLocalizable.buttonCancel, role: .cancel) {
+                    viewModel.onCancel?()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SynchroConfigurationPickerView()
+    let viewModel = SynchroConfigurationFlowViewModel(onConfirm: nil, onCancel: nil)
+    viewModel.setupInitialConfigurations([
+        SynchroConfiguration(drive: PreviewHelper.drive1, blackList: [])
+    ])
+
+    return SynchroConfigurationPickerView()
+        .environmentObject(viewModel)
 }
