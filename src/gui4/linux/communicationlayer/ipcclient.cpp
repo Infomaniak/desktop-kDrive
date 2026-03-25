5ef6f2b3d90ad356b210c349bb2dcd5f91870ebc
Romain Galland <romain.galland@infomaniak.com>
2026-03-25 12:05:45 +0100

    chore(build): standardize file naming conventions for GUI sources by lowercasing the filenames

--- src/gui4/linux/communicationlayer/ipcclient.cpp
+++ src/gui4/linux/communicationlayer/ipcclient.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "IpcClient.h"
+#include "ipcclient.h"
 
 #include "libcommon/utility/utility.h"
 #include "libcommon/utility/cstypes.h"
