460fd6d9be671cf9b9039129d8f83754da9dd473
Romain Galland <romain.galland@infomaniak.com>
2026-03-25 10:12:51 +0100

    refactor(app): introduce AppClientLinux as application entry point

---
+++ src/gui4/linux/src/AppClientLinux.cpp
@@ -0,0 +1,53 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "AppClientLinux.h"
+
+#include "libcommongui/logger.h"
+
+#include <cstdlib>
+
+namespace KDC {
+
+Q_LOGGING_CATEGORY(lcAppClientLinux, "gui.v4.app", QtInfoMsg)
+
+AppClientLinux::AppClientLinux(int &argc, char **argv) :
+    QGuiApplication(argc, argv) {
+    setupLogging();
+
+    connect(&_ipcClient, &IpcClient::connected, this, &AppClientLinux::ipcConnected);
+    connect(&_ipcClient, &IpcClient::disconnected, this, &AppClientLinux::ipcDisconnected);
+    connect(&_ipcClient, &IpcClient::messageReceived, this, &AppClientLinux::ipcMessageReceived);
+
+#ifdef QT_DEBUG
+    _ipcClient.connectToServer();
+#else
+    qCCritical(lcAppClientLinux) << "Release mode not already supported.";
+    std::exit(EXIT_FAILURE);
+#endif
+}
+
+void AppClientLinux::setupLogging() {
+    auto *logger = Logger::instance();
+    logger->setIsCLientLog(true);
+    logger->setLogDebug(true);
+    logger->setupTemporaryFolderLogDir();
+    logger->enterNextLogFile();
+}
+
+} // namespace KDC
