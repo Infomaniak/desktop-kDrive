087b0a4c71050ddb37cbd7d46ac0cce914cf8281
Hervé Eruam <herve.eruam@infomaniak.com>
2026-03-26 14:45:40 +0100

    feat(ui): introduce custom AppTitleBar control
    Replace TitleBar with new AppTitleBar control for unified header logic and visuals
    Update MainWindow and OnBoardingWindow to use (App)TitleBar
    Switch FinishPage button to AccentButtonStyle

---
+++ src/gui4/windows/kDrive client/kDrive client/CustomControls/AppTitleBar.xaml.cs
@@ -0,0 +1,20 @@
+using Infomaniak.kDrive.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace Infomaniak.kDrive.CustomControls
+{
+    public sealed partial class AppTitleBar : TitleBar
+    {
+        public AppModel ViewModel { get; } =
+           App.ServiceProvider.GetRequiredService<AppModel>();
+
+        public AppTitleBar()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
