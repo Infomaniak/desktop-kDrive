e24e1c038b0545f7a2151234712c67e1dc58355e
Hervé Eruam <herve.eruam@infomaniak.com>
2026-03-26 15:51:58 +0100

    fix: fix test

--- test/server/comm/guicommchannel/testerrorjobs.cpp
+++ test/server/comm/guicommchannel/testerrorjobs.cpp
@@ -221,7 +221,7 @@ void TestGuiCommChannel::testErrorDeleteJob() {
     auto processFct = [](std::shared_ptr<AbstractGuiJob> job) {
         auto deleteJob = std::dynamic_pointer_cast<ErrorDeleteJob>(job);
         CPPUNIT_ASSERT(deleteJob);
-        CPPUNIT_ASSERT_EQUAL(42, deleteJob->_errorDbId);
+        CPPUNIT_ASSERT_EQUAL(int64_t(42), deleteJob->_errorDbId);
     };
 
 #if defined(KD_WINDOWS) || defined(KD_LINUX)
