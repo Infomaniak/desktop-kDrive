74d4884025807cd9febd93ead1207221295415f6
Romain Galland <romain.galland@infomaniak.com>
2026-04-01 14:13:53 +0200

    refactor(signal): update output parameter comments for clarityrefactor(server): replace inline output parameter comments with detailed `serializeOutputParms()` mappings

--- src/server/comm/guijobs/signalerroraddedjob.cpp
+++ src/server/comm/guijobs/signalerroraddedjob.cpp
@@ -20,6 +20,10 @@
 #include "libcommon/utility/utility.h"
 #include "libcommon/comm.h"
 
+// Signal: SignalNum::UTILITY_ERROR_ADDED
+// serializeOutputParms() writes: {
+//   MSG_PARAM_ERROR_INFO ("errorInfo"): Error
+// }
 namespace KDC {
 
 SignalErrorAddedJob::SignalErrorAddedJob(const Error &error) :
