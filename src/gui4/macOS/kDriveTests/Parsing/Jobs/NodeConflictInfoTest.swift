7848b38dd9590e9bef3b17f802af14a5cf2685b7
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-04-07 16:29:04 +0200

    test: Parsing response test for new job NODE_CONFLICT_INFO

---
+++ src/gui4/macOS/kDriveTests/Parsing/Jobs/NodeConflictInfoTest.swift
@@ -0,0 +1,54 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+@testable import kDriveCore
+import Testing
+
+@Suite("NodeConflictInfoTest Job Parsing Test")
+struct NodeConflictInfoTest {
+    private let decoder = JSONDecoder()
+
+    // MARK: - Test Data
+
+    var validJobCallbackData: Data {
+        let bundle = Bundle(for: TestBundleMarker.self)
+
+        guard let url = bundle.url(forResource: "NODE_CONFLICT_INFO", withExtension: "json") else {
+            fatalError("Unable to find specified JSON file")
+        }
+
+        return try! Data(contentsOf: url)
+    }
+
+    // MARK: - Parsing Test
+
+    @Test("Successfully parses a valid NODE_CONFLICT_INFO.json")
+    func parseValidJobCallback() throws {
+        // GIVEN
+        let callbackData = validJobCallbackData
+
+        // WHEN
+        let response = try decoder.decode(CallbackMessage<NodeConflictInfoResponse>.self, from: callbackData)
+
+        // THEN
+        #expect(response.body.nodeConflictInfo.authorName == "Test Author")
+        #expect(response.body.nodeConflictInfo.fileSize == 12345678)
+        #expect(response.body.nodeConflictInfo.lastModificationDate == 1704067200)
+    }
+}
