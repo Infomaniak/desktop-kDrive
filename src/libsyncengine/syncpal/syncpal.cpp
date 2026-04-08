b81e02a9c82fdc5d2eb2db554b3a12dfee2f4657
Clément Kunz <clement.kunz@infomaniak.com>
2026-04-08 10:25:31 +0200

    fix(DownloadJob): fix circular reference

--- src/libsyncengine/syncpal/syncpal.cpp
+++ src/libsyncengine/syncpal/syncpal.cpp
@@ -735,11 +735,13 @@ ExitCode SyncPal::addDlDirectJob(const SyncPath &relativePath, const SyncPath &a
     std::function<void(UniqueId)> callback = std::bind_front(&SyncPal::directDownloadCallback, this);
     job->setAdditionalCallback(callback);
 
-    const auto progressPercentCallback = [job, this](UniqueId,
+    const auto progressPercentCallback = [jobWeak = std::weak_ptr<DownloadJob>(job), this](UniqueId,
                                                      int progress // %
                                          ) {
-        if (!setProgress(job->affectedFilePath(), progress)) {
-            LOGW_SYNCPAL_WARN(_logger, L"Error in SyncPal::setProgress: " << Utility::formatSyncPath(job->affectedFilePath()));
+        if (auto job = jobWeak.lock()) {
+            if (!setProgress(job->affectedFilePath(), progress)) {
+                LOGW_SYNCPAL_WARN(_logger, L"Error in SyncPal::setProgress: " << Utility::formatSyncPath(job->affectedFilePath()));
+            }
         }
     };
 
