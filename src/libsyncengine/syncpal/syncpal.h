c2f970d4cafda932abf651071386abc30e5305b1
Clément Kunz <clement.kunz@infomaniak.com>
2026-04-09 11:49:25 +0200

    feat(CacheDirectory): cache directory is managed at SyncPal level

--- src/libsyncengine/syncpal/syncpal.h
+++ src/libsyncengine/syncpal/syncpal.h
@@ -338,6 +338,8 @@ class SYNCENGINE_EXPORT SyncPal : public std::enable_shared_from_this<SyncPal> {
             (void) _localOperationSet->removeOp(localNodeId, operationType);
         }
 
+        [[nodiscard]] std::shared_ptr<CacheDirectory> cacheDirectory() const { return _cacheDirectory; }
+
     protected:
         virtual void createWorkers(const std::chrono::seconds &startDelay = std::chrono::seconds(0));
 
@@ -422,6 +424,8 @@ class SYNCENGINE_EXPORT SyncPal : public std::enable_shared_from_this<SyncPal> {
         void setUpConflictingFilesCorrector(const std::vector<Error> &keepLocalErrorList,
                                             const std::vector<Error> &keepRemoteErrorList);
         log4cplus::Logger _logger;
+
+        std::shared_ptr<CacheDirectory> _cacheDirectory;
 
         // TODO : Refactor to not use friend classes (should be reserved for test purpose).
         friend class SyncPalWorker;
