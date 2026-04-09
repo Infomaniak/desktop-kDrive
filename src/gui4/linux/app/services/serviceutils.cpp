4bf350d9d421940abc1a8306d946d819f7c6d574
Romain Galland <romain.galland@infomaniak.com>
2026-04-09 11:52:52 +0200

    feat(linux-v4): add `ServiceUtils` for handling formatted IPC exit info

---
+++ src/gui4/linux/app/services/serviceutils.cpp
@@ -0,0 +1,31 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "serviceutils.h"
+
+#include <cstdint>
+
+namespace KDC::ServiceUtils {
+
+QString formatExitInfo(const ExitInfo &exitInfo) {
+    return QStringLiteral("IPC request failed (code=%1, cause=%2)")
+            .arg(static_cast<int32_t>(exitInfo.code()))
+            .arg(static_cast<int32_t>(exitInfo.cause()));
+}
+
+} // namespace KDC::ServiceUtils
---
+++ src/gui4/linux/app/services/serviceutils.h
@@ -0,0 +1,29 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "libcommon/utility/types.h"
+
+#include <QString>
+
+namespace KDC::ServiceUtils {
+
+QString formatExitInfo(const ExitInfo &exitInfo);
+
+} // namespace KDC::ServiceUtils
