7a3bf0db3cf0bd8776c0f33a2b8c7ad4165f51c2
Romain Galland <romain.galland@infomaniak.com>
2026-04-27 15:32:06 +0200

    feat(cachepipeline): introduce CachePipeline to manage push-driven cache updates

---
+++ src/gui4/linux/app/cache/cachepipeline.h
@@ -0,0 +1,44 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "app/cache/appcache.h"
+#include "app/services/commservice.h"
+
+#include <QObject>
+
+namespace KDC {
+
+/**
+ * Owns all server-push signal connections from CommService to AppCache.
+ *
+ * This is the single bridge for push-driven cache mutation in the Linux v4 services layer.
+ */
+class CachePipeline : public QObject {
+        Q_OBJECT
+
+    public:
+        explicit CachePipeline(CommService &commService, AppCache &appCache, QObject *parent = nullptr);
+
+    private:
+        CommService &_commService;
+        AppCache &_appCache;
+};
+
+} // namespace KDC
