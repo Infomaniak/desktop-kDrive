c4c46dc1f58c865edf7beccd6db4825f06c1ff15
Romain Galland <romain.galland@infomaniak.com>
2026-04-27 15:32:28 +0200

    feat(driveservice): introduce DriveService for high-level drive operations and cache synchronization

---
+++ src/gui4/linux/app/services/driveservice.cpp
@@ -0,0 +1,124 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "driveservice.h"
+
+#include <QLoggingCategory>
+#include <QPointer>
+
+namespace {
+constexpr char serviceKeyDrive[] = "drive";
+constexpr char actionLoadDrives[] = "loadDrives";
+constexpr char actionDeleteDrive[] = "deleteDrive";
+constexpr char actionUpdateDrive[] = "updateDrive";
+} // namespace
+
+namespace KDC {
+
+Q_LOGGING_CATEGORY(lcDriveService, "gui.v4.driveservice", QtInfoMsg)
+
+DriveService::DriveService(CommService &commService, AppCache &appCache, ServiceActionTracker &serviceActionTracker,
+                           ServiceEventBus &serviceEventBus, QObject *const parent) :
+    QObject(parent),
+    _commService(commService),
+    _appCache(appCache),
+    _serviceActionTracker(serviceActionTracker),
+    _serviceEventBus(serviceEventBus) {
+    (void) connect(&_serviceActionTracker, &ServiceActionTracker::servicePendingChanged, this,
+                   [this](const ServiceActionTracker::ServiceKey &serviceKey, const bool) {
+                       if (serviceKey == serviceKeyDrive) {
+                           emit loadingChanged();
+                       }
+                   });
+}
+
+bool DriveService::loading() const {
+    return _serviceActionTracker.isServicePending(serviceKeyDrive);
+}
+
+void DriveService::loadDrives() {
+    beginAction(actionLoadDrives);
+
+    const QPointer<DriveService> self(this);
+    _commService.requestDriveInfoList([self](const ExitInfo &exitInfo, const std::vector<DriveInfo> &list) {
+        if (!self) {
+            return;
+        }
+
+        self->endAction(actionLoadDrives);
+        if (!exitInfo) {
+            self->notifyRequestFailure(exitInfo, RequestNum::DRIVE_INFOLIST);
+            return;
+        }
+
+        self->_appCache.replaceDrives(list);
+    });
+}
+
+void DriveService::deleteDrive(const qint64 driveDbId) {
+    beginAction(actionDeleteDrive, driveDbId);
+
+    const QPointer<DriveService> self(this);
+    // Cache consistency is signal-driven: we wait for driveRemoved/driveUpdated pushes.
+    _commService.requestDriveDelete(static_cast<DriveDbId>(driveDbId), [self, driveDbId](const ExitInfo &exitInfo) {
+        if (!self) {
+            return;
+        }
+
+        self->endAction(actionDeleteDrive, driveDbId);
+        if (!exitInfo) {
+            self->notifyRequestFailure(exitInfo, RequestNum::DRIVE_DELETE);
+        }
+    });
+}
+
+void DriveService::updateDrive(const DriveInfo &driveInfo) {
+    beginAction(actionUpdateDrive, static_cast<qint64>(driveInfo.dbId()));
+
+    const QPointer<DriveService> self(this);
+    _commService.requestDriveUpdate(driveInfo, [self, driveDbId = static_cast<qint64>(driveInfo.dbId())](const ExitInfo &exitInfo) {
+        if (!self) {
+            return;
+        }
+
+        self->endAction(actionUpdateDrive, driveDbId);
+        if (!exitInfo) {
+            self->notifyRequestFailure(exitInfo, RequestNum::DRIVE_UPDATE);
+        }
+    });
+}
+
+void DriveService::beginAction(const ServiceActionTracker::ActionKey &actionKey, const ServiceActionTracker::ScopeId scopeId) {
+    _serviceActionTracker.beginAction(serviceKeyDrive, actionKey, scopeId);
+}
+
+void DriveService::endAction(const ServiceActionTracker::ActionKey &actionKey, const ServiceActionTracker::ScopeId scopeId) {
+    _serviceActionTracker.endAction(serviceKeyDrive, actionKey, scopeId);
+}
+
+bool DriveService::isActionPending(const ServiceActionTracker::ActionKey &actionKey,
+                                   const ServiceActionTracker::ScopeId scopeId) const {
+    return _serviceActionTracker.isActionPending(serviceKeyDrive, actionKey, scopeId);
+}
+
+void DriveService::notifyRequestFailure(const ExitInfo &exitInfo, const RequestNum requestNum) {
+    qCWarning(lcDriveService) << "Drive service request failed | code:" << exitInfo.code() << "/ cause:" << exitInfo.cause();
+    _serviceEventBus.notifyGenericError(exitInfo, requestNum);
+}
+
+} // namespace KDC
