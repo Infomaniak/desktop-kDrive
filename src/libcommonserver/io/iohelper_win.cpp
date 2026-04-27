877a7e8fc3c9e6cb73604ae10849bab2e9fade5e
Herve <herve.eruam@infomaniak.com>
2026-04-27 16:08:04 +0200

    feat(iohelper_win): add GetRootNodeId for root paths
    Introduce GetRootNodeId to retrieve node id for root paths on Windows by opening the root handle and extracting file information. Also includes minor style adjustments.

--- src/libcommonserver/io/iohelper_win.cpp
+++ src/libcommonserver/io/iohelper_win.cpp
@@ -134,21 +134,24 @@ uint64_t computeNodeId(const _FILE_ID_FULL_DIR_INFORMATION *pFileInfo) {
 
 uint64_t computeNodeId(const BY_HANDLE_FILE_INFORMATION &pFileInfo) {
     // We keep `long long` type cast for legacy reason.
-    const auto longLongId = (static_cast<long long>(pFileInfo.nFileIndexHigh) << 32) + static_cast<long long>(pFileInfo.nFileIndexLow);
+    const auto longLongId =
+            (static_cast<long long>(pFileInfo.nFileIndexHigh) << 32) + static_cast<long long>(pFileInfo.nFileIndexLow);
 
     return static_cast<uint64_t>(longLongId);
 }
 
+// Get the node id of a root path. This is necessary because pzwQueryDirectoryFile requires a parent handle, which is not
+// available for root paths.
 bool GetRootNodeId(const SyncPath &rootPath, NodeId &nodeId) noexcept {
-    const HANDLE hRoot = CreateFileW(rootPath.wstring().c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr,
-                               OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, nullptr);
+    const HANDLE hRoot = CreateFileW(rootPath.wstring().c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                                     nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, nullptr);
 
     if (hRoot == INVALID_HANDLE_VALUE) {
         return false;
     }
     BY_HANDLE_FILE_INFORMATION info{};
     if (!GetFileInformationByHandle(hRoot, &info)) {
-        (void)  CloseHandle(hRoot);
+        (void) CloseHandle(hRoot);
         return false;
     }
 
