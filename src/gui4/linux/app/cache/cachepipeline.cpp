c9b3bc18e8f933496a3b62daaf08711e9bf612dd
Romain Galland <romain.galland@infomaniak.com>
2026-05-04 15:39:22 +0200

    fix(cachepipeline): wrap push signal handling with conditional checks via lambdas

--- src/gui4/linux/app/cache/cachepipeline.cpp
+++ src/gui4/linux/app/cache/cachepipeline.cpp
@@ -32,36 +32,64 @@ CachePipeline::CachePipeline(CommService &commService, AppCache &appCache, QObje
     QObject(parent),
     _commService(commService),
     _appCache(appCache) {
-    (void) connect(&_commService, &CommService::userAdded, &_appCache, &AppCache::upsertUser, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::userUpdated, &_appCache, &AppCache::upsertUser, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::userRemoved, &_appCache, &AppCache::removeUser, Qt::UniqueConnection);
+    (void) connect(&_commService, &CommService::userAdded, this, [this](const UserInfo &info) {
+        if (acceptPush("userAdded")) _appCache.upsertUser(info);
+    });
+    (void) connect(&_commService, &CommService::userUpdated, this, [this](const UserInfo &info) {
+        if (acceptPush("userUpdated")) _appCache.upsertUser(info);
+    });
+    (void) connect(&_commService, &CommService::userRemoved, this, [this](const UserDbId userDbId) {
+        if (acceptPush("userRemoved")) _appCache.removeUser(userDbId);
+    });
 
-    (void) connect(&_commService, &CommService::accountAdded, &_appCache, &AppCache::upsertAccount, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::accountUpdated, &_appCache, &AppCache::upsertAccount, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::accountRemoved, &_appCache, &AppCache::removeAccount, Qt::UniqueConnection);
+    (void) connect(&_commService, &CommService::accountAdded, this, [this](const AccountInfo &info) {
+        if (acceptPush("accountAdded")) _appCache.upsertAccount(info);
+    });
+    (void) connect(&_commService, &CommService::accountUpdated, this, [this](const AccountInfo &info) {
+        if (acceptPush("accountUpdated")) _appCache.upsertAccount(info);
+    });
+    (void) connect(&_commService, &CommService::accountRemoved, this, [this](const AccountDbId accountDbId) {
+        if (acceptPush("accountRemoved")) _appCache.removeAccount(accountDbId);
+    });
 
-    (void) connect(&_commService, &CommService::driveAdded, &_appCache, &AppCache::upsertDrive, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::driveUpdated, &_appCache, &AppCache::upsertDrive, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::driveRemoved, &_appCache, &AppCache::removeDrive, Qt::UniqueConnection);
+    (void) connect(&_commService, &CommService::driveAdded, this, [this](const DriveInfo &info) {
+        if (acceptPush("driveAdded")) _appCache.upsertDrive(info);
+    });
+    (void) connect(&_commService, &CommService::driveUpdated, this, [this](const DriveInfo &info) {
+        if (acceptPush("driveUpdated")) _appCache.upsertDrive(info);
+    });
+    (void) connect(&_commService, &CommService::driveRemoved, this, [this](const DriveDbId driveDbId) {
+        if (acceptPush("driveRemoved")) _appCache.removeDrive(driveDbId);
+    });
 
-    (void) connect(&_commService, &CommService::syncAdded, &_appCache, &AppCache::upsertSync, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::syncUpdated, &_appCache, &AppCache::upsertSync, Qt::UniqueConnection);
-    (void) connect(&_commService, &CommService::syncRemoved, &_appCache, &AppCache::removeSync, Qt::UniqueConnection);
+    (void) connect(&_commService, &CommService::syncAdded, this, [this](const SyncInfo &info) {
+        if (acceptPush("syncAdded")) _appCache.upsertSync(info);
+    });
+    (void) connect(&_commService, &CommService::syncUpdated, this, [this](const SyncInfo &info) {
+        if (acceptPush("syncUpdated")) _appCache.upsertSync(info);
+    });
+    (void) connect(&_commService, &CommService::syncRemoved, this, [this](const SyncDbId syncDbId) {
+        if (acceptPush("syncRemoved")) _appCache.removeSync(syncDbId);
+    });
 
     (void) connect(&_commService, &CommService::errorAdded, this, [this](const ErrorInfo &info) {
+        if (!acceptPush("errorAdded")) {
+            return;
+        }
         if (info.level() == ErrorLevel::Server) {
             _appCache.upsertServerError(info);
             return;
         }
-
         _appCache.upsertSyncError(info);
     });
     (void) connect(&_commService, &CommService::errorRemoved, this, [this](const ErrorDbId errorDbId) {
+        if (!acceptPush("errorRemoved")) {
+            return;
+        }
         if (_appCache.syncError(errorDbId).has_value()) {
             _appCache.removeSyncError(errorDbId);
             return;
         }
-
         if (_appCache.serverError(errorDbId).has_value()) {
             _appCache.removeServerError(errorDbId);
         }
