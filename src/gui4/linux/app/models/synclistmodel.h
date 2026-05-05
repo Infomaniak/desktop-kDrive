4371186113ed290fb93d192acb11e7d08a8a2b79
Romain Galland <romain.galland@infomaniak.com>
2026-05-05 15:31:42 +0200

    refactor(appclientlinux): standardize data types in list models by replacing `int` with `qint32`

--- src/gui4/linux/app/models/synclistmodel.h
+++ src/gui4/linux/app/models/synclistmodel.h
@@ -38,7 +38,7 @@ class SyncListModel : public QAbstractListModel {
         Q_OBJECT
 
     public:
-        enum Roles : int {
+        enum Roles : qint32 {
             DbIdRole = Qt::UserRole + 1,
             DriveDbIdRole,
             DriveIdRole,
@@ -69,9 +69,9 @@ class SyncListModel : public QAbstractListModel {
 
         explicit SyncListModel(AppCache &appCache, MainSelectionStore &mainSelectionStore, QObject *parent = nullptr);
 
-        [[nodiscard]] int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-        [[nodiscard]] QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-        [[nodiscard]] QHash<int, QByteArray> roleNames() const override;
+        [[nodiscard]] qint32 rowCount(const QModelIndex &parent = QModelIndex()) const override;
+        [[nodiscard]] QVariant data(const QModelIndex &index, qint32 role = Qt::DisplayRole) const override;
+        [[nodiscard]] QHash<qint32, QByteArray> roleNames() const override;
 
         Q_INVOKABLE void selectSync(qint64 syncDbId);
 
