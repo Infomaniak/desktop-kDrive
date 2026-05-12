edbad85f20a646db4186328ccec86656363cbef3
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-05-12 12:56:03 +0200

    test: New parsing JSON test for EXCLAPP_GET_FETCHING_APP_LIST

---
+++ src/gui4/macOS/kDriveTests/Parsing/Jobs/ExclusionAppGetFetchingAppListTest.swift
@@ -0,0 +1,60 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+@testable import kDriveCore
+import Testing
+
+@Suite("ExclusionAppGetFetchingAppList Job Parsing Test")
+struct ExclusionAppGetFetchingAppListTest {
+    private let decoder = JSONDecoder()
+
+    // MARK: - Test Data
+
+    var validJobCallbackData: Data {
+        let bundle = Bundle(for: TestBundleMarker.self)
+
+        guard let url = bundle.url(forResource: "EXCLAPP_GET_FETCHING_APP_LIST", withExtension: "json") else {
+            fatalError("Unable to find specified JSON file")
+        }
+
+        return try! Data(contentsOf: url)
+    }
+
+    // MARK: - Parsing Test
+
+    @Test("Successfully parses a valid EXCLAPP_GET_FETCHING_APP_LIST.json")
+    func parseValidJobCallback() throws {
+        // GIVEN
+        let callbackData = validJobCallbackData
+
+        // WHEN
+        let response = try decoder.decode(
+            CallbackMessage<ExclusionAppGetFetchingAppListResponse>.self,
+            from: callbackData
+        )
+
+        // THEN: verify response envelope and decoded map values
+        #expect(response.code == KDC.ExitCode.Ok)
+        #expect(response.cause == KDC.ExitCause.Unknown)
+        #expect(response.id == 1)
+        #expect(response.body.applicationTable.count == 2)
+        #expect(response.body.applicationTable["appId1"] == "applicationName1")
+        #expect(response.body.applicationTable["appId2"] == "applicationName2")
+    }
+}
