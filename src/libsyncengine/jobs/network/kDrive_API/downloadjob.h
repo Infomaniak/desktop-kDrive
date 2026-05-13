7b11c9b306af9316b8fc4c664e61f1d9436b06a8
Luc Guyot <luc.guyot@infomaniak.com>
2026-05-13 14:14:13 +0200

    refactor(complexity): Reduces method cognitive complexity (SonarCloud code smell)

--- src/libsyncengine/jobs/network/kDrive_API/downloadjob.h
+++ src/libsyncengine/jobs/network/kDrive_API/downloadjob.h
@@ -73,6 +73,7 @@ class DownloadJob : public AbstractTokenNetworkJob {
         ExitInfo resolveDownloadNeed();
 
         ExitInfo createLink(const std::string &mimeType, const std::string &data);
+        void handleCreation(bool &fetchFinished, bool &fetchCanceled, bool &fetchError, bool &writeError);
         ExitInfo createFile(std::istream &is, bool &fetchedCanceled);
         bool removeTmpFile();
         ExitInfo moveTmpFile();
