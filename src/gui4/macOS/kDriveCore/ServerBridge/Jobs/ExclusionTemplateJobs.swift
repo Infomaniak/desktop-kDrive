aa87d1ae2658dc668d6f7279b320717ed8e346ec
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-05-13 14:19:39 +0200

    feat: New job EXCLTEMPL_GETEXCLUDED

---
+++ src/gui4/macOS/kDriveCore/ServerBridge/Jobs/ExclusionTemplateJobs.swift
@@ -0,0 +1,43 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import CppInterop
+import Foundation
+import InfomaniakDI
+
+public struct ExclusionTemplateJobs: Sendable {
+    @LazyInjectService private var queryFetcher: XPCQueryFetcherProtocol
+
+    public init() {}
+
+    public func getNameExcluded(name: String) async throws -> Bool {
+        IKLogger.data.log("Query for exclusionTemplateGetExcluded")
+        let query = ExclusionTemplateGetExcludedQuery(name: name)
+        let request = await RequestMessage<ExclusionTemplateGetExcludedQuery>(
+            num: RequestNum.EXCLTEMPL_GETEXCLUDED,
+            body: query
+        )
+
+        let decodedMessage = try await queryFetcher.query(
+            request,
+            responseType: CallbackMessage<ExclusionTemplateGetExcludedResponse>.self
+        )
+
+        return decodedMessage.body.isExcluded
+    }
+}
