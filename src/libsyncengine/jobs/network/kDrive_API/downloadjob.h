01819e418158c9937b8a9a3ba5a56be8832fe4c3
Luc Guyot <luc.guyot@infomaniak.com>
2026-05-13 12:01:11 +0200

    fix(wrong-order-of-argument): Several fixes found by Christophe. In particular, an incorrect ordering of function argument in ApiTranslator

--- src/libsyncengine/jobs/network/kDrive_API/downloadjob.h
+++ src/libsyncengine/jobs/network/kDrive_API/downloadjob.h
@@ -55,7 +55,7 @@ class DownloadJob : public AbstractTokenNetworkJob {
         const RemoteNodeId &remoteNodeId() const { return _fileDownloadInfo.remoteFileId; }
         const SyncPath &localPath() const { return _fileDownloadInfo.localPath; }
 
-        const RemoteNodeId &localNodeId() const { return _localNodeId; }
+        const NodeId &localNodeId() const { return _localNodeId; }
         SyncTime creationTime() const { return _creationTimeOut; }
         SyncTime modificationTime() const { return _modificationTimeOut; }
         [[nodiscard]] int64_t size() const { return _sizeOut; }
