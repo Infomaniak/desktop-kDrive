c41d495e248090fc331d3147ab1aa582ce7a2279
Clément Kunz <clement.kunz@infomaniak.com>
2026-05-27 11:19:27 +0200

    feat(log): persist Linux logs in XDG_STATE_HOME

--- src/libcommon/utility/utility.cpp
+++ src/libcommon/utility/utility.cpp
@@ -1,20 +1,18 @@
-/*
- * Infomaniak kDrive - Desktop
- * Copyright (C) 2023-2026 Infomaniak Network SA
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Infomaniak kDrive - Desktop
+// Copyright (C) 2023-2026 Infomaniak Network SA
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "utility.h"
 #include "utility_base.h"
@@ -1531,7 +1529,7 @@ ExitInfo CommonUtility::deviceTempDirectoryPath(SyncPath &directoryPath) noexcep
     return stdErrorToExitInfo(ec);
 }
 
-#if defined(KD_WINDOWS) || defined(KD_LINUX)
+#if defined(KD_WINDOWS)
 ExitInfo CommonUtility::logDirectoryPath(SyncPath &directoryPath) noexcept {
     // Generate directory path
     if (const auto exitInfo = deviceTempDirectoryPath(directoryPath); !exitInfo) {
--- src/libcommon/utility/utility_linux.cpp
+++ src/libcommon/utility/utility_linux.cpp
@@ -1,20 +1,18 @@
-/*
- * Infomaniak kDrive - Desktop
- * Copyright (C) 2023-2026 Infomaniak Network SA
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Infomaniak kDrive - Desktop
+// Copyright (C) 2023-2026 Infomaniak Network SA
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "utility.h"
 
@@ -26,6 +24,8 @@
 #include <pwd.h>
 
 #include "utility/types.h"
+
+#include <config.h>
 
 namespace KDC {
 
@@ -169,6 +169,39 @@ std::string CommonUtility::fileSystemName(const SyncPath &targetPath) {
     }
 
     return "UNIDENTIFIED";
+}
+
+ExitInfo CommonUtility::logDirectoryPath(SyncPath &directoryPath) noexcept {
+    // XDG Base Directory Specification: use $XDG_STATE_HOME, fallback to ~/.local/state
+    const char *xdgStateHome = getenv("XDG_STATE_HOME");
+    if (xdgStateHome && *xdgStateHome != '\0') {
+        directoryPath = SyncPath(xdgStateHome);
+    } else {
+        const char *homeDir = getenv("HOME");
+        if (!homeDir || *homeDir == '\0') {
+            homeDir = getpwuid(getuid())->pw_dir;
+        }
+        if (!homeDir || *homeDir == '\0') {
+            return {ExitCode::SystemError, ExitCause::NotFound};
+        }
+        directoryPath = SyncPath(homeDir) / ".local" / "state";
+    }
+
+    directoryPath /= Str2SyncName(APPLICATION_NAME);
+    directoryPath /= "logs";
+
+    std::error_code ec;
+    if (!std::filesystem::exists(directoryPath, ec)) {
+        if (ec.value() != 0) {
+            return CommonUtility::stdErrorToExitInfo(ec);
+        }
+
+        if (!std::filesystem::create_directories(directoryPath, ec)) {
+            return CommonUtility::stdErrorToExitInfo(ec);
+        }
+    }
+
+    return ExitCode::Ok;
 }
 
 } // namespace KDC
