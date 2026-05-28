2aabe26e3b3e563381a5bf5bc784ac27608902bc
Herve <herve.eruam@infomaniak.com>
2026-05-28 19:03:33 +0200

    fix(upload-scripts): update hash file names per platform
    Updated hash file names for Windows, macOS, Linux AMD64, and
    Linux ARM64 to match their respective file types, ensuring
    consistency and clarity in release uploads.

--- infomaniak-build-tools/upload-release-to-kDrive.ps1
+++ infomaniak-build-tools/upload-release-to-kDrive.ps1
@@ -171,7 +171,7 @@ if ($os -eq "win") {
     Write-Host " - Windows Files - " # Windows
     $win_files = @(
         "$app.exe",
-        "$app.sha256",
+        "$app.exe.sha256",
         "$app.msi",
         "kDrive.pdb",
         "kDrive_client.pdb",
@@ -186,7 +186,7 @@ if ($os -eq "macos") {
     Write-Host " - macOS Files - " # macOS
     $macos_files = @(
         "$app.pkg",
-        "$app.sha256",
+        "$app.pkg.sha256",
         "$app.zip", # Sparkle zip
         "update-macos-$version.xml", # Sparkle update xml
         "kDrive.dSYM",
@@ -202,7 +202,7 @@ if ($os -eq "linux-amd") {
     Write-Host " - Linux AMD64 Files - " # Linux AMD
     $linux_amd_files = @(
         "$app-amd64.AppImage",
-        "$app-amd64.sha256",
+        "$app-amd64.AppImage.sha256",
         "kDrive.dbg",
         "kDrive_client.dbg",
         "kDrive.src.zip",
@@ -216,7 +216,7 @@ if ($os -eq "linux-arm") {
     Write-Host " - Linux ARM64 Files - " # Linux ARM
     $linux_arm_files = @(
         "$app-arm64.AppImage",
-        "$app-arm64.sha256",
+        "$app-arm64.AppImage.sha256",
         "kDrive.dbg",
         "kDrive_client.dbg",
         "kDrive.src.zip",
