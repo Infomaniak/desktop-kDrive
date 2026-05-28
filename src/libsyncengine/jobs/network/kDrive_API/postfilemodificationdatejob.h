6622ad2b3f3190a11c2160bb093edc84accbfaf5
Raphael OSTIER <raphael.ostier@infomaniak.com>
2026-05-28 13:22:24 +0200

    feat: Add PostFileModificationDateJob for kDrive API updates

---
+++ src/libsyncengine/jobs/network/kDrive_API/postfilemodificationdatejob.h
@@ -0,0 +1,38 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "jobs/network/abstracttokennetworkjob.h"
+
+namespace KDC {
+
+class PostFileModificationDateJob : public AbstractTokenNetworkJob {
+    public:
+        PostFileModificationDateJob(DriveDbId driveDbId, const NodeId &nodeId, uint64_t lastModifiedAt);
+
+    private:
+        virtual std::string getSpecificUrl() override;
+        virtual void setQueryParameters(Poco::URI &uri) override;
+        inline virtual ExitInfo setData() override { return ExitCode::Ok; }
+
+        NodeId _nodeId;
+        uint64_t _lastModifiedAt;
+};
+
+} // namespace KDC
