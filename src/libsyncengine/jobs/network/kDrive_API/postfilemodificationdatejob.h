661c8307d55904560267fffef36302ea4eeb8e50
Luc Guyot <luc.guyot@infomaniak.com>
2026-06-01 16:39:58 +0200

    fix(post-file-modification-date-job): Adapts PostFileModificationDateJob to the API changes done in AbstractTokenNetworkJob (fewer constructor arguments)

--- src/libsyncengine/jobs/network/kDrive_API/postfilemodificationdatejob.h
+++ src/libsyncengine/jobs/network/kDrive_API/postfilemodificationdatejob.h
@@ -24,16 +24,14 @@ namespace KDC {
 
 class PostFileModificationDateJob : public AbstractTokenNetworkJob {
     public:
-        PostFileModificationDateJob(DriveDbId driveDbId, const NodeId &nodeId, SyncTime lastModifiedAt);
-
-        [[nodiscard]] SyncTime lastModifiedAt() const { return _lastModifiedAtOut; }
+        PostFileModificationDateJob(DriveDbId driveDbId, RemoteNodeId nodeId, SyncTime lastModifiedAt);
 
     private:
         std::string getSpecificUrl() override;
         ExitInfo setData() override;
         ExitInfo handleResponse(std::istream &is) override;
 
-        NodeId _nodeId;
+        RemoteNodeId _nodeId;
         SyncTime _lastModifiedAt = 0;
         SyncTime _lastModifiedAtOut = 0;
 };
