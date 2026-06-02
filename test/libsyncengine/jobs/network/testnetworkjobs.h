e2ef9737d714de9bba30b4bf8a66d6f671b88d0c
Raphael OSTIER <raphael.ostier@infomaniak.com>
2026-06-02 11:00:48 +0200

    feat: Refactor DownloadJob and add checksum mismatch test

--- test/libsyncengine/jobs/network/testnetworkjobs.h
+++ test/libsyncengine/jobs/network/testnetworkjobs.h
@@ -38,7 +38,7 @@ class TestNetworkJobs : public CppUnit::TestFixture, public TestBase {
         CPPUNIT_TEST(testDelete);
         CPPUNIT_TEST(testDownload);
         CPPUNIT_TEST(testDownloadAborted);
-        CPPUNIT_TEST(testGetAvatar);
+        CPPUNIT_TEST(testDownloadChecksumMismatch);
         CPPUNIT_TEST(testGetDriveList);
         CPPUNIT_TEST(testGetFileInfo);
         CPPUNIT_TEST(testGetFileList);
@@ -84,6 +84,7 @@ class TestNetworkJobs : public CppUnit::TestFixture, public TestBase {
         void testCopyToDir();
         void testDelete();
         void testDownload();
+        void testDownloadChecksumMismatch();
         void testDownloadAborted();
         void testGetAvatar();
         void testGetDriveList();
