3d1be02cae5b71e8a6d87e202ad224663ef88143
Christophe Larchier <christophe.larchier@infomaniak.com>
2026-06-04 09:30:56 +0200

    fix: address comment

--- extensions/MacOSX/kDriveLiteSync/Extension/xpcService.m
+++ extensions/MacOSX/kDriveLiteSync/Extension/xpcService.m
@@ -59,11 +59,11 @@ const NSString *timeoutBlockKey = @"timeoutBlock";
 
 - (void)initOpenWhiteListThumbnailSet {
     _defaultOpenWhiteListThumbnailSet =
-            [NSSet setWithObjects: @"com.apple.quicklook.satellite", // Quicklook
-                                   @"com.apple.quicklook.thumbnail.ImageExtension", // Quicklook Thumbnail
-                                   @"com.apple.quicklook.thumbnail.AudiovisualExtension", // Quicklook Thumbnail
-                                   @"com.apple.quicklook.ThumbnailsAgent", // Quicklook
-                                   nil];
+            [NSSet setWithObjects:@"com.apple.quicklook.satellite", // Quicklook
+                                  @"com.apple.quicklook.thumbnail.ImageExtension", // Quicklook Thumbnail
+                                  @"com.apple.quicklook.thumbnail.AudiovisualExtension", // Quicklook Thumbnail
+                                  @"com.apple.quicklook.ThumbnailsAgent", // Quicklook
+                                  nil];
 }
 
 - (void)initOpenWhiteListSet {
