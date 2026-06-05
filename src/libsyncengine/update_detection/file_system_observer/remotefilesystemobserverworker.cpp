8b9f68b7a4b902904dc813aa16c8e1b1e508a9db
Luc Guyot <luc.guyot@infomaniak.com>
2026-06-05 11:53:43 +0200

    fix(remote-file-system-observer): Remove user-blacklisted folders from the special remote folders list (Hervé's CR comment)

--- src/libsyncengine/update_detection/file_system_observer/remotefilesystemobserverworker.cpp
+++ src/libsyncengine/update_detection/file_system_observer/remotefilesystemobserverworker.cpp
@@ -158,6 +158,8 @@ ExitInfo RemoteFileSystemObserverWorker::processEvents(const std::vector<RemoteN
     const ForcedUpdate updateFlag = initializing() || updating() ? ForcedUpdate::Asked : ForcedUpdate::None;
 
     for (const auto &remoteDirId: specialFoldersRemoteIds) {
+        if (_blackList.contains(remoteDirId)) continue;
+
         bool hasChanges = false;
         if (const auto exitInfo = checkIfRemoteDirHasChanges(remoteDirId, updateFlag, longPollJobs, hasChanges); !exitInfo)
             return exitInfo;
@@ -470,8 +472,6 @@ ExitInfo RemoteFileSystemObserverWorker::initWithCursor() {
     }
     for (const auto &specialFolderRemoteId: specialFoldersRemoteIds) {
         if (stopAsked()) return ExitCode::Ok;
-
-        if (_blackList.contains(specialFolderRemoteId)) continue;
 
         if (const auto exitInfo = updateSpecialFolderItem(specialFolderRemoteId); !exitInfo) return exitInfo;
 
@@ -1206,7 +1206,8 @@ ExitInfo RemoteFileSystemObserverWorker::getSpecialFoldersRemoteIds(std::vector<
         return exitInfo;
 
     specialFoldersRemoteIds = {userPrivateFolderRemoteId, commonDocumentsFolderRemoteId, sharedFolderRemoteId};
-    (void) std::erase_if(specialFoldersRemoteIds, [](const std::string_view s) { return s.empty(); });
+    (void) std::erase_if(specialFoldersRemoteIds,
+                         [this](const auto &remoteNodeId) { return remoteNodeId.empty() || _blackList.contains(remoteNodeId); });
 
     return ExitCode::Ok;
 }
