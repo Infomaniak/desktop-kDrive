b1bba4771e4f0d8c73b726a61a2e0e0bbf57c6c2
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-06-09 14:59:02 +0200

    feat: Add Cells for OutlineView

---
+++ src/gui4/macOS/kDrive/UI/Views/SynchroConfiguration/Components/FileTree/FileTreeCells.swift
@@ -0,0 +1,218 @@
+/*
+ Infomaniak kDrive - Desktop
+ Copyright (C) 2023-2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import AppKit
+import UniformTypeIdentifiers
+
+enum FileTreeIcons {
+    static let folder = NSWorkspace.shared.icon(for: .folder)
+    static let file = NSWorkspace.shared.icon(for: .data)
+}
+
+final class FileTreeCheckboxCell: NSTableCellView {
+    private let checkbox = NSButton()
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        checkbox.setButtonType(.switch)
+        checkbox.allowsMixedState = true
+        checkbox.title = ""
+        checkbox.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(checkbox)
+
+        NSLayoutConstraint.activate([
+            checkbox.centerXAnchor.constraint(equalTo: centerXAnchor),
+            checkbox.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(state: NSControl.StateValue, isEnabled: Bool, target: AnyObject, action: Selector) {
+        checkbox.state = state
+        checkbox.isEnabled = isEnabled
+        checkbox.target = target
+        checkbox.action = action
+    }
+}
+
+final class FileTreeNameCell: NSTableCellView {
+    private let icon = NSImageView()
+    private let label = NSTextField(labelWithString: "")
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        icon.imageScaling = .scaleProportionallyUpOrDown
+        icon.translatesAutoresizingMaskIntoConstraints = false
+
+        label.lineBreakMode = .byTruncatingMiddle
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: NSFont.systemFontSize)
+
+        addSubview(icon)
+        addSubview(label)
+
+        imageView = icon
+        textField = label
+
+        NSLayoutConstraint.activate([
+            icon.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2),
+            icon.centerYAnchor.constraint(equalTo: centerYAnchor),
+            icon.widthAnchor.constraint(equalToConstant: 16),
+            icon.heightAnchor.constraint(equalToConstant: 16),
+
+            label.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 6),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with item: FileTreeItem) {
+        icon.image = item.isFolder ? FileTreeIcons.folder : FileTreeIcons.file
+        label.stringValue = item.name
+        label.textColor = item.isEnabled ? .labelColor : .disabledControlTextColor
+        alphaValue = item.isEnabled ? 1 : 0.5
+    }
+}
+
+final class FileTreeSizeCell: NSTableCellView {
+    private let label = NSTextField(labelWithString: "")
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: NSFont.systemFontSize)
+
+        addSubview(label)
+        textField = label
+
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with item: FileTreeItem) {
+        if let size = item.size {
+            label.stringValue = ByteCountFormatter.string(fromByteCount: size, countStyle: .file)
+        } else {
+            label.stringValue = "-"
+        }
+        label.textColor = item.isEnabled ? .secondaryLabelColor : .disabledControlTextColor
+    }
+}
+
+final class FileTreeShimmerCell: NSTableCellView {
+    private let shimmer = ShimmerView()
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        shimmer.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(shimmer)
+
+        NSLayoutConstraint.activate([
+            shimmer.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2),
+            shimmer.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            shimmer.centerYAnchor.constraint(equalTo: centerYAnchor),
+            shimmer.heightAnchor.constraint(equalToConstant: 12)
+        ])
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class ShimmerView: NSView {
+    private let gradientLayer = CAGradientLayer()
+
+    private static let animationKey = "shimmer"
+
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        wantsLayer = true
+        layer?.cornerRadius = 6
+        layer?.masksToBounds = true
+
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        gradientLayer.locations = [0, 0.5, 1]
+        layer?.addSublayer(gradientLayer)
+        updateColors()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layout() {
+        super.layout()
+        gradientLayer.frame = bounds
+    }
+
+    override func viewDidChangeEffectiveAppearance() {
+        super.viewDidChangeEffectiveAppearance()
+        updateColors()
+    }
+
+    override func viewDidMoveToWindow() {
+        super.viewDidMoveToWindow()
+        if window == nil {
+            gradientLayer.removeAnimation(forKey: Self.animationKey)
+        } else {
+            startAnimating()
+        }
+    }
+
+    private func updateColors() {
+        effectiveAppearance.performAsCurrentDrawingAppearance {
+            let base = NSColor.quaternaryLabelColor.cgColor
+            let highlight = NSColor.tertiaryLabelColor.cgColor
+            gradientLayer.colors = [base, highlight, base]
+        }
+    }
+
+    private func startAnimating() {
+        guard gradientLayer.animation(forKey: Self.animationKey) == nil else { return }
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [-1.0, -0.5, 0.0]
+        animation.toValue = [1.0, 1.5, 2.0]
+        animation.duration = 1.2
+        animation.repeatCount = .infinity
+        gradientLayer.add(animation, forKey: Self.animationKey)
+    }
+}
