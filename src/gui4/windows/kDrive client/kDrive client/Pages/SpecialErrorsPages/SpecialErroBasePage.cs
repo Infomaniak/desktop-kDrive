45a743175cbfcd6f5d466235deaf9a9977b69d1b
Herve <herve.eruam@infomaniak.com>
2026-06-11 16:12:16 +0200

    test: reactivate tests and adjust dependencies
    Reactivated previously commented-out tests in `testnetworkjobs.h`
    to improve unit test coverage. Adjusted `testGetInfoUserTrialsOn401Error`
    in `testnetworkjobs.cpp` to refine `_apiToken` handling.
    
    Added `using Microsoft.UI.Xaml;` in `SpecialErroBasePage.cs` to
    include necessary UI functionalities. Updated namespace in `test.cpp`
    to ensure proper test registration under `KDC`.

--- src/gui4/windows/kDrive client/kDrive client/Pages/SpecialErrorsPages/SpecialErroBasePage.cs
+++ src/gui4/windows/kDrive client/kDrive client/Pages/SpecialErrorsPages/SpecialErroBasePage.cs
@@ -25,7 +25,6 @@ using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Infomaniak.kDrive.Pages
