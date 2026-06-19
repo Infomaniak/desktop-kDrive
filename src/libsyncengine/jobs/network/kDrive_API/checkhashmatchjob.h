48362bee947843c536435ca55e5ecbd507939908
Raphael OSTIER <raphael.ostier@infomaniak.com>
2026-06-19 11:44:49 +0200

    feat: Refactor CheckHashMatchJob and fix UploadJob

--- src/libsyncengine/jobs/network/kDrive_API/checkhashmatchjob.h
+++ src/libsyncengine/jobs/network/kDrive_API/checkhashmatchjob.h
@@ -37,6 +37,7 @@ class CheckHashMatchJob : public AbstractTokenNetworkJob {
         std::string getSpecificUrl() override;
         ExitInfo getFileSize(const SyncPath &path, int64_t &size);
         ExitInfo runJob() noexcept override;
+        ExitInfo remoteHashMatch() noexcept;
 
         SyncPath _filePath;
 
