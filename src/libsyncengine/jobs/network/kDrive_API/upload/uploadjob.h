dcef794762563d6e4f3b867f3237fb5bed576074
Raphael OSTIER <raphael.ostier@infomaniak.com>
2026-06-22 11:33:23 +0200

    chore: sonarQube

--- src/libsyncengine/jobs/network/kDrive_API/upload/uploadjob.h
+++ src/libsyncengine/jobs/network/kDrive_API/upload/uploadjob.h
@@ -80,7 +80,7 @@ class UploadJob : public AbstractTokenNetworkJob {
 
         bool _shouldUpload = true;
         bool _isHydrated = true;
-        int64_t _remoteSize;
+        int64_t _remoteSize = -1;
 
         const std::shared_ptr<Vfs> _vfs;
 };
