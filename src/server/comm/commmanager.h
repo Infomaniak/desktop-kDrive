f9770924efcba5fb79dd2866d1bf4c1be6376f1d
Romain Galland <romain.galland@infomaniak.com>
2026-06-23 13:46:57 +0200

    feat(server): add support for retrieving GUI communication port on Linux and Windows platforms
    - Introduced `tryGetGUICommPort` in `CommManager` to retrieve the GUI communication port.
    - Updated client launch logic to pass the communication port as an argument on Linux builds.
    
    Co-authored-by: Herve <herve.eruam@infomaniak.com>

--- src/server/comm/commmanager.h
+++ src/server/comm/commmanager.h
@@ -58,6 +58,10 @@ class CommManager : public std::enable_shared_from_this<CommManager> {
         void sendGuiSignal(const std::shared_ptr<AbstractGuiJob> signal);
         bool hasActiveGuiConnection();
 
+#if defined(KD_WINDOWS) || defined(KD_LINUX)
+        int32_t tryGetGUICommPort() const;
+#endif
+
     private:
         // AppServer maps
         AppServer &_appServer;
@@ -69,7 +73,7 @@ class CommManager : public std::enable_shared_from_this<CommManager> {
         std::shared_ptr<AbstractCommServer> _guiCommServer;
 
         std::unique_ptr<GuiJobFactory> _guiJobFactory;
-        std::recursive_mutex _mutex;
+        mutable std::recursive_mutex _mutex;
 
 #if defined(KD_MACOS) || defined(KD_WINDOWS)
         // Execute a command received from an extension, which does not require an answer
