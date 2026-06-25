91e975540fd6347ca40653f0029b72eb3fb4b995
Christophe Larchier <christophe.larchier@infomaniak.com>
2026-06-25 15:27:36 +0200

    fix: issue

--- src/libsyncengine/update_detection/update_detector/updatetreeworker.cpp
+++ src/libsyncengine/update_detection/update_detector/updatetreeworker.cpp
@@ -1470,6 +1470,8 @@ ExitCode UpdateTreeWorker::tryToMergeTmpNode(const std::shared_ptr<Node> tmpNode
             break;
         }
     }
+
+    return ExitCode::Ok;
 }
 
 ExitCode UpdateTreeWorker::updateTmpNode(const std::shared_ptr<Node> tmpNode) {
