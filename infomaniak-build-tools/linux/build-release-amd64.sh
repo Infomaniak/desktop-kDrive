f6a919e3fc07b5b4010de6171d0d2f73295f7c7a
Romain Galland <romain.galland@infomaniak.com>
2026-06-26 14:42:03 +0200

    fix(release): use the shell PATH lookup instead of hardcoded path

--- infomaniak-build-tools/linux/build-release-amd64.sh
+++ infomaniak-build-tools/linux/build-release-amd64.sh
@@ -192,7 +192,7 @@ package_release() {
 
   cp "$QTDIR/lib/libQt6SerialPort.so.6" "$app_dir/usr/lib/"
 
-  "$HOME/desktop-setup/linuxdeploy-x86_64.AppImage" --appdir "$app_dir" -e "$app_dir/usr/bin/kDrive" -i "$app_dir/kdrive-win.png" -d "$app_dir/usr/share/applications/kDrive_client.desktop" --plugin qt --output appimage -v0
+  linuxdeploy --appdir "$app_dir" -e "$app_dir/usr/bin/kDrive" -i "$app_dir/kdrive-win.png" -d "$app_dir/usr/share/applications/kDrive_client.desktop" --plugin qt --output appimage -v0
 
   full_version="$(grep "KDRIVE_VERSION_FULL" "$build_dir/build/version.h" | awk '{print $3}')"
   app_name="kDrive-${full_version}-amd64.AppImage"
