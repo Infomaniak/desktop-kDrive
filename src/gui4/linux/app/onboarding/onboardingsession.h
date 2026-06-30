185514f62bcfe9b4a7c33acc7d0da158e70fe7ff
Romain Galland <romain.galland@infomaniak.com>
2026-06-30 17:42:57 +0200

    refactor(linux): integrate `OnboardingSyncCreationCoordinator` into `AppClientLinux` and extend onboarding UI with sync and completion views

--- src/gui4/linux/app/onboarding/onboardingsession.h
+++ src/gui4/linux/app/onboarding/onboardingsession.h
@@ -22,6 +22,7 @@
 #include "app/onboarding/driveselectioncontroller.h"
 #include "app/onboarding/onboardingflowcontroller.h"
 #include "app/onboarding/onboardinglogincoordinator.h"
+#include "app/onboarding/onboardingsynccreationcoordinator.h"
 
 #include <QObject>
 
@@ -33,6 +34,7 @@ namespace KDC {
 class AppCache;
 class AvailableDrivesModel;
 class CommService;
+class ServiceEventBus;
 class UserService;
 
 /**
@@ -53,7 +55,8 @@ class OnboardingSession final : public QObject {
             DriveSelection,
         };
 
-        explicit OnboardingSession(AppCache &appCache, CommService &commService, UserService &userService, EntryPoint entryPoint,
+        explicit OnboardingSession(AppCache &appCache, CommService &commService, UserService &userService,
+                                   ServiceEventBus &serviceEventBus, EntryPoint entryPoint,
                                    std::optional<UserDbId> selectedUserDbId, uint64_t generation, QObject *parent = nullptr);
 
         [[nodiscard]] OnboardingFlowController *flowController() { return &_flowController; }
@@ -71,6 +74,7 @@ class OnboardingSession final : public QObject {
         OnboardingFlowController _flowController;
         OnboardingLoginCoordinator _loginCoordinator;
         DriveSelectionController _driveSelectionController;
+        OnboardingSyncCreationCoordinator _syncCreationCoordinator;
         const uint64_t _generation;
 };
 
