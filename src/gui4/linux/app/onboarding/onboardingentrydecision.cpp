0e33ec445e4bae120f142e3dcb51fad7c427a41b
Romain Galland <romain.galland@infomaniak.com>
2026-07-06 09:59:44 +0200

    feat(gui): add `DriveSelectionController` and `OnboardingEntryDecision` for improved onboarding flow logic

---
+++ src/gui4/linux/app/onboarding/onboardingentrydecision.cpp
@@ -0,0 +1,47 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "onboardingentrydecision.h"
+
+#include "app/cache/appcache.h"
+
+#include <ranges>
+
+namespace KDC {
+
+OnboardingEntryDecision determineOnboardingEntry(const AppCache &appCache) {
+    if (!appCache.driveContexts().empty()) {
+        return {};
+    }
+
+    const auto users = appCache.users();
+    const auto connectedUserIt = std::ranges::find_if(users, [](const UserInfo &user) { return user.connected(); });
+    if (connectedUserIt == users.end()) {
+        return {
+                .entryPoint = OnboardingEntryDecision::EntryPoint::Login,
+                .selectedUserDbId = std::nullopt,
+        };
+    }
+
+    return {
+            .entryPoint = OnboardingEntryDecision::EntryPoint::DriveSelection,
+            .selectedUserDbId = connectedUserIt->dbId(),
+    };
+}
+
+} // namespace KDC
---
+++ src/gui4/linux/app/onboarding/onboardingentrydecision.h
@@ -0,0 +1,43 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "libcommon/utility/types.h"
+
+#include <cstdint>
+#include <optional>
+
+namespace KDC {
+
+class AppCache;
+
+struct OnboardingEntryDecision {
+        enum class EntryPoint : uint8_t {
+            Inactive,
+            Login,
+            DriveSelection,
+        };
+
+        EntryPoint entryPoint{EntryPoint::Inactive};
+        std::optional<UserDbId> selectedUserDbId;
+};
+
+[[nodiscard]] OnboardingEntryDecision determineOnboardingEntry(const AppCache &appCache);
+
+} // namespace KDC
