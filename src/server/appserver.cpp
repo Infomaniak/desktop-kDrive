1d4ae44038dccea7e717283867b63a196e77e77d
Romain Galland <romain.galland@infomaniak.com>
2026-07-08 09:53:25 +0200

    refactor(server): reorder `parseOptions` call and remove redundant authorization check

--- src/server/appserver.cpp
+++ src/server/appserver.cpp
@@ -218,17 +218,12 @@ void AppServer::init() {
     setWindowIcon(_theme->applicationIcon());
     setApplicationVersion(QString::fromStdString(_theme->version()));
 
-    parseOptions(_arguments);
-    if (!_authorizationCodeStr.isEmpty()) {
-        std::cout << "Authorization code received";
-        return;
-    }
-
     // Setup logging with default parameters
     if (!initLogging()) {
         throw std::runtime_error("Unable to init logging.");
     }
 
+    parseOptions(_arguments);
     if (_helpAsked || _versionAsked || _clearSyncNodesAsked || _clearKeychainKeysAsked) {
         LOG_INFO(_logger, "Command line options processed");
         return;
