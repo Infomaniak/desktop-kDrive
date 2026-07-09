492ae80737359a852ac363865579e74a5b24f9c1
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-07-09 17:27:19 +0200

    feat(macOS UI): Increase search file icons (#2119)
    * chore: Use Material for search bar
    * feat: Make icon bigger
    * chore: Format code

--- src/gui4/macOS/kDrive/UI/Views/MainWindow/Search/SearchResultRowView.swift
+++ src/gui4/macOS/kDrive/UI/Views/MainWindow/Search/SearchResultRowView.swift
@@ -30,9 +30,9 @@ struct SearchResultRowView: View {
     }
 
     var body: some View {
-        HStack(alignment: .firstTextBaseline) {
+        HStack(alignment: .center) {
             FileTypeView(fileTypeRepresentation: file.fileTypeRepresentation)
-                .frame(size: AppIconSize.iconSize12)
+                .frame(size: AppIconSize.iconSize16)
                 .accessibilityHidden(true)
 
             VStack(alignment: .leading) {
@@ -54,10 +54,12 @@ struct SearchResultRowView: View {
                     .accessibilityHidden(true)
             }
         }
-        .padding(.vertical, AppPadding.padding8)
-        .padding(.horizontal, AppPadding.padding8)
-        .background(isHovered ? ColorToken.Surface.secondary.asColor : Color.clear, in: .rect(cornerRadius: AppRadius.radius4))
-        .contentShape(Rectangle())
+        .padding(AppPadding.padding8)
+        .background(
+            isHovered ? ColorToken.Surface.secondary.asColor : Color.clear,
+            in: .rect(cornerRadius: 8)
+        )
+        .contentShape(.rect)
         .accessibilityHint(file.isAvailableLocally ? KDriveLocalizable.buttonOpenInFinder : KDriveLocalizable.buttonOpenInBrowser)
         .onHover { hovering in
             isHovered = hovering
