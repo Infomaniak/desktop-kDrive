c3c6ce0e332199a4eb5ce6968f2fc82df4e10cb2
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-07-15 10:07:25 +0200

    feat: Pre-install script to stop new GUI4 mac client as well

--- admin/osx/pre_install.sh.cmake
+++ admin/osx/pre_install.sh.cmake
@@ -2,5 +2,7 @@
 
 # kill the old version. see issue #2044
 killall @APPLICATION_EXECUTABLE@
+# also stop the redesigned (gui4) macOS GUI, which runs as a separate process
+killall @APPLICATION_EXECUTABLE@.gui
 
 exit 0
