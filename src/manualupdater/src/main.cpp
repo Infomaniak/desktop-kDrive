e5a5b7c8e35897f210dcc3555b4e3f491ec0b6a1
Lawrence Leitgib <lawrence.leitgib@infomaniak.com>
2026-07-15 09:23:32 +0200

    feat: enhance kDrive Recovery Updater with database version selection and logging

--- src/manualupdater/src/main.cpp
+++ src/manualupdater/src/main.cpp
@@ -1,11 +1,28 @@
 #include <QApplication>
 #include "MainWindow.h"
+#include "libcommonserver/utility/utility.h"
+#include "libcommonserver/log/log.h"
+#include "utility/types.h"
+#include "utility/utility.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     QApplication::setApplicationName(QStringLiteral("kDrive Recovery Updater"));
     QApplication::setOrganizationName(QStringLiteral("Infomaniak"));
+
+    KDC::SyncPath logDirPath;
+    if (const auto exitInfo = KDC::CommonUtility::logDirectoryPath(logDirPath); !exitInfo) {
+        return 1;
+    }
+
+    const auto logFilePath = logDirPath / KDC::Utility::logFileNameWithTime();
+    if (!KDC::Log::instance(Path2WStr(logFilePath))) {
+        return 1;
+    }
+    // test the logger
+    LOG_INFO(KDC::Log::instance()->getLogger(), "kDrive Recovery Updater started");
+
 
     KDUpdater::MainWindow window;
     window.show();
