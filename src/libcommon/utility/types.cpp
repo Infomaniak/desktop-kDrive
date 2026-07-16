73ce1ef474b472faab220b40d651ed4fa68875f0
Herve <herve.eruam@infomaniak.com>
2026-07-16 11:41:05 +0200

    fix: add missing toString

--- src/libcommon/utility/types.cpp
+++ src/libcommon/utility/types.cpp
@@ -736,6 +736,8 @@ std::string toString(const AppStateKey e) {
             return "AppUid";
         case AppStateKey::NoUpdate:
             return "NoUpdate";
+        case AppStateKey::ShowV4Onboarding:
+            return "ShowV4Onboarding";
         case AppStateKey::Unknown:
             return "Unknown";
         default:
