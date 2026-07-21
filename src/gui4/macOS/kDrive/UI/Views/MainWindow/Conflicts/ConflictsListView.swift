45929b93f9d846aac0d1ba4c35cc99bb7fe6c4dc
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-07-21 13:33:28 +0200

    fix(individualConflictResolutionPage): Add matomo events

--- src/gui4/macOS/kDrive/UI/Views/MainWindow/Conflicts/ConflictsListView.swift
+++ src/gui4/macOS/kDrive/UI/Views/MainWindow/Conflicts/ConflictsListView.swift
@@ -20,6 +20,7 @@ import kDriveCore
 import kDriveCoreUI
 import kDriveResources
 import SwiftUI
+import InfomaniakDI
 
 struct ConflictsToResolve: Sendable, Identifiable {
     var id: String {
@@ -30,6 +31,8 @@ struct ConflictsToResolve: Sendable, Identifiable {
 }
 
 struct ConflictsListView: View {
+    @LazyInjectService private var matomo: MatomoUtils
+    
     @State private var search = ""
     @State private var isShowingVersionSelectorSheet: ConflictsToResolve?
 
@@ -62,6 +65,7 @@ struct ConflictsListView: View {
             Section {
                 ForEach(filteredErrors) { error in
                     ConflictCellView(path: error.metadata.path) {
+                        matomo.track(eventWithCategory: .individualConflictResolutionPage, name: "manageSingleConflict")
                         resolveConflict(error)
                     }
                 }
@@ -97,6 +101,7 @@ struct ConflictsListView: View {
     }
 
     private func startConflictsResolution() {
+        matomo.track(eventWithCategory: .individualConflictResolutionPage, name: "startChoices")
         isShowingVersionSelectorSheet = ConflictsToResolve(errors: errors)
     }
 
