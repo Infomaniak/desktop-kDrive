5f8f73f07eb25a60356257b4f5a3b886c021e330
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2026-07-21 16:56:51 +0200

    feat(macOS): New dedicated mac sentry project

--- src/gui4/macOS/kDriveCore/Sentry/SentryService.swift
+++ src/gui4/macOS/kDriveCore/Sentry/SentryService.swift
@@ -35,7 +35,7 @@ public final class SentryService {
 
     public func initSentry() {
         SentrySDK.start { options in
-            options.dsn = "https://b6784e934228fe7683d206898d9d0f1e@sentry-desktop.infomaniak.com/6"
+            options.dsn = "https://8eb11fdf352fd4ea3db7d07ad21c1d6f@sentry-desktop.infomaniak.com/8"
             options.tracePropagationTargets = []
             options.enableNetworkTracking = false
             options.enableNetworkBreadcrumbs = false
