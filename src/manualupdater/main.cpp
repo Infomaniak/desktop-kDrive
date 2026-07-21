35630c5541aff9062626d2e37943eb43acf1c6f9
Lawrence Leitgib <lawrence.leitgib@infomaniak.com>
2026-07-21 13:18:21 +0200

    refactor: add preemptive check for kDrive process before updater execution

--- src/manualupdater/main.cpp
+++ src/manualupdater/main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include "updaterdata.h"
+#include "processchecker.h"
+
 #include "libcommon/utility/utility.h"
 #include "libcommonserver/utility/utility.h"
 #include "libcommonserver/log/log.h"
+
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -22,6 +26,30 @@ int main(int argc, char *argv[]) {
     {
         using namespace KDC; // Required: in Release mode the LOG macro calls a KDC method without a ::KDC qualifier.
         LOG_INFO(Log::instance()->getLogger(), "kDrive Recovery Updater started");
+    }
+
+    // Preemptive check: kDrive must not be running so its DB isn't locked.
+    if (KDUpdater::ProcessChecker::isKDriveRunning()) {
+        {
+            using namespace KDC; // same as above
+            LOG_INFO(Log::instance()->getLogger(), "kDrive is running");
+        }
+
+        const auto reply = QMessageBox::question(nullptr, QObject::tr("kDrive is running"),
+                                                 QObject::tr("kDrive must be closed before using the Recovery Updater. "
+                                                             "Do you want to close it now?"),
+                                                 QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+        if (reply == QMessageBox::Yes) {
+            if (QString errorMsg; !KDUpdater::ProcessChecker::terminateKDrive(errorMsg)) {
+                {
+                    using namespace KDC; // same as above
+                    LOGW_ERROR(Log::instance()->getLogger(), L"Failed to terminate kDrive: " << errorMsg.toStdWString());
+                }
+                return 1;
+            }
+        } else {
+            return 0;
+        }
     }
 
     KDC::UpdaterData updaterData;
