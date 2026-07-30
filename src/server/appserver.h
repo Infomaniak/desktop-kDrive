f1b4c7c14774c7c6ba6f4d4e833c024c7d49c35a
Luc Guyot <luc.guyot@infomaniak.com>
2026-07-30 15:54:47 +0200

    fix(app-server): Fixes compile error on Windows and ensures SyncDb is appropriately destroyed once the corresponding sync is removed from syncMap

--- src/server/appserver.h
+++ src/server/appserver.h
@@ -332,8 +332,8 @@ class AppServer : public SharedTools::QtSingleApplication {
                                           std::unordered_set<SyncDbId> &startedSyncDbIds);
         [[nodiscard]] ExitInfo processMigratedSyncOnceConnected(UserDbId userDbId, DriveId driveId, Sync &sync,
                                                                 QSet<QString> &blackList, bool &syncUpdated);
-        [[nodiscard]] static bool shouldStopVfs([[maybe_unused]] const std::optional<SyncPath> syncDbPath,
-                                                const std::shared_ptr<const Vfs> vfs);
+        [[nodiscard]] bool shouldStopVfs([[maybe_unused]] const std::optional<SyncPath> syncDbPath,
+                                         const std::shared_ptr<const Vfs> vfs);
         virtual void sendUserAdded(const User &userInfo) const;
         virtual void sendUserUpdated(const User &userInfo) const;
         virtual void sendUserStatusChanged(UserDbId userDbId, bool connected, const QString &connexionError) const;
