0f817305f6cd074d04ad1477d43b59d06a1f2870
Luc Guyot <luc.guyot@infomaniak.com>
2026-07-31 11:50:32 +0200

    feat(parms-db): Introduces the column toDelete in ParmDb with a view to safely handle interrupted synchronization deletion

--- src/libparms/db/parmsdb.cpp
+++ src/libparms/db/parmsdb.cpp
@@ -2667,6 +2667,7 @@ void ParmsDb::fillSyncWithQueryResult(Sync &sync, const char *requestId) {
     sync.setListingCursor(strResult, int64Result);
 
     int32_t toDeleteResult{0};
+
     LOG_IF_FAIL(queryIntValue(requestId, 15, toDeleteResult));
     sync.setToDelete(static_cast<bool>(toDeleteResult));
 }
@@ -2820,6 +2821,7 @@ bool ParmsDb::selectAllSyncs(const DriveDbId driveDbId, std::vector<Sync> &syncL
         LOG_IF_FAIL(queryStringValue(SELECT_ALL_SYNCS_BY_DRIVE_REQUEST_ID, 12, listingCursor));
         int64_t listingCursorTimestamp;
         LOG_IF_FAIL(queryInt64Value(SELECT_ALL_SYNCS_BY_DRIVE_REQUEST_ID, 13, listingCursorTimestamp));
+
         int32_t toDelete = 0;
         LOG_IF_FAIL(queryIntValue(SELECT_ALL_SYNCS_BY_DRIVE_REQUEST_ID, 14, toDelete));
 
