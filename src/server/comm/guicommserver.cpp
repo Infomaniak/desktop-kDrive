0c3d6836386ec761e4d15b40d317332ee01744c3
Lawrence Leitgib <lawrence.leitgib@infomaniak.com>
2026-08-04 08:51:42 +0200

    fix: implement isReadable() method for improved TLS channel data handling

--- src/server/comm/guicommserver.cpp
+++ src/server/comm/guicommserver.cpp
@@ -94,9 +94,9 @@ bool GuiCommChannel::containsCompleteMessage(const CommString &message, size_t &
 }
 
 void GuiCommChannel::fetchDataToBuffer() {
-    while (bytesAvailable() > 0) {
+    while (isReadable()) {
         CommChar data[1024];
-        if (uint64_t charRead = readData(data, (std::min) (bytesAvailable(), static_cast<uint64_t>(1024))); charRead > 0) {
+        if (uint64_t charRead = readData(data, 1024); charRead > 0) {
             const std::scoped_lock lock(_readBufferMutex);
             _readBuffer.append(data, charRead);
         } else {
