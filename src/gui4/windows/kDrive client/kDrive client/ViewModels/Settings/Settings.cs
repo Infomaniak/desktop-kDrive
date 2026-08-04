042c3ca157790317c3b2365288b78bafb78e2727
Raphael OSTIER <raphael.ostier@infomaniak.com>
2026-08-04 10:12:35 +0200

    feat: Add `AskBeforeDelete` property and update related logic

--- src/gui4/windows/kDrive client/kDrive client/ViewModels/Settings/Settings.cs
+++ src/gui4/windows/kDrive client/kDrive client/ViewModels/Settings/Settings.cs
@@ -123,7 +123,7 @@ namespace Infomaniak.kDrive.ViewModels
             }
         }
 
-        public bool NotifyBeforeDelete
+        public bool AskBeforeDelete
         {
             get => _notifyBeforeDelete;
             set => SetPropertyInUIThread(ref _notifyBeforeDelete, value);
@@ -206,13 +206,13 @@ namespace Infomaniak.kDrive.ViewModels
 
         public async Task<bool> ChangeNotifyBeforeDelete(bool enabled)
         {
-            if (NotifyBeforeDelete == enabled)
+            if (AskBeforeDelete == enabled)
                 return true;
 
-            NotifyBeforeDelete = enabled;
+            AskBeforeDelete = enabled;
             if (!await App.ServiceProvider.GetRequiredService<IServerCommService>().SaveSettings(CancellationToken.None))
             {
-                NotifyBeforeDelete = !enabled;
+                AskBeforeDelete = !enabled;
                 return false;
             }
             return true;
