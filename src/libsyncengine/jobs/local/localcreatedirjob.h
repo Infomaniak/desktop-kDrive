12787c4f9d31c5ce1356174ed2990f0835b5a857
Christophe Larchier <christophe.larchier@infomaniak.com>
2026-08-06 16:28:54 +0200

    fix: issue

--- src/libsyncengine/jobs/local/localcreatedirjob.h
+++ src/libsyncengine/jobs/local/localcreatedirjob.h
@@ -24,7 +24,7 @@ namespace KDC {
 
 class LocalCreateDirJob : public SyncJob {
     public:
-        LocalCreateDirJob(const SyncPath &destFilepath, bool readOnly = false);
+        LocalCreateDirJob(const SyncPath &destFilepath);
 
         SyncPath destFilePath() const { return _destFilePath; }
 
@@ -39,8 +39,6 @@ class LocalCreateDirJob : public SyncJob {
         ExitInfo runJob() override;
 
         SyncPath _destFilePath;
-        bool _readOnly{false};
-
         NodeId _nodeId;
         SyncTime _modtime = 0;
         SyncTime _creationTime = 0;
