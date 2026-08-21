a5542b6d3a9cff38cf14d21c19073819e1b4faa5
Romain Galland <romain.galland@infomaniak.com>
2026-08-21 10:29:31 +0200

    fix(tlscert): correct log message for loaded certificate

--- src/gui4/linux/communicationlayer/tlscerthelper.cpp
+++ src/gui4/linux/communicationlayer/tlscerthelper.cpp
@@ -70,7 +70,7 @@ bool TLSCertHelper::readServerCertificate(QSslCertificate &certificate) {
         qCWarning(lcCertHelper) << "Failed to parse certificate PEM from keychain";
         return false;
     }
-    qCInfo(lcCertHelper) << "Certificate loaded from keychain";
+    qCInfo(lcCertHelper) << "Server ertificate loaded from keychain";
     return true;
 }
 
