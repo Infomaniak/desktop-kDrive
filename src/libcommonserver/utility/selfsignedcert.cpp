d79fc76b9f842affcd9e1b2c0cc2a726561a5fc6
Clément Kunz <clement.kunz@infomaniak.com>
2026-08-24 09:28:48 +0200

    chore: fix rebase issues

--- src/libcommonserver/utility/selfsignedcert.cpp
+++ src/libcommonserver/utility/selfsignedcert.cpp
@@ -170,11 +170,11 @@ bool SelfSignedCert::generateAndPublishClientCert(Pem &pem) {
     }
     if (!generate(pem, true)) return false;
 
-    if (!keychain->writeToken(std::string(clientCertKeychainKey), pem.cert)) {
+    if (!keychain->writeData(std::string(clientCertKeychainKey), pem.cert)) {
         LOG_ERROR(Log::instance()->getLogger(), "Failed to store the TLS client certificate in the keychain");
         return false;
     }
-    if (!keychain->writeToken(std::string(clientKeyKeychainKey), pem.key)) {
+    if (!keychain->writeData(std::string(clientKeyKeychainKey), pem.key)) {
         LOG_ERROR(Log::instance()->getLogger(), "Failed to store the TLS client private key in the keychain");
         return false;
     }
