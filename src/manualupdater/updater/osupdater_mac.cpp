d4a53ac79582a5bb282e7b7d20e8ebb704282a36
Lawrence Leitgib <lawrence.leitgib@infomaniak.com>
2026-08-25 17:45:33 +0200

    fix: update checksum verification to include error message output

--- src/manualupdater/updater/osupdater_mac.cpp
+++ src/manualupdater/updater/osupdater_mac.cpp
@@ -89,8 +89,7 @@ bool OSUpdater::install(const VersionInfo &versionInfo, const std::function<void
     }
 
     progressCallback(InstallStep::Verifying, QObject::tr("Verifying file integrity..."));
-    if (!verifyChecksum(SyncPath(pkgPath.toStdString()), pkgUrl.toStdString())) {
-        outMessage = QObject::tr("Checksum verification failed. The file may be corrupted.");
+    if (!verifyChecksum(SyncPath(pkgPath.toStdString()), pkgUrl.toStdString(), outMessage)) {
         return false;
     }
 
