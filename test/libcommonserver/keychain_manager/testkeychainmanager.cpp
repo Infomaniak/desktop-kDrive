51a21f5af6d2ffe54d6d7e840f17e078efe77195
Clément Kunz <clement.kunz@infomaniak.com>
2026-08-28 15:22:32 +0200

    test(KeychainManager): implement unit test and fix timeout evaluation

---
+++ test/libcommonserver/keychain_manager/testkeychainmanager.cpp
@@ -0,0 +1,54 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "testkeychainmanager.h"
+
+#include "keychainmanager/keychainmanager.h"
+#include "keychainmanager/keychainstorage.h"
+#include "test_utility/testhelpers.h"
+#include "utility/utility.h"
+
+namespace KDC {
+
+class MockKeyChainStorageWithTimeout : public IKeyChainStorage {
+    public:
+        bool writePassword([[maybe_unused]] const std::string &keychainKey,
+                           [[maybe_unused]] const std::string &rawData) override {
+            return true;
+        }
+        bool readPassword([[maybe_unused]] const std::string &keychainKey, std::string &data, bool &found) override {
+            Utility::msleep(90000); // Simulate a timeout by sleeping for 90 seconds
+            data = "dummy_data";
+            found = true;
+            return true;
+        }
+        bool deletePassword([[maybe_unused]] const std::string &keychainKey) override { return true; }
+
+        bool isTesting() override { return true; }
+};
+
+void TestKeychainManager::testTimeOut() {
+    if (!testhelpers::isExtendedTest()) return;
+
+    std::string data;
+    bool found = false;
+    KeyChainManager::instance(std::make_shared<MockKeyChainStorageWithTimeout>());
+    CPPUNIT_ASSERT(!KeyChainManager::instance()->readData("dummy_key", data, found));
+}
+
+} // namespace KDC
