15295a94c280c6cf2053e8f1600d3170b73bd336
Herve <herve.eruam@infomaniak.com>
2026-09-02 15:07:08 +0200

    feat: add support for InvalidLinkTarget

---
+++ src/gui4/windows/kDrive client/kDrive client/CustomControls/Errors/Templates/Node/InvalidLinkTarget.xaml.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using Infomaniak.kDrive.Analytics;
+using Infomaniak.kDrive.Types;
+using Infomaniak.kDrive.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace Infomaniak.kDrive.CustomControls.Errors.Templates.Node
+{
+    [ErrorMetadata(
+        Levels = new[] { ErrorLevel.Node },
+        NodeTypes = new[] { NodeType.File, NodeType.Directory },
+        CancelTypes = new[] { CancelType.InvalidLinkTarget }
+    )]
+    public sealed partial class InvalidLinkTarget : UserControl
+    {
+        private readonly IAnalyticsService _analyticsService = App.ServiceProvider.GetRequiredService<IAnalyticsService>();
+        private Error Error { get; init; }
+        public InvalidLinkTarget(Error error)
+        {
+            this.InitializeComponent();
+            Error = error;
+        }
+    }
+}
--- src/gui4/windows/kDrive client/kDrive client/ServerCommunication/Services/CommSharedEnums.cs
+++ src/gui4/windows/kDrive client/kDrive client/ServerCommunication/Services/CommSharedEnums.cs
@@ -345,7 +345,8 @@ namespace Infomaniak.kDrive.Types
         TmpBlacklisted,
         ExcludedByTemplate,
         Hardlink,
-        FileRescued
+        FileRescued,
+        InvalidLinkTarget
     };
 
     public enum InconsistencyType
