84cc3072bce6e4e8888b16e3de5fe1ca322a2452
Herve <herve.eruam@infomaniak.com>
2026-09-03 18:25:24 +0200

    feat: avoid useless copy of filesPaths vector in SignalSyncNotifyManyDeletesJob

--- src/libsyncengine/syncpal/syncpal.h
+++ src/libsyncengine/syncpal/syncpal.h
@@ -163,7 +163,7 @@ class SYNCENGINE_EXPORT SyncPal : public std::enable_shared_from_this<SyncPal> {
         }
 
         inline void setSendManyDeletesNotification(
-                const std::function<void(SyncDbId, TooManyDeletesNotificationType, uint64_t, std::vector<SyncPath>)>
+                const std::function<void(SyncDbId, TooManyDeletesNotificationType, uint64_t, const std::vector<SyncPath> &)>
                         &sendManyDeletesNotification) {
             _sendManyDeletesNotification = sendManyDeletesNotification;
         }
@@ -265,7 +265,7 @@ class SYNCENGINE_EXPORT SyncPal : public std::enable_shared_from_this<SyncPal> {
         void fixConflictedFilesCompleted(SyncDbId syncDbId, uint64_t nbErrors);
         void resolveSyncErrorsByExitCause(ExitCause cause);
         void sendManyDeletesNotification(TooManyDeletesNotificationType notificationType, uint64_t nbFiles,
-                                         std::vector<SyncPath> filesPaths);
+                                         const std::vector<SyncPath> &filesPaths);
 
         bool wipeVirtualFiles();
         bool wipeOldPlaceholders();
@@ -407,7 +407,7 @@ class SYNCENGINE_EXPORT SyncPal : public std::enable_shared_from_this<SyncPal> {
         std::function<void(SyncDbId syncDbId, const SyncFileItem &item, bool notify)> _addCompletedItem;
         std::function<void(SyncDbId syncDbId, uint64_t nbErrors)> _fixConflictedFilesCompleted;
         std::function<void(SyncDbId syncDbId, TooManyDeletesNotificationType notificationType, uint64_t nbFiles,
-                           std::vector<SyncPath> filesPaths)>
+                           const std::vector<SyncPath> &filesPaths)>
                 _sendManyDeletesNotification;
         std::shared_ptr<Vfs> _vfs;
 
