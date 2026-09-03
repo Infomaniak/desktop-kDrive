965654f5a943353c94daa6fed42fdcf399972317
Luc Guyot <luc.guyot@infomaniak.com>
2026-09-03 14:29:14 +0200

    fix(unit-tests): Implements unit tests for IoHelper::deleteItemAtomically and GenericDeleteJob::hardDelete"

--- src/libsyncengine/jobs/local/synclocaldeletejob.cpp
+++ src/libsyncengine/jobs/local/synclocaldeletejob.cpp
@@ -60,7 +60,7 @@ bool SyncLocalDeleteJob::matchRelativePaths(const SyncPath &remoteTargetPath, co
 SyncLocalDeleteJob::SyncLocalDeleteJob(const std::shared_ptr<SyncPal> syncPal, const SyncPath &relativeLocalPath,
                                        const bool liteSyncIsEnabled, RemoteNodeId remoteNodeId,
                                        ForceToTrash forceToTrash /* = ForceToTrash::No */) :
-    GenericLocalDeleteJob(syncPal->localPath(), syncPal->cacheDirectory()),
+    GenericLocalDeleteJob(syncPal->localPath() / relativeLocalPath, syncPal->cacheDirectory()),
     _liteSyncIsEnabled(liteSyncIsEnabled),
     _syncPal(syncPal),
     _relativeLocalPath(relativeLocalPath),
