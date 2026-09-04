23e5999df557c1c8bd5aa746c1e4fdfdf5de1ecc
Luc Guyot <luc.guyot@infomaniak.com>
2026-09-04 16:33:53 +0200

    feat(abstract-network-job): Passes the application UUID through the custom context header named ik-client-app-id

---
+++ test/libsyncengine/jobs/network/testabstractnetworkjob.h
@@ -0,0 +1,44 @@
+/*
+ * Infomaniak kDrive - Desktop
+ * Copyright (C) 2023-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "testincludes.h"
+#include "test_utility/localtemporarydirectory.h"
+
+using namespace CppUnit;
+
+namespace KDC {
+class TestAbstractNetworkJob : public CppUnit::TestFixture, public TestBase {
+    public:
+        CPPUNIT_TEST_SUITE(TestAbstractNetworkJob);
+        CPPUNIT_TEST(testAppUIDIsSentInClientAppIdHeader);
+        CPPUNIT_TEST(testExplicitContextIsPassedThroughRequestContext);
+        CPPUNIT_TEST_SUITE_END();
+
+    public:
+        void setUp() override;
+        void tearDown() override;
+
+    private:
+        void testAppUIDIsSentInClientAppIdHeader();
+        void testExplicitContextIsPassedThroughRequestContext();
+
+        LocalTemporaryDirectory _localTempDir{"testAbstractNetworkJob"};
+};
+} // namespace KDC
