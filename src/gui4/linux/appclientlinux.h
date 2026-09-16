d8048e6134121480d01f14916a93990c1e0e008b
Romain Galland <romain.galland@infomaniak.com>
2026-09-16 17:18:20 +0200

    feat(logs): apply client log settings and register debug settings files

--- src/gui4/linux/appclientlinux.h
+++ src/gui4/linux/appclientlinux.h
@@ -115,7 +115,7 @@ class AppClientLinux : public QApplication {
         void handleBootstrapCompletion();
         void openSettingsWindow();
         void retranslatePresentation();
-        void updateLoggerMinLevel() const;
+        void updateLoggerSettings() const;
         void requestQuit();
         void quitOnServerDisconnection();
         void handleManyDeletesPresentationRequested();
@@ -156,8 +156,13 @@ class AppClientLinux : public QApplication {
         StorageController _storageController{_mainSelectionStore, this};
         TranslationService _translationService{_parametersStore, this};
         UpdateStatusService _updateStatusService{_serverCommService, _parametersStore, this};
-        SettingsWindowController _settingsWindowController{_parametersStore,     _parametersService, _translationService,
-                                                           _updateStatusService, _sentryService,     this};
+        SettingsWindowController _settingsWindowController{_parametersStore,
+                                                           _parametersService,
+                                                           _translationService,
+                                                           _updateStatusService,
+                                                           _sentryService,
+                                                           _serverCommService,
+                                                           this};
         QPointer<QWindow> _settingsWindow;
         QQmlApplicationEngine _qmlEngine;
         bool _bootstrapCompleted{false};
