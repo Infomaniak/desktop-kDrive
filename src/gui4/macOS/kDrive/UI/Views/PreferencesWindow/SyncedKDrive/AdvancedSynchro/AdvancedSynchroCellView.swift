8df4ea66daae74f871b83f973a91848e0d806528
Clément Kunz <clement.kunz@infomaniak.com>
2026-09-16 10:19:04 +0200

    chore: fix copyright

--- src/gui4/macOS/kDrive/UI/Views/PreferencesWindow/SyncedKDrive/AdvancedSynchro/AdvancedSynchroCellView.swift
+++ src/gui4/macOS/kDrive/UI/Views/PreferencesWindow/SyncedKDrive/AdvancedSynchro/AdvancedSynchroCellView.swift
@@ -1,4 +1,4 @@
-SignalMessageTests/*
+/*
  Infomaniak kDrive - Desktop
  Copyright (C) 2023-2026 Infomaniak Network SA
 
@@ -115,11 +115,11 @@ struct AdvancedSynchroCellView: View {
                 isConverting: synchro.isConverting,
                 isCallFromAdvancedSync: true
             )
-            .disabled(!synchro.supportsVirtualFileSystem || synchro.isConverting)
-            .onChange(of: synchroMode) { newValue in
-                guard newValue != committedSynchroMode else { return }
-                switchSynchroMode(newValue)
-            }
+                .disabled(!synchro.supportsVirtualFileSystem || synchro.isConverting)
+                .onChange(of: synchroMode) { newValue in
+                    guard newValue != committedSynchroMode else { return }
+                    switchSynchroMode(newValue)
+                }
         }
     }
 
