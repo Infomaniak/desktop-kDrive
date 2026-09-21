1fed600146b19bdc8b6396bb20140ae3c8c93094
Clément Kunz <clement.kunz@infomaniak.com>
2026-09-21 09:53:00 +0200

    fix: propagate exit info

--- src/libsyncengine/propagation/executor/executorworker.cpp
+++ src/libsyncengine/propagation/executor/executorworker.cpp
@@ -1547,8 +1547,11 @@ ExitInfo ExecutorWorker::handleForbiddenAction(SyncOpPtr syncOp, const SyncPath 
         case OperationType::Edit: {
             // Rename the file so as not to lose any information
             SyncPath newSyncPath;
-            PlatformInconsistencyCheckerUtility::renameLocalFile(
-                    absoluteLocalFilePath, PlatformInconsistencyCheckerUtility::SuffixType::Conflict, &newSyncPath);
+            if (const auto exitInfo = PlatformInconsistencyCheckerUtility::renameLocalFile(
+                        absoluteLocalFilePath, PlatformInconsistencyCheckerUtility::SuffixType::Conflict, &newSyncPath);
+                !exitInfo) {
+                return exitInfo;
+            }
 
             // Exclude file from sync
             if (!_syncPal->vfs()->fileStatusChanged(newSyncPath, SyncFileStatus::Ignored)) {
@@ -2456,10 +2459,13 @@ ExitInfo ExecutorWorker::removeDependentOps(const std::shared_ptr<Node> localNod
     return ExitCode::Ok;
 }
 
-ExitInfo ExecutorWorker::excludeFileFromSync(SyncOpPtr syncOp, const SyncPath &absoluteLocalFilepath) {
+ExitInfo ExecutorWorker::excludeFileFromSync(const SyncOpPtr syncOp, const SyncPath &absoluteLocalFilepath) {
     // Blacklist placeholder
-    (void) PlatformInconsistencyCheckerUtility::renameLocalFile(absoluteLocalFilepath,
-                                                                PlatformInconsistencyCheckerUtility::SuffixType::Blacklisted);
+    if (const auto exitInfo = PlatformInconsistencyCheckerUtility::renameLocalFile(
+                absoluteLocalFilepath, PlatformInconsistencyCheckerUtility::SuffixType::Blacklisted);
+        !exitInfo) {
+        return exitInfo;
+    }
 
     // Clear update tree
     if (!deleteOpNodes(syncOp)) {
